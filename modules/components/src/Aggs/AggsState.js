59e02b8feea88f909147fe82ef9509892764e0c8
Alex Wilmer <djazium@gmail.com>
2018-01-11 18:25:56 -0500

    npm issue

---
+++ modules/components/src/Aggs/AggsState.js
@@ -0,0 +1,79 @@
+import { Component } from 'react';
+import { debounce } from 'lodash';
+
+let API = 'http://localhost:5050';
+
+let aggFields = `
+  index
+  states {
+    timestamp
+    state {
+      field
+      displayName
+      active
+      type
+      allowedValues
+      restricted
+    }
+  }
+`;
+
+let api = body =>
+  fetch(API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).then(r => r.json());
+
+export default class extends Component {
+  state = { aggs: [], temp: [] };
+  async componentDidMount() {
+    let { data } = await api({
+      query: `
+        {
+        	aggsState(indices:["${this.props.index}"]) {
+            ${aggFields}
+          }
+        }
+      `,
+    });
+
+    this.setState({
+      aggs: data.aggsState[0].states[0].state,
+      temp: data.aggsState[0].states[0].state,
+    });
+  }
+  save = debounce(async state => {
+    let { data } = await api({
+      variables: { state },
+      query: `
+        mutation($state: JSON!) {
+          saveAggsState(
+            state: $state
+            index: "${this.props.index}"
+          ) {
+            ${aggFields}
+          }
+        }
+      `,
+    });
+
+    this.setState({
+      aggs: data.saveAggsState.states[0].state,
+      temp: data.saveAggsState.states[0].state,
+    });
+  }, 300);
+  update = ({ field, key, value }) => {
+    let agg = this.state.temp.find(x => x.field === field);
+    let index = this.state.temp.findIndex(x => x.field === field);
+    let temp = Object.assign([], this.state.temp, {
+      [index]: { ...agg, [key]: value },
+    });
+    this.setState({ temp }, () => this.save(temp));
+  };
+  render() {
+    return this.props.render({ update: this.update, aggs: this.state.temp });
+  }
+}
--- sandbox/example-ui/src/App.js
+++ sandbox/example-ui/src/App.js
@@ -1,122 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { debounce } from 'lodash';
-
-let API = 'http://localhost:5050';
-
-let aggFields = `
-  index
-  states {
-    timestamp
-    state {
-      field
-      displayName
-      active
-      type
-      allowedValues
-      restricted
-    }
-  }
-`;
-
-let api = body =>
-  fetch(API, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  }).then(r => r.json());
-
-class Aggs extends Component {
-  state = { aggs: [], temp: [], searchTerm: '' };
-  async componentDidMount() {
-    let { data } = await api({
-      query: `
-        {
-        	aggsState(indices:["${this.props.index}"]) {
-            ${aggFields}
-          }
-        }
-      `,
-    });
-
-    this.setState({
-      aggs: data.aggsState[0].states[0].state,
-      temp: data.aggsState[0].states[0].state,
-    });
-  }
-  save = debounce(async state => {
-    let { data } = await api({
-      variables: { state },
-      query: `
-        mutation($state: JSON!) {
-          saveAggsState(
-            state: $state
-            index: "${this.props.index}"
-          ) {
-            ${aggFields}
-          }
-        }
-      `,
-    });
-
-    this.setState({
-      aggs: data.saveAggsState.states[0].state,
-      temp: data.saveAggsState.states[0].state,
-    });
-  }, 300);
-  update = ({ field, key, value }) => {
-    let agg = this.state.temp.find(x => x.field === field);
-    let index = this.state.temp.findIndex(x => x.field === field);
-    let temp = Object.assign([], this.state.temp, {
-      [index]: { ...agg, [key]: value },
-    });
-    this.setState({ temp }, () => this.save(temp));
-  };
-  render() {
-    return (
-      <Fragment>
-        <div style={{ padding: 10 }}>
-          <label>filter: </label>
-          <input
-            type="text"
-            value={this.state.searchTerm}
-            onChange={e => this.setState({ searchTerm: e.target.value })}
-          />
-        </div>
-        {this.state.temp
-          .filter(x => x.field.includes(this.state.searchTerm))
-          .map(x => (
-            <div key={x.field} style={{ padding: 10 }}>
-              <div>field: {x.field}</div>
-              <div>
-                displayName:
-                <input
-                  value={x.displayName}
-                  onChange={e =>
-                    this.update({
-                      field: x.field,
-                      key: 'displayName',
-                      value: e.target.value,
-                    })
-                  }
-                />
-              </div>
-              <div>
-                active: <input type="checkbox" checked={x.active} />
-              </div>
-              <div>type: {x.type}</div>
-              <div>allowedValues: {x.allowedValues}</div>
-              <div>
-                restricted: <input type="checkbox" checked={x.restricted} />
-              </div>
-            </div>
-          ))}
-      </Fragment>
-    );
-  }
-}
+import { AggsState, EditAggs } from '@arranger/components/lib/Aggs';
 
 class App extends Component {
   render() {
@@ -125,7 +9,9 @@ class App extends Component {
         <Fragment>
           <Route
             path="/admin/:index/:component"
-            component={props => <Aggs index={props.match.params.index} />}
+            component={props => (
+              <AggsState index={props.match.params.index} render={EditAggs} />
+            )}
           />
         </Fragment>
       </BrowserRouter>
