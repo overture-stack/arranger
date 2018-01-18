2b94f5042aa922f0e92bf6514a9dad1c9aeb4219
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-01-18 07:07:12 -0500

    Added _get_and_filter and helper functions

--- modules/middleware/src/filters.js
+++ modules/middleware/src/filters.js
@@ -53,13 +53,13 @@ export default class FilterProcessor{
         if(op === CONSTANTS.IN)
             f = this._get_term_or_regex_or_set_filter(doc_type, nested, filters);
         if(op === CONSTANTS.EXCLUDE)
-            f = _get_must_not_filter(doc_type, nested, filters);
+            f = this._get_must_not_filter(doc_type, nested, filters);
         if(op === CONSTANTS.EXCLUDE_IF_ANY)
-            f = _get_must_not_any_filter(doc_type, nested, filters);
+            f = this._get_must_not_any_filter(doc_type, nested, filters);
         if(op in CONSTANTS.IS_OPS)
-            f = _get_missing_filter(doc_type, nested, content,op);
+            f = this._get_missing_filter(doc_type, nested, content,op);
         if(op === CONSTANTS.AND)
-            f = _get_and_filter(doc_type, nested, content);
+            f = this._get_and_filter(doc_type, nested, content);
         if(op === CONSTANTS.OR)
             f = _get_or_filter(doc_type, nested, content);
         if(op in CONSTANTS.RANGE_OPS_KEYS)
@@ -118,6 +118,91 @@ export default class FilterProcessor{
             };
         }
     }
+    _get_term_or_regex_or_set_filter(doc_type, nested, x){
+        let value;
+
+        typeof x['content']['value'] === "object" ? value = x['content']['value'][0] : value = x['content']['value'];
+
+        if (typeof value === "string"){
+            if (value.includes('*'))
+                return this._get_regex_filter(doc_type, nested, x);
+            else if (value.includes('set_id:'))
+                return this._get_set_filter(doc_type, nested, x);
+        }
+        return this._get_term_filter(doc_type, nested, x);
+    }
+    _get_must_not_filter(doc_type, nested, x){
+        let tf = this._get_term_filter(doc_type, nested, x)
+        return this.is_nested(tf) ? tf : this.wrap_not(tf);
+    }
+    _get_must_not_any_filter(doc_type, nested, x){
+        let tf = _get_term_filter(doc_type, nested, x);
+        return this.wrap_not(tf);
+    }
+    _get_missing_filter(doc_type, nested, content, op){
+        // TODO: make this generic
+        let k = field_to_full_path_on_doc_type(doc_type, content['field']);
+
+        //# FIXME assumes missing
+        let r = {'exists': {'field': k, "boost": 0}};
+        let path = k.split(".");
+
+        for( let i =1; i < path.length; i++) {
+            let p = _.chunk(path, path.length - i)[0].join(".");
+            if (nested.includes(p)) {
+                r = this.wrap_must(r);
+                r = this.wrap_filter(r, p);
+            }
+        }
+        if (op === CONSTANTS.IS) r = this.wrap_not(r);
+        return r
+    }
+    _get_and_filter(doc_type, nested, xs){
+        let musts= [],
+            must_nots = [],
+            shoulds = [];
+
+        //# TODO remove for loop
+        xs.forEach(x => {
+            let op = x['op'],
+                content = x['content'];
+
+            let is_must = op in CONSTANTS.MUST_OPS.concat(CONSTANTS.RANGE_OPS_KEYS);
+            let is_must_not = op in CONSTANTS.MUST_NOT_OPS;
+            let is_should = op === CONSTANTS.OR;
+
+            if (is_must || is_must_not){
+                let curr;
+                if (op in CONSTANTS.VALUE_OPS)
+                    curr = this._get_term_or_regex_or_set_filter(doc_type, nested, x);
+                else if(op in CONSTANTS.IS_OPS)
+                    curr = this._get_missing_filter(doc_type, nested, content, op);
+                else if (op in CONSTANTS.RANGE_OPS_KEYS)
+                    curr = this._get_range_filter(doc_type, nested, content, op);
+
+                if (this.is_nested(curr)){
+                    let filteredMusts =
+                        _.filter(musts, m => this.is_nested(m) && this.read_path(m) === this.read_path(curr));
+                    this.collapse_nested_filters(filteredMusts.length > 0 ? filteredMusts[0] : null,
+                        curr, op === CONSTANTS.EXCLUDE_IF_ANY ? must_nots : musts);
+                }
+                else if(is_must_not) must_nots.concat(curr);
+                else musts.concat(curr)
+            }
+            else if (is_should)
+                shoulds.concat(_get_or_filter(doc_type, nested, content));
+        });
+        //   # wrap both shoulds and must in ES_MUST so ES_SHOULD so score does not interfere
+        //   # _get_or_filter already wraps shoulds with ES_SHOULD
+        let esMust = CONSTANTS.ES_MUST,
+            esMustNot = CONSTANTS.ES_MUST_NOT;
+        let r = _.reduce({ esMust: shoulds !== null ? musts.concat(shoulds): musts,
+              esMustNot : must_nots
+            }, (result, value, key) => value !== null && value.length > 0 ? result[key] = value : result
+            , {});
+
+        return {ES_BOOL:r}
+    }
     term_optimizer(op, content){
         let op_mapping = {
             EQ: IN,
@@ -144,27 +229,6 @@ export default class FilterProcessor{
         });
 
         return other.concat(inside);
-    }
-    _get_term_or_regex_or_set_filter(doc_type, nested, x){
-        let value;
-
-        typeof x['content']['value'] === "object" ? value = x['content']['value'][0] : value = x['content']['value'];
-
-        if (typeof value === "string"){
-            if (value.includes('*'))
-                return this._get_regex_filter(doc_type, nested, x);
-            else if (value.includes('set_id:'))
-                return this._get_set_filter(doc_type, nested, x);
-        }
-        return this._get_term_filter(doc_type, nested, x);
-    }
-    _get_must_not_filter(doc_type, nested, x){
-        let tf = this._get_term_filter(doc_type, nested, x)
-        return this.is_nested(tf) ? tf : this.wrap_not(tf);
-    }
-    _get_must_not_any_filter(doc_type, nested, x){
-        let tf = _get_term_filter(doc_type, nested, x);
-        return this.wrap_not(tf);
     }
     _get_regex_filter(doc_type, nested, x, upperCase){
         let op = x['op'],
@@ -255,6 +319,34 @@ export default class FilterProcessor{
         r = this.wrap_filter_based_on_path(r, path,nested, op);
         return r
     }
+    collapse_nested_filters(found, curr, musts){
+        if (found !== null) {
+            if(this.read_nested_bool(curr).includes(CONSTANTS.ES_MUST)) {
+                let child = this.read_nested_bool(curr)[ES_MUST][0];
+                let found_musts = _.get(this.read_nested_bool(found), CONSTANTS.ES_MUST, []);
+                if (this.is_nested(child)) {
+                    let filtered = _.filter(found_musts,
+                        m => this.is_nested(m) && this.read_path(m) === this.read_path(child));
+                    this.collapse_nested_filters(filtered.length > 0 ? filtered[0] : null, child, found_musts)
+                }
+                else {
+                    this.read_nested_bool(found)[CONSTANTS.ES_MUST] =
+                        _.get(this.read_nested_bool(found), CONSTANTS.ES_MUST, []).
+                        concat(this.read_nested_bool(curr)[CONSTANTS.ES_MUST]);
+                }
+            }
+        else
+            {
+                //# must_not won't have any nested filters
+               this.read_nested_bool(found)[CONSTANTS.ES_MUST_NOT] =
+                   _.get(this.read_nested_bool(found),CONSTANTS.ES_MUST_NOT, []).
+                   concat(this.read_nested_bool(curr)[CONSTANTS.ES_MUST_NOT]);
+            }
+        }
+        else
+            musts.concat(curr)
+
+    }
     wrap_filter_based_on_path(r, path,nested, op){
         for( let i =1; i < path.length; i++) {
             let p = _.chunk(path, path.length - i)[0].join(".");
@@ -287,6 +379,18 @@ export default class FilterProcessor{
                 ES_QUERY: val
             }
         }
+    }
+    read_nested(x){
+        return _.get(x,CONSTANTS.ES_NESTED, {});
+    }
+    read_path(x){
+        return _.get(this.read_nested(x),'path', '');
+    }
+    read_nested_query(x){
+        return _.get(this.read_nested(x), CONSTANTS.ES_QUERY, {});
+    }
+    read_nested_bool(x){
+        return _.get(this.read_nested_query(x), CONSTANTS.ES_BOOL, {});
     }
 
 }
