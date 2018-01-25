da1ab989d9aaa42082b6004e87b36d5983046c5e
Alex Wilmer <djazium@gmail.com>
2018-01-24 19:58:13 -0500

    delete type index if deleted from project

---
+++ sandbox/example-api/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+require('babel-polyfill');
+require('@babel/register')({
+  presets: ['@babel/env'],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+});
+
+require('./src/index');
