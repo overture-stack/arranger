79c051f02577acff6d32005fc8dd3f4bcdb58861
Minh <hlminh2000@gmail.com>
2018-02-01 13:53:36 -0500

    bubbling mapping data from treeview to the facet view

--- modules/components/src/AdvancedFacetView/index.js
+++ modules/components/src/AdvancedFacetView/index.js
@@ -6,24 +6,39 @@ import './AdvancedFacetView.css';
 
 const { elasticMappingToDisplayTreeData } = mappingToDisplayTreeData;
 
-export default ({ elasticMapping }) => (
-  <State
-    initial={{
-      selectedPath: '',
-    }}
-    render={({ update, selectedPath }) => (
-      <div className="facetViewWrapper">
-        <div className="treeViewPanel">
-          <NestedTreeView
-            dataSource={elasticMappingToDisplayTreeData(elasticMapping)}
-            selectedPath={selectedPath}
-            onLeafSelect={path => {
-              update({ selectedPath: path });
-            }}
-          />
+export default ({ elasticMapping }) => {
+  const fieldMappingFromPath = path =>
+    path
+      .split('.')
+      .reduce(
+        (parentNode, nextPath) =>
+          parentNode[nextPath]
+            ? parentNode[nextPath]
+            : parentNode.properties ? parentNode.properties[nextPath] : {},
+        elasticMapping,
+      ) || {};
+
+  return (
+    <State
+      initial={{
+        selectedPath: '',
+      }}
+      render={({ update, selectedPath }) => (
+        <div className="facetViewWrapper">
+          <div className="treeViewPanel">
+            <NestedTreeView
+              dataSource={elasticMappingToDisplayTreeData(elasticMapping)}
+              selectedPath={selectedPath}
+              onLeafSelect={path => {
+                update({ selectedPath: path });
+              }}
+            />
+          </div>
+          <div className="facetsPanel">
+            type: {fieldMappingFromPath(selectedPath).type}
+          </div>
         </div>
-        <div className="facetsPanel" />
-      </div>
-    )}
-  />
-);
+      )}
+    />
+  );
+};
