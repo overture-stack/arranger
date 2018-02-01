3d31f2c774c4daea1a2dbb8884953a0fe59b2421
Minh <hlminh2000@gmail.com>
2018-02-01 16:45:35 -0500

    live advanced facet view story

--- modules/components/stories/AdvancedFacetView.js
+++ modules/components/stories/AdvancedFacetView.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { mappingToDisplayTreeData } from '@arranger/mapping-utils';
 import { themeDecorator } from './decorators';
 import AdvancedFacetView from '../src/AdvancedFacetView';
 import elasticMockMapping from '../src/AdvancedFacetView/elasticMockMapping';
 
-const { elasticMappingToDisplayTreeData } = mappingToDisplayTreeData;
+const PROJECT_ID = 'some_project';
+const API_HOST = 'http://localhost:5050';
+const ES_HOST = 'http://localhost:9200';
+
+const fetchMapping = () =>
+  fetch(`${API_HOST}/${PROJECT_ID}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ES_HOST: ES_HOST, // TODO; get from somewhere
+    },
+    body: JSON.stringify({
+      query: `{
+        some_project {
+          mapping,
+          extended,
+        }
+      }`,
+    }),
+  })
+    .then(r => r.json())
+    .then(({ data }) => {
+      return Promise.resolve(data[PROJECT_ID]);
+    });
+
+class AdvancedFacetViewLiveStory extends React.Component {
+  state = {
+    mapping: {},
+    extended: {},
+  };
+  componentDidMount() {
+    fetchMapping().then(({ extended, mapping }) =>
+      this.setState({ extended, mapping }),
+    );
+  }
+  render() {
+    return (
+      <AdvancedFacetView
+        elasticMapping={this.state.mapping}
+        extendedMapping={this.state.extended}
+      />
+    );
+  }
+}
 
 storiesOf('AdvancedFacetView', module)
   .addDecorator(themeDecorator)
+  .add('AdvancedFacetViewLive', () => <AdvancedFacetViewLiveStory />)
   .add('AdvancedFacetView', () => (
     <AdvancedFacetView elasticMapping={elasticMockMapping} />
   ));
