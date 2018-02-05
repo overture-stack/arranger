68356cc60658bb634ba8707b48694604930b5d5a
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-02-05 15:03:23 -0500

    ✨ only watch dependencies

---
+++ modules/server/getDependencies.js
@@ -0,0 +1,16 @@
+var packageJSON = require('./package.json');
+var dependencyKeys = ['dependencies', 'peerDependencies', 'devDependencies'];
+console.log(
+  dependencyKeys
+    .reduce(
+      function(dependencies, key) {
+        return dependencies.concat(
+          Object.keys(packageJSON[key] || {})
+            .filter(n => n.indexOf('@arranger') >= 0)
+            .map(n => n.split('/')[1]),
+        );
+      },
+      ['', 'server'],
+    )
+    .join(' --watch '),
+);
