77f02b6a69f5ccb8fbe2931d6db8a334f6432762
Minh <hlminh2000@gmail.com>
2018-02-08 10:15:00 -0500

    styling

--- modules/components/src/AdvancedFacetView/FacetViewNode.js
+++ modules/components/src/AdvancedFacetView/FacetViewNode.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { esToAggTypeMap } from '@arranger/mapping-utils';
 import FacetWrapper from './Facets';
+import State from '../State';
 
 const esTypeToAggType = esType => esToAggTypeMap[esType];
 
@@ -15,11 +16,7 @@ const FacetViewNode = ({
   sqon = {},
 }) => {
   return (
-    <div
-      className="facetViewNode"
-      id={path.split('.').join('__')}
-      style={{ marginLeft: 20 }}
-    >
+    <div className="facetViewNode" id={path.split('.').join('__')}>
       {!children && (
         <div className="aggWrapper">
           {!esType && <div className="facetTitle">{title}</div>}
@@ -41,21 +38,33 @@ const FacetViewNode = ({
         </div>
       )}
       {children && (
-        <div className="aggWrapper">
-          <div className="facetTitle">{title}</div>
-          {children.map(childNode => (
-            <FacetViewNode
-              sqon={sqon}
-              key={childNode.path}
-              aggregations={aggregations}
-              onValueChange={onValueChange}
-              {...childNode}
-            />
-          ))}
-        </div>
+        <State
+          initial={{ expanded: true }}
+          render={({ update, expanded }) => (
+            <div className="aggWrapper">
+              <div
+                className="facetTitle"
+                onClick={() => update({ expanded: !expanded })}
+              >
+                {title}
+                <div className={`arrow ${!expanded ? 'collapsed' : ''}`} />
+              </div>
+              {expanded &&
+                children.map(childNode => (
+                  <FacetViewNode
+                    sqon={sqon}
+                    key={childNode.path}
+                    aggregations={aggregations}
+                    onValueChange={onValueChange}
+                    {...childNode}
+                  />
+                ))}
+            </div>
+          )}
+        />
       )}
     </div>
   );
 };
-
+// '\203A'
 export default FacetViewNode;
