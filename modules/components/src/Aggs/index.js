f6ede2def8c245ba4de35ac3147a2000bac5c3cc
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-02-09 09:30:34 -0500

    :bug: was missing some module exports (#125)

--- modules/components/src/Aggs/index.js
+++ modules/components/src/Aggs/index.js
@@ -1,4 +1,6 @@
 export { default as EditAggs } from './EditAggs';
+export { default as AggsPanel } from './AggsPanel';
 export { default as AggsState } from './AggsState';
 export { default as AggsQuery } from './AggsQuery';
 export { default as TermAgg } from './TermAgg';
+export { default as RangeAgg } from './RangeAgg';
