2b4e6227084e7791e732019bc5478cb9a70a0421
Minh <hlminh2000@gmail.com>
2018-02-09 15:43:27 -0500

    allow nesting in flattenMapping

--- modules/mapping-utils/src/flattenMapping.js
+++ modules/mapping-utils/src/flattenMapping.js
@@ -3,21 +3,20 @@ import { flattenDeep } from 'lodash';
 let joinWith = (s = '.') => x => (x ? x + s : '');
 
 let flattenMapping = (properties, parent = '') => {
-  return flattenDeep(
-    Object.entries(properties)
-      .filter(
-        ([field, data]) =>
-          (data.type && data.type !== 'nested') || data.properties,
-      )
-      .map(
-        ([field, data]) =>
-          data.type && data.type !== 'nested'
-            ? {
-                field: joinWith()(parent) + field,
-                type: data.type,
-              }
-            : flattenMapping(data.properties, joinWith()(parent) + field),
-      ),
+  return Object.entries(properties).map(
+    ([field, data]) =>
+      data.type && data.type !== 'nested' && !data.properties
+        ? {
+            field: joinWith()(parent) + field,
+            type: data.type,
+          }
+        : [
+            ...{
+              field: joinWith()(parent) + field,
+              type: data.type || 'object',
+            },
+            flattenMapping(data.properties, joinWith()(parent) + field),
+          ],
   );
 };
-export default flattenMapping;
+export default properties => flattenDeep(flattenMapping(properties));
