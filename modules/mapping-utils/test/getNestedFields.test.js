605459930eafb54196e102c5532566bd2fac59ab
Alex Wilmer <djazium@gmail.com>
2018-02-13 17:09:02 -0500

    ✅ add test back to mapping-utils

---
+++ modules/mapping-utils/test/getNestedFields.test.js
@@ -0,0 +1,33 @@
+import getNestedFields from '../src/getNestedFields';
+
+test('getNestedFields', () => {
+  let actual = getNestedFields({
+    diagnoses: {
+      type: 'nested',
+      properties: {
+        age_at_diagnosis: {
+          type: 'long',
+        },
+        project: {
+          properties: {
+            project_id: {
+              type: 'keyword',
+            },
+          },
+        },
+        treatments: {
+          type: 'nested',
+          properties: {
+            days_to_treatment: {
+              type: 'long',
+            },
+          },
+        },
+      },
+    },
+  });
+
+  let expected = ['diagnoses', 'diagnoses.treatments'];
+
+  expect(actual.length).toBe(expected.length);
+});
