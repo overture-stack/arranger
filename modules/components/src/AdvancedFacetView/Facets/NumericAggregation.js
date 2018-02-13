44b66b74d47d7ecc18bdd61270621426cf42587c
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-02-13 14:07:16 -0500

    :art: moved logic into helper for use externally

--- modules/components/src/AdvancedFacetView/Facets/NumericAggregation.js
+++ modules/components/src/AdvancedFacetView/Facets/NumericAggregation.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import RangeAgg from '../../Aggs/RangeAgg';
 
+import { currentFieldValue } from '../../SQONView/utils';
+
 export default ({ aggType, aggProps, title, onValueChange, sqon, path }) => (
   <RangeAgg
     stats={aggProps?.stats}
     collapsible={false}
     value={{
       min:
-        sqon?.content?.find(
-          content => content.content?.field === path && content.op === '>=',
-        )?.content.value || aggProps?.stats.min,
+        currentFieldValue({ sqon, dotField: path, op: '>=' }) ||
+        aggProps?.stats.min,
       max:
-        sqon?.content?.find(
-          content => content.content?.field === path && content.op === '<=',
-        )?.content.value || aggProps?.stats.max,
+        currentFieldValue({ sqon, dotField: path, op: '<=' }) ||
+        aggProps?.stats.max,
     }}
     displayName={title}
     handleChange={({ min, max, field }) =>
