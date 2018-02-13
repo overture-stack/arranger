00f820b2fde741e54beb1c994abfe3186e7f399e
Minh <hlminh2000@gmail.com>
2018-02-13 10:27:30 -0500

    minor refactoring to follow the original signature

--- modules/mapping-utils/src/flattenMapping.js
+++ modules/mapping-utils/src/flattenMapping.js
@@ -2,21 +2,22 @@ import { flattenDeep } from 'lodash';
 
 let joinWith = (s = '.') => x => (x ? x + s : '');
 
-let flattenMapping = (properties, parent = '') => {
-  return Object.entries(properties).map(
-    ([field, data]) =>
-      !data.properties
-        ? {
-            field: joinWith()(parent) + field,
-            type: data.type,
-          }
-        : [
-            {
+let flattenMapping = (properties, parent = '') =>
+  flattenDeep(
+    Object.entries(properties).map(
+      ([field, data]) =>
+        !data.properties
+          ? {
               field: joinWith()(parent) + field,
-              type: data.type || 'object',
-            },
-            ...flattenMapping(data.properties, joinWith()(parent) + field),
-          ],
+              type: data.type,
+            }
+          : [
+              {
+                field: joinWith()(parent) + field,
+                type: data.type || 'object',
+              },
+              ...flattenMapping(data.properties, joinWith()(parent) + field),
+            ],
+    ),
   );
-};
-export default properties => flattenDeep(flattenMapping(properties));
+export default flattenMapping;
