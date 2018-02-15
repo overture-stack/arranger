160627afcbef84fbce53e590ee73040ac7608934
Minh <hlminh2000@gmail.com>
2018-02-15 10:32:27 -0500

    filters fields with value by default

--- modules/components/src/AdvancedFacetView/index.js
+++ modules/components/src/AdvancedFacetView/index.js
@@ -73,7 +73,7 @@ export default class AdvancedFacetView extends React.Component {
     super(props);
     this.state = {
       selectedPath: null,
-      withValueOnly: false,
+      withValueOnly: true,
     };
   }
   fieldMappingFromPath = path => {
