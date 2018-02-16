dc15a2d480c21be8bd6ec37c454a90980af36934
Minh Ha <hlminh2000@gmail.com>
2018-02-16 14:55:34 -0500

    ✨handles nesting in flattenMapping (#133)
    * handles nesting in flattenMapping
    * adds testing for flattenMapping

--- modules/mapping-utils/src/flattenMapping.js
+++ modules/mapping-utils/src/flattenMapping.js
@@ -4,20 +4,21 @@ let joinWith = (s = '.') => x => (x ? x + s : '');
 
 let flattenMapping = (properties, parent = '') => {
   return flattenDeep(
-    Object.entries(properties)
-      .filter(
-        ([field, data]) =>
-          (data.type && data.type !== 'nested') || data.properties,
-      )
-      .map(
-        ([field, data]) =>
-          data.type && data.type !== 'nested'
-            ? {
+    Object.entries(properties).map(
+      ([field, data]) =>
+        !data.properties
+          ? {
+              field: joinWith()(parent) + field,
+              type: data.type,
+            }
+          : [
+              {
                 field: joinWith()(parent) + field,
-                type: data.type,
-              }
-            : flattenMapping(data.properties, joinWith()(parent) + field),
-      ),
+                type: data.type || 'object',
+              },
+              ...flattenMapping(data.properties, joinWith()(parent) + field),
+            ],
+    ),
   );
 };
 export default flattenMapping;
