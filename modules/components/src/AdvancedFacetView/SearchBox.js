2e14a066caedcbb0534b81ff134c9a9ab7ca8d2f
Minh <hlminh2000@gmail.com>
2018-02-20 16:57:41 -0500

    refactors for some more conventional ui

--- modules/components/src/AdvancedFacetView/SearchBox.js
+++ modules/components/src/AdvancedFacetView/SearchBox.js
@@ -5,53 +5,70 @@ import SearchIcon from 'react-icons/lib/fa/search';
 
 import { filterOutNonValue } from './utils.js';
 
-export default ({
-  withValueOnly,
-  elasticMapping,
-  extendedMapping,
-  aggregations,
-  onFieldSelect = () => {},
-}) => (
-  <State
-    initial={{ currentValue: '' }}
-    render={({ update, currentValue }) => {
-      const filteredList = (withValueOnly
-        ? filterOutNonValue({ extendedMapping, aggregations })
-            .extendedMappingWithValue
-        : extendedMapping
-      )?.filter?.(
-        ({ displayName }) =>
-          displayName
-            .toLowerCase()
-            .indexOf(
-              (currentValue?.length ? currentValue : null)?.toLowerCase(),
-            ) > -1,
-      );
-      return (
-        <div className="filterWrapper">
-          <TextInput
-            icon={<SearchIcon />}
-            className="filterInput"
-            type="text"
-            placeholder="Filter"
-            value={currentValue}
-            onChange={e => update({ currentValue: e.target.value })}
-          />
-          {currentValue.length ? (
-            <div className={`resultList shown`}>
-              {filteredList?.map(({ displayName, field, ...rest }) => (
-                <div
-                  className="resultItem"
-                  onClick={() => onFieldSelect(field)}
-                >
-                  <span className="title">{displayName}</span>
-                  <span className="field">{field}</span>
-                </div>
-              ))}
-            </div>
-          ) : null}
-        </div>
-      );
-    }}
-  />
-);
+export default class extends React.Component {
+  state = { currentValue: '', isDropdownShown: false };
+  onWindowClick = e =>
+    this.setState({
+      isDropdownShown: false,
+    });
+  componentDidMount() {
+    window.addEventListener('click', this.onWindowClick);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
+  }
+  render() {
+    const {
+      withValueOnly,
+      elasticMapping,
+      extendedMapping,
+      aggregations,
+      onFieldSelect = () => {},
+    } = this.props;
+    const { currentValue, isDropdownShown } = this.state;
+    const filteredList = (withValueOnly
+      ? filterOutNonValue({ extendedMapping, aggregations })
+          .extendedMappingWithValue
+      : extendedMapping
+    )?.filter?.(
+      ({ displayName }) =>
+        displayName
+          .toLowerCase()
+          .indexOf(
+            (currentValue?.length ? currentValue : null)?.toLowerCase(),
+          ) > -1,
+    );
+    return (
+      <div className="filterWrapper">
+        <TextInput
+          icon={<SearchIcon />}
+          className="filterInput"
+          type="text"
+          placeholder="Filter"
+          value={currentValue}
+          onClick={e => {
+            this.setState({ isDropdownShown: true });
+            e.stopPropagation();
+          }}
+          onChange={e => this.setState({ currentValue: e.target.value })}
+        />
+        {filteredList?.length && isDropdownShown ? (
+          <div className={`resultList shown`}>
+            {filteredList?.map(({ displayName, field, ...rest }) => (
+              <div
+                key={field}
+                className="resultItem"
+                onClick={() => {
+                  onFieldSelect(field);
+                }}
+              >
+                <span className="title">{displayName}</span>
+                <span className="field">{field}</span>
+              </div>
+            ))}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
