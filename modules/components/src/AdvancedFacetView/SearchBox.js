dc811e0aeaeec4c780ac4c310dcbbcb0c9ed3f2b
Minh <hlminh2000@gmail.com>
2018-02-21 15:17:10 -0500

    improvements to the search box

--- modules/components/src/AdvancedFacetView/SearchBox.js
+++ modules/components/src/AdvancedFacetView/SearchBox.js
@@ -1,23 +1,89 @@
 import React from 'react';
+import SearchIcon from 'react-icons/lib/fa/search';
+import $ from 'jquery';
+
 import State from '../State';
 import TextInput from '../Input';
-import SearchIcon from 'react-icons/lib/fa/search';
-
 import { filterOutNonValue } from './utils.js';
 
 export default class extends React.Component {
-  state = { currentValue: '', isDropdownShown: false };
+  state = { currentValue: '', isDropdownShown: false, highlightedField: null };
+  dropdownRefs = {};
+
   onWindowClick = e =>
     this.setState({
       isDropdownShown: false,
+      highlightedField: null,
     });
-  componentDidMount() {
-    window.addEventListener('click', this.onWindowClick);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('click', this.onWindowClick);
-  }
-  render() {
+
+  handleKeyPress = e => {
+    const filteredList = this.getFilteredList();
+    const { highlightedField } = this.state;
+    const { extendedMapping, onFieldSelect = () => {} } = this.props;
+    const highlightedAgg = extendedMapping.find(
+      ({ field, displayName }) => field === highlightedField,
+    );
+
+    // enter
+    if (e.keyCode === 13) {
+      this.setState({
+        isDropdownShown: false,
+        highlightedField: null,
+        ...(highlightedAgg
+          ? {
+              currentValue: highlightedAgg.displayName,
+            }
+          : {}),
+      });
+      onFieldSelect(highlightedField);
+    }
+
+    // up
+    if (e.keyCode === 38) {
+      const newHighlightedField =
+        filteredList?.[
+          Math.max(
+            (filteredList?.map(agg => agg.field).indexOf(highlightedField) ||
+              0) - 1,
+            0,
+          )
+        ]?.field || null;
+      this.setState(
+        {
+          highlightedField: newHighlightedField,
+        },
+        () =>
+          this.dropdownRefs[newHighlightedField]?.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+          }),
+      );
+    }
+
+    // down
+    if (e.keyCode === 40) {
+      const newHighlightedField =
+        filteredList?.[
+          Math.min(
+            (filteredList?.map(agg => agg.field).indexOf(highlightedField) ||
+              0) + 1,
+            filteredList.length - 1,
+          )
+        ]?.field || null;
+      this.setState(
+        {
+          highlightedField: newHighlightedField,
+        },
+        () =>
+          this.dropdownRefs[newHighlightedField]?.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+          }),
+      );
+    }
+  };
+
+  getFilteredList = () => {
     const {
       withValueOnly,
       elasticMapping,
@@ -25,8 +91,8 @@ export default class extends React.Component {
       aggregations,
       onFieldSelect = () => {},
     } = this.props;
-    const { currentValue, isDropdownShown } = this.state;
-    const filteredList = (withValueOnly
+    const { currentValue, isDropdownShown, highlightedField } = this.state;
+    return (withValueOnly
       ? filterOutNonValue({ extendedMapping, aggregations })
           .extendedMappingWithValue
       : extendedMapping
@@ -38,6 +104,26 @@ export default class extends React.Component {
             (currentValue?.length ? currentValue : null)?.toLowerCase(),
           ) > -1,
     );
+  };
+
+  componentDidMount() {
+    window.addEventListener('click', this.onWindowClick);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
+  }
+
+  render() {
+    const {
+      withValueOnly,
+      elasticMapping,
+      extendedMapping,
+      aggregations,
+      onFieldSelect = () => {},
+    } = this.props;
+    const { currentValue, isDropdownShown, highlightedField } = this.state;
+    const filteredList = this.getFilteredList();
     return (
       <div className="filterWrapper">
         <TextInput
@@ -50,14 +136,25 @@ export default class extends React.Component {
             this.setState({ isDropdownShown: true });
             e.stopPropagation();
           }}
-          onChange={e => this.setState({ currentValue: e.target.value })}
+          onChange={e =>
+            this.setState({
+              isDropdownShown: true,
+              currentValue: e.target.value,
+              highlightedField: null,
+            })
+          }
+          onKeyDown={e => this.handleKeyPress(e)}
         />
         {filteredList?.length && isDropdownShown ? (
           <div className={`resultList shown`}>
             {filteredList?.map(({ displayName, field, ...rest }) => (
               <div
                 key={field}
-                className="resultItem"
+                ref={el => (this.dropdownRefs[field] = el)}
+                onMouseEnter={e => this.setState({ highlightedField: field })}
+                className={`resultItem ${
+                  highlightedField === field ? 'highlighted' : ''
+                }`}
                 onClick={() => {
                   this.setState({ currentValue: displayName });
                   onFieldSelect(field);
