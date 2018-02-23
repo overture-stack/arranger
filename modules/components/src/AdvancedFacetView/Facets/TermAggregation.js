9d0870f53331ba78a199978ceb52adafbf3cd1c3
Minh <hlminh2000@gmail.com>
2018-02-23 12:27:57 -0500

    some responsiveness

--- modules/components/src/AdvancedFacetView/Facets/TermAggregation.js
+++ modules/components/src/AdvancedFacetView/Facets/TermAggregation.js
@@ -2,24 +2,30 @@ import React from 'react';
 import TermAggs from '../../Aggs/TermAgg';
 import { inCurrentSQON, replaceSQON, toggleSQON } from '../../SQONView/utils';
 
-export default ({ aggType, aggProps, title, onValueChange, sqon, path }) => (
-  <TermAggs
-    key={title}
-    collapsible={false}
-    handleValueClick={({ bucket }) => {
-      onValueChange({
-        value: bucket.key,
-      });
-    }}
-    field={path}
-    buckets={aggProps ? aggProps.buckets || [] : []}
-    displayName={title}
-    isActive={d => {
-      return inCurrentSQON({
-        value: d.value,
-        dotField: d.field,
-        currentSQON: sqon,
-      });
-    }}
-  />
-);
+export default ({ aggType, aggProps, title, onValueChange, sqon, path }) => {
+  const buckets = aggProps ? aggProps.buckets || [] : [];
+  const hasLongValues = true && buckets.find(({ key }) => key.length > 50);
+  return (
+    <div className={hasLongValues ? 'hasLongValues' : 'hasShortValues'}>
+      <TermAggs
+        key={title}
+        collapsible={false}
+        handleValueClick={({ bucket }) => {
+          onValueChange({
+            value: bucket.key,
+          });
+        }}
+        field={path}
+        buckets={buckets}
+        displayName={title}
+        isActive={d => {
+          return inCurrentSQON({
+            value: d.value,
+            dotField: d.field,
+            currentSQON: sqon,
+          });
+        }}
+      />
+    </div>
+  );
+};
