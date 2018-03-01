46c23a53db9a4559f2d756b23fa60a8155f33fe5
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-03-01 15:55:18 -0500

    🐛  fix nested fields not being picked up if not top level

--- modules/mapping-utils/src/getNestedFields.js
+++ modules/mapping-utils/src/getNestedFields.js
@@ -1,17 +1,15 @@
-import { flattenDeep } from 'lodash'
+import { flattenDeep } from 'lodash';
 
 let getNestedFields = (mapping, parent = '') => {
   return flattenDeep(
-    Object.entries(mapping)
-      .filter(([, metadata]) => metadata.type === 'nested')
-      .map(([field, metadata]) => [
-        parent ? `${parent}.${field}` : field,
-        ...getNestedFields(
-          metadata.properties,
-          parent ? `${parent}.${field}` : field,
-        ),
-      ]),
-  )
-}
+    Object.entries(mapping || {}).map(([field, metadata]) => {
+      const fullPath = parent ? `${parent}.${field}` : field;
+      return [
+        metadata.type === 'nested' && fullPath,
+        ...getNestedFields(metadata.properties, fullPath),
+      ];
+    }),
+  ).filter(Boolean);
+};
 
-export default getNestedFields
+export default getNestedFields;
--- modules/mapping-utils/test/getNestedFields.test.js
+++ modules/mapping-utils/test/getNestedFields.test.js
@@ -32,3 +32,27 @@ test('getNestedFields', () => {
   expect(actual.length).toBe(expected.length);
   expected.forEach((field, i) => expect(field).toEqual(actual[i]));
 });
+
+test('getNestedFields deep nested', () => {
+  let actual = getNestedFields({
+    family: {
+      properties: {
+        family_members: {
+          type: 'nested',
+          properties: {
+            available_data_types: {
+              type: 'keyword',
+            },
+            created_at: {
+              type: 'date',
+            },
+          },
+        },
+      },
+    },
+  });
+  let expected = [ 'family.family_members' ];
+
+  expect(actual.length).toBe(expected.length);
+  expected.forEach((field, i) => expect(field).toEqual(actual[i]));
+});
