610c7a8bd9a3333f37395a7e402a282971bded1d
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-03-02 12:02:43 -0500

    ✨ eslint all the things

--- modules/middleware/.eslintrc.js
+++ modules/middleware/.eslintrc.js
@@ -1,15 +1,1 @@
-module.exports = {
-  extends: ['prettier', 'prettier/flowtype', 'plugin:flowtype/recommended'],
-  plugins: ['flowtype', 'prettier'],
-  rules: {
-    'prettier/prettier': [1, { trailingComma: 'all', singleQuote: true }],
-    'flowtype/define-flow-type': 1,
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      modules: true,
-    },
-  },
-};
+module.exports = require('../../.eslintrc.js');
