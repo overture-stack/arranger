380e163fcd2440c35fc01df4a9fb258b62501877
Alex Wilmer <djazium@gmail.com>
2018-03-02 15:25:12 -0500

    🐛 transcription fix for global_aggs

---
+++ modules/middleware/test/global_aggs.test.js
@@ -0,0 +1,66 @@
+import AggregationProcessor from '../src/aggregations.js';
+
+let input = {
+  type: {
+    index: 'file',
+    es_type: 'file',
+    name: 'file',
+  },
+  fields: ['acl'],
+  nested_fields: [],
+  graphql_fields: {
+    acl: {
+      buckets: {
+        key: {},
+        doc_count: {},
+      },
+    },
+  },
+  args: {
+    filters: {
+      op: 'and',
+      content: [{ op: 'in', content: { field: 'acl', value: ['phs000178'] } }],
+    },
+    aggregations_filter_themselves: false,
+  },
+};
+
+let expected = {
+  query: {
+    bool: {
+      must: [
+        {
+          terms: {
+            acl: ['phs000178'],
+            boost: 0,
+          },
+        },
+      ],
+    },
+  },
+  aggs: {
+    'acl:global': {
+      global: {},
+      aggs: {
+        'acl:filtered': {
+          filter: {
+            match_all: {},
+          },
+          aggs: {
+            acl: {
+              terms: {
+                field: 'acl',
+                size: 100,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+test('build_aggregations should handle nested aggregations', () => {
+  const actual = new AggregationProcessor().buildAggregations(input);
+  expect(actual).toEqual(expected);
+});
--- modules/server/src/utils/config.js
+++ modules/server/src/utils/config.js
@@ -2,3 +2,4 @@ export const PORT = process.env.PORT || 5050;
 export const ES_HOST = process.env.ES_HOST;
 export const PROJECT_ID = process.env.PROJECT_ID;
 export const PING_MS = process.env.PING_MS || 2200;
+export const ES_LOG = process.env.ES_LOG?.split?.(',') || 'error';
