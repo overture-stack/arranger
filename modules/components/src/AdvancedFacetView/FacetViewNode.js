5099a4ea40def4a8e84710d1b9f0d91681bfd5f3
Minh <hlminh2000@gmail.com>
2018-03-06 11:40:28 -0500

    puts FacetViewNode back as functional stateless

--- modules/components/src/AdvancedFacetView/FacetViewNode.js
+++ modules/components/src/AdvancedFacetView/FacetViewNode.js
@@ -5,70 +5,65 @@ import State from '../State';
 
 const esTypeToAggType = esType => esToAggTypeMap[esType];
 
-class FacetViewNode extends React.Component {
-  render() {
-    const {
-      title,
-      id,
-      children,
-      path,
-      type: esType,
-      aggregations,
-      onValueChange,
-      sqon = {},
-      constructEntryId = ({ field, value }) =>
-        value ? `${field}---${value}` : field,
-      depth,
-      searchboxSelectionObservable,
-    } = this.props;
-    return (
-      <div
-        className={`facetViewNode depth_${depth}`}
-        id={constructEntryId({ field: path.split('.').join('__') })}
-      >
-        {!children && ( // if there is no children FacetWrapper is brought in to render an agg corresponding to the type
-          <div className={`aggWrapper depth_${depth}`}>
-            {!esType && <div className="facetTitle">{title}</div>}
-            <FacetWrapper
-              sqon={sqon}
-              title={title}
-              aggType={esTypeToAggType(esType)}
-              aggProps={aggregations[path]}
-              path={path}
-              constructEntryId={({ value }) =>
-                constructEntryId({ field: path.split('.').join('__'), value })
-              }
-              searchboxSelectionObservable={searchboxSelectionObservable}
-              onValueChange={({ value }) =>
-                onValueChange({
-                  value,
-                  path: path,
-                  esType: esType,
-                  aggType: esTypeToAggType(esType),
-                })
-              }
-            />
-          </div>
-        )}
-        {children && ( // if there are children, another nested layer is recursively rendered
-          <div className={`aggWrapper depth_${depth}`}>
-            <div className="facetTitle">{title}</div>
-            {children.map(childNode => (
-              <FacetViewNode
-                depth={depth + 1}
-                sqon={sqon}
-                key={childNode.path}
-                aggregations={aggregations}
-                onValueChange={onValueChange}
-                searchboxSelectionObservable={searchboxSelectionObservable}
-                {...childNode}
-              />
-            ))}
-          </div>
-        )}
+const FacetViewNode = ({
+  title,
+  id,
+  children,
+  path,
+  type: esType,
+  aggregations,
+  onValueChange,
+  sqon = {},
+  constructEntryId = ({ field, value }) =>
+    value ? `${field}---${value}` : field,
+  depth,
+  searchboxSelectionObservable,
+}) => (
+  <div
+    className={`facetViewNode depth_${depth}`}
+    id={constructEntryId({ field: path.split('.').join('__') })}
+  >
+    {!children && ( // if there is no children FacetWrapper is brought in to render an agg corresponding to the type
+      <div className={`aggWrapper depth_${depth}`}>
+        {!esType && <div className="facetTitle">{title}</div>}
+        <FacetWrapper
+          sqon={sqon}
+          title={title}
+          aggType={esTypeToAggType(esType)}
+          aggProps={aggregations[path]}
+          path={path}
+          constructEntryId={({ value }) =>
+            constructEntryId({ field: path.split('.').join('__'), value })
+          }
+          searchboxSelectionObservable={searchboxSelectionObservable}
+          onValueChange={({ value }) =>
+            onValueChange({
+              value,
+              path: path,
+              esType: esType,
+              aggType: esTypeToAggType(esType),
+            })
+          }
+        />
       </div>
-    );
-  }
-}
-// '\203A'
+    )}
+    {children && ( // if there are children, another nested layer is recursively rendered
+      <div className={`aggWrapper depth_${depth}`}>
+        <div className="facetTitle">{title}</div>
+        {children.map(childNode => (
+          <FacetViewNode
+            depth={depth + 1}
+            sqon={sqon}
+            key={childNode.path}
+            aggregations={aggregations}
+            onValueChange={onValueChange}
+            searchboxSelectionObservable={searchboxSelectionObservable}
+            {...childNode}
+          />
+        ))}
+      </div>
+    )}
+  </div>
+);
+
 export default FacetViewNode;
