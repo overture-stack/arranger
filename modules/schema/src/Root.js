6172a097baf523d8b177e05d432f25c42ac60009
Alex Wilmer <djazium@gmail.com>
2018-03-07 11:50:58 -0500

    🐛 types is 'entries' in resolvers

--- modules/schema/src/Root.js
+++ modules/schema/src/Root.js
@@ -106,7 +106,8 @@ export let resolvers = ({ types, rootTypes, scalarTypes }) => {
         { es, projectId, io },
       ) => {
         // TODO: validate / make proper input type
-        const type = types.find(t => t.name === graphqlField);
+        const type = types.find(([, t]) => t.name === graphqlField);
+
         await es.create({
           index: `${type.indexPrefix}-aggs-state`,
           type: `${type.indexPrefix}-aggs-state`,
@@ -137,7 +138,7 @@ export let resolvers = ({ types, rootTypes, scalarTypes }) => {
         { es, projectId, io },
       ) => {
         // TODO: validate / make proper input type
-        const type = types.find(t => t.name === graphqlField);
+        const type = types.find(([, t]) => t.name === graphqlField);
         await es.create({
           index: `${type.indexPrefix}-columns-state`,
           type: `${type.indexPrefix}-columns-state`,
