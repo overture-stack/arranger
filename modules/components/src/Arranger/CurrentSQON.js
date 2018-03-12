ee9161bc6b6789380ff83c6eb7462cb65e6823e7
Minh <hlminh2000@gmail.com>
2018-03-12 11:55:52 -0400

    fetches extended mapping to feed into CurrentSQON

--- modules/components/src/Arranger/CurrentSQON.js
+++ modules/components/src/Arranger/CurrentSQON.js
@@ -1,25 +1,52 @@
 import React from 'react';
 
 import SQONView, { Value, Bubble, Field } from '../SQONView';
+import State from '../State';
+import api from '../utils/api';
 
-const CurrentSQON = ({ sqon, setSQON, extendedMapping }) => {
+const fetchExtendedMapping = ({ graphqlField, projectId }) =>
+  api({
+    endpoint: `/${projectId}/graphql`,
+    body: {
+      query: `
+      {
+        ${graphqlField}{
+          extended
+        }
+      }
+    `,
+    },
+  })
+    .then(response => ({
+      extendedMapping: response.data[graphqlField].extended,
+    }))
+    .catch(err => console.log(err));
+
+const CurrentSQON = ({ sqon, setSQON, graphqlField, projectId }) => {
   return (
-    <SQONView
-      sqon={sqon}
-      FieldCrumb={({ field, ...props }) => (
-        <Field {...{ field, ...props }}>
-          {extendedMapping?.find(e => e.field === field)?.displayName || field}
-        </Field>
-      )}
-      ValueCrumb={({ value, nextSQON, ...props }) => (
-        <Value onClick={() => setSQON(nextSQON)} {...props}>
-          {value}
-        </Value>
-      )}
-      Clear={({ nextSQON }) => (
-        <Bubble className="sqon-clear" onClick={() => setSQON(nextSQON)}>
-          Clear
-        </Bubble>
+    <State
+      initial={{ extendedMapping: null }}
+      async={() => fetchExtendedMapping({ graphqlField, projectId })}
+      render={({ extendedMapping }) => (
+        <SQONView
+          sqon={sqon}
+          FieldCrumb={({ field, ...props }) => (
+            <Field {...{ field, ...props }}>
+              {extendedMapping?.find(e => e.field === field)?.displayName ||
+                field}
+            </Field>
+          )}
+          ValueCrumb={({ value, nextSQON, ...props }) => (
+            <Value onClick={() => setSQON(nextSQON)} {...props}>
+              {value}
+            </Value>
+          )}
+          Clear={({ nextSQON }) => (
+            <Bubble className="sqon-clear" onClick={() => setSQON(nextSQON)}>
+              Clear
+            </Bubble>
+          )}
+        />
       )}
     />
   );
