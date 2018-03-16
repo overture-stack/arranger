53e0c8a11b608f4f2d46c47b8c29ffa45041eefa
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-03-16 15:57:54 -0400

    skip set tests

--- modules/middleware/test/buildQuery/buildQuerySetId.test.js
+++ modules/middleware/test/buildQuery/buildQuerySetId.test.js
@@ -1,79 +1,79 @@
 import buildQuery from '../../src/buildQuery';
-test('buildQuery nested', () => {
+test.skip('buildQuery sets', () => {
   const nestedFields = ['files', 'files.foo'];
-  // cases
+
   const tests = [
-    // {
-    //   input: {
-    //     nestedFields: ['files', 'files.foo'],
-    //     filters: {
-    //       content: { field: 'cases.case_id', value: ['set_id:aaa'] },
-    //       op: 'in',
-    //     },
-    //   },
-    //   output: {
-    //     terms: {
-    //       case_id: {
-    //         index: 'case_set',
-    //         type: 'case_set',
-    //         id: 'aaa',
-    //         path: 'ids',
-    //       },
-    //       boost: 0,
-    //     },
-    //   },
-    // },
-    // {
-    //   input: {
-    //     nestedFields: ['files', 'files.foo'],
-    //     filters: {
-    //       content: { field: 'ssms.ssm_id', value: ['set_id:aaa'] },
-    //       op: 'in',
-    //     },
-    //   },
-    //   output: {
-    //     terms: {
-    //       'gene.ssm.ssm_id': {
-    //         index: 'ssm_set',
-    //         type: 'ssm_set',
-    //         id: 'aaa',
-    //         path: 'ids',
-    //       },
-    //       boost: 0,
-    //     },
-    //   },
-    // },
-    // {
-    //   input: {
-    //     nestedFields: ['files', 'files.foo'],
-    //     filters: {
-    //       content: { field: 'files.file_id', value: ['set_id:aaa'] },
-    //       op: 'in',
-    //     },
-    //   },
-    //   output: {
-    //     nested: {
-    //       path: 'files',
-    //       query: {
-    //         bool: {
-    //           must: [
-    //             {
-    //               terms: {
-    //                 'files.file_id': {
-    //                   index: 'file_set',
-    //                   type: 'file_set',
-    //                   id: 'aaa',
-    //                   path: 'ids',
-    //                 },
-    //                 boost: 0,
-    //               },
-    //             },
-    //           ],
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
+    {
+      input: {
+        nestedFields,
+        filters: {
+          content: { field: 'case_id', value: ['set_id:aaa'] },
+          op: 'in',
+        },
+      },
+      output: {
+        terms: {
+          case_id: {
+            index: 'case_set',
+            type: 'case_set',
+            id: 'aaa',
+            path: 'ids',
+          },
+          boost: 0,
+        },
+      },
+    },
+    {
+      input: {
+        nestedFields,
+        filters: {
+          content: { field: 'ssms.ssm_id', value: ['set_id:aaa'] },
+          op: 'in',
+        },
+      },
+      output: {
+        terms: {
+          'gene.ssm.ssm_id': {
+            index: 'ssm_set',
+            type: 'ssm_set',
+            id: 'aaa',
+            path: 'ids',
+          },
+          boost: 0,
+        },
+      },
+    },
+    {
+      input: {
+        nestedFields,
+        filters: {
+          content: { field: 'files.file_id', value: ['set_id:aaa'] },
+          op: 'in',
+        },
+      },
+      output: {
+        nested: {
+          path: 'files',
+          query: {
+            bool: {
+              must: [
+                {
+                  terms: {
+                    'files.file_id': {
+                      index: 'file_set',
+                      type: 'file_set',
+                      id: 'aaa',
+                      path: 'ids',
+                    },
+                    boost: 0,
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
   ];
 
   tests.forEach(({ input, output }) => {
