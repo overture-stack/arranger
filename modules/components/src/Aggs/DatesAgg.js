3375ad07248515b890a49ea1d6b1767785f245e9
Minh <hlminh2000@gmail.com>
2018-03-19 17:37:57 -0400

    adds custom input

--- modules/components/src/Aggs/DatesAgg.js
+++ modules/components/src/Aggs/DatesAgg.js
@@ -158,6 +158,16 @@ class DatesAgg extends Component {
             <span className={`arrow ${isCollapsed && 'collapsed'}`} />
           )}
         </div>
+        <div>
+          <input value={rangeToRender.startDate.format('YY/MM/DD')} />
+          <svg
+            class="DayPickerNavigation_svg__horizontal DayPickerNavigation_svg__horizontal_1"
+            viewBox="0 0 1000 1000"
+          >
+            <path d="M694.4 242.4l249.1 249.1c11 11 11 21 0 32L694.4 772.7c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210.1-210.1H67.1c-13 0-23-10-23-23s10-23 23-23h805.4L662.4 274.5c-21-21.1 11-53.1 32-32.1z" />
+          </svg>
+          <input value={rangeToRender.endDate.format('YY/MM/DD')} />
+        </div>
         <DayPickerRangeController
           numberOfMonths={2}
           focusedInput={this.state.focusedInput}
