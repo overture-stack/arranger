5154bf3d1a05d95746fcac3389f22e43c7350fd1
Minh <hlminh2000@gmail.com>
2018-03-22 15:40:18 -0400

    refactors to handle state in class

--- modules/components/src/Aggs/DatesAgg.js
+++ modules/components/src/Aggs/DatesAgg.js
@@ -5,7 +5,6 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import { maxBy, minBy, sumBy } from 'lodash';
 import { css } from 'emotion';
-import Component from 'react-component-component';
 import {
   inCurrentSQON,
   replaceSQON,
@@ -60,233 +59,232 @@ const Arrow = ({ style }) => (
 );
 
 class DatesAgg extends React.Component {
-  // needs ref...
+  constructor(props) {
+    super(props);
+    this.state = {
+      localRange: { ...this.getInitialRange(props) }, // the moment objects passed into DayPickerRangeController
+      inputRangeValues: {
+        // the strings to render in the inputs
+        startDate: null,
+        endDate: null,
+      },
+      focusedInput: null, // must be set with this.setInputFocus to bind with dom state
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      localRange: { ...this.getInitialRange(nextProps) },
+    });
+  }
+
+  getInitialRange = ({
+    buckets = [],
+    startDateFromSqon = () => null,
+    endDateFromSqon = () => null,
+  }) => {
+    const bucketWithMoment = buckets.map(({ key_as_string, ...rest }) => ({
+      ...rest,
+      key_as_string,
+      moment: bucketDateToMoment(key_as_string),
+    }));
+    const minBucket = minBy(bucketWithMoment, ({ moment }) => moment.valueOf());
+    const maxBucket = maxBy(bucketWithMoment, ({ moment }) => moment.valueOf());
+
+    const startFromSqon = startDateFromSqon({
+      getDateFromSqon: this.getDateFromSqon(START_DATE_INPUT),
+    });
+    const endFromSqon = endDateFromSqon({
+      getDateFromSqon: this.getDateFromSqon(END_DATE_INPUT),
+    });
+    return {
+      startDate:
+        (startFromSqon && bucketDateToMoment(startFromSqon)) ||
+        minBucket?.moment,
+      endDate:
+        (startFromSqon && bucketDateToMoment(endFromSqon)) || maxBucket?.moment,
+    };
+  };
+
+  setInputFocus = focusedInput => {
+    this.setState({ focusedInput }, () => {
+      if (focusedInput === START_DATE_INPUT) {
+        this.startDateInput.focus();
+      } else if (focusedInput === END_DATE_INPUT) {
+        this.endDateInput.focus();
+      } else {
+        this.startDateInput.blur();
+        this.endDateInput.blur();
+      }
+    });
+  };
+
+  getDateFromSqon = dateToGet => sqon => {
+    const { field } = this.props;
+    return sqon?.content
+      ?.filter(({ content: { field: sqonField } }) => {
+        return sqonField === field;
+      })
+      ?.find(({ op }) => op === (dateToGet === START_DATE_INPUT ? '>=' : '<='))
+      ?.content.value;
+  };
+
+  handleInputValueChange = ({ value, input }) => {
+    const newMoment = inputDateToMoment(value);
+    const isValidInputDateString =
+      newMoment.isValid() &&
+      sumBy(value.split('/'), str => str.length === 2) === 3;
+    if (!isValidInputDateString) {
+      this.setState({
+        inputRangeValues: { ...inputRangeValues, [input]: value },
+      });
+    } else {
+      this.setState({
+        inputRangeValues: {},
+        localRange: {
+          ...localRange,
+          [input]: newMoment,
+        },
+      });
+    }
+  };
+
+  onDatesSet = ({ startDate, endDate }) => {
+    const {
+      field = '',
+      handleDateChange = ({ generateNextSQON }) => null,
+    } = this.props;
+    if (startDate && endDate) {
+      handleDateChange({
+        generateNextSQON: sqon =>
+          replaceSQON(
+            {
+              op: 'and',
+              content: [
+                {
+                  op: '>=',
+                  content: {
+                    field,
+                    value: momentToBucketDate(startDate.startOf('day')),
+                  },
+                },
+                {
+                  op: '<=',
+                  content: {
+                    field,
+                    value: momentToBucketDate(endDate.endOf('day')),
+                  },
+                },
+              ],
+            },
+            sqon,
+          ),
+      });
+    }
+  };
 
   render() {
     const {
-      field = '',
       displayName = 'Unnamed Field',
-      buckets = [],
       collapsible = true,
-      handleDateChange = ({ generateNextSQON }) => null,
-      startDateFromSqon = () => null,
-      endDateFromSqon = () => null,
       datePickerPosition = 'BOTTOM_LEFT',
+      numberOfMonths = 2,
     } = this.props;
 
-    const onDatesSet = ({ startDate, endDate }) => {
-      if (startDate && endDate) {
-        handleDateChange({
-          generateNextSQON: sqon =>
-            replaceSQON(
-              {
-                op: 'and',
-                content: [
-                  {
-                    op: '>=',
-                    content: {
-                      field,
-                      value: momentToBucketDate(startDate.startOf('day')),
-                    },
-                  },
-                  {
-                    op: '<=',
-                    content: {
-                      field,
-                      value: momentToBucketDate(endDate.endOf('day')),
-                    },
-                  },
-                ],
-              },
-              sqon,
-            ),
-        });
-      }
-    };
+    const { localRange, inputRangeValues, focusedInput } = this.state;
 
-    const getDateFromSqon = dateToGet => sqon =>
-      sqon?.content
-        ?.filter(({ content: { field: sqonField } }) => {
-          return sqonField === field;
-        })
-        ?.find(
-          ({ op }) => op === (dateToGet === START_DATE_INPUT ? '>=' : '<='),
-        )?.content.value;
+    const initialRange = this.getInitialRange(this.props);
 
-    const getInitialRange = () => {
-      const bucketWithMoment = buckets.map(({ key_as_string, ...rest }) => ({
-        ...rest,
-        key_as_string,
-        moment: bucketDateToMoment(key_as_string),
-      }));
-      const minBucket = minBy(bucketWithMoment, ({ moment }) =>
-        moment.valueOf(),
+    const getInitialVisibleMonth = () => {
+      return (
+        focusedInput &&
+        (focusedInput === START_DATE_INPUT
+          ? initialRange.startDate || Moment()
+          : initialRange.endDate || Moment())
       );
-      const maxBucket = maxBy(bucketWithMoment, ({ moment }) =>
-        moment.valueOf(),
-      );
-
-      const startFromSqon = startDateFromSqon({
-        getDateFromSqon: getDateFromSqon(START_DATE_INPUT),
-      });
-      const endFromSqon = endDateFromSqon({
-        getDateFromSqon: getDateFromSqon(END_DATE_INPUT),
-      });
-      return {
-        startDate:
-          (startFromSqon && bucketDateToMoment(startFromSqon)) ||
-          minBucket?.moment,
-        endDate:
-          (startFromSqon && bucketDateToMoment(endFromSqon)) ||
-          maxBucket?.moment,
-      };
     };
-
-    const initialRange = getInitialRange();
 
     return (
       <AggsWrapper {...{ displayName, collapsible }}>
-        <Component
-          initialState={{
-            localRange: { ...initialRange }, // the moment objects passed into DayPickerRangeController
-            inputRangeValues: {
-              // the strings to render in the inputs
-              startDate: null,
-              endDate: null,
-            },
-            focusedInput: null,
-          }}
-        >
-          {({
-            state: { localRange, inputRangeValues, focusedInput },
-            setState,
-          }) => {
-            const handleInputValueChange = ({ value, input }) => {
-              const newMoment = inputDateToMoment(value);
-              const isValidInputDateString =
-                newMoment.isValid() &&
-                sumBy(value.split('/'), str => str.length === 2) === 3;
-              if (!isValidInputDateString) {
-                setState({
-                  inputRangeValues: { ...inputRangeValues, [input]: value },
-                });
-              } else {
-                setState({
-                  inputRangeValues: {},
-                  localRange: {
-                    ...localRange,
-                    [input]: newMoment,
-                  },
-                });
-              }
-            };
-            const setInputFocus = focusedInput => {
-              setState({ focusedInput }, () => {
-                if (focusedInput === START_DATE_INPUT) {
-                  this.startDateInput.focus();
-                } else if (focusedInput === END_DATE_INPUT) {
-                  this.endDateInput.focus();
-                } else {
-                  this.startDateInput.blur();
-                  this.endDateInput.blur();
-                }
-              });
-            };
-            const getInitialVisibleMonth = () =>
-              focusedInput &&
-              (focusedInput === START_DATE_INPUT
-                ? initialRange.startDate || Moment()
-                : initialRange.endDate || Moment());
-            return (
-              <div className={`inputRow`}>
-                <input
-                  ref={el => (this.startDateInput = el)}
-                  onFocus={() => setInputFocus(START_DATE_INPUT)}
-                  className={`dateInput`}
-                  value={
-                    inputRangeValues.startDate
-                      ? inputRangeValues.startDate
-                      : momentToInputDate(localRange.startDate)
-                  }
-                  onChange={e =>
-                    handleInputValueChange({
-                      value: e.target.value,
-                      input: START_DATE_INPUT,
-                    })
-                  }
-                />
-                <div style={{ padding: 5 }}>
-                  <Arrow />
+        <div className={`inputRow`}>
+          <input
+            ref={el => (this.startDateInput = el)}
+            onFocus={() => this.setInputFocus(START_DATE_INPUT)}
+            className={`dateInput`}
+            value={
+              inputRangeValues.startDate
+                ? inputRangeValues.startDate
+                : momentToInputDate(localRange.startDate)
+            }
+            onChange={e =>
+              this.handleInputValueChange({
+                value: e.target.value,
+                input: START_DATE_INPUT,
+              })
+            }
+          />
+          <div style={{ padding: 5 }}>
+            <Arrow />
+          </div>
+          <input
+            ref={el => (this.endDateInput = el)}
+            onFocus={() => this.setInputFocus(END_DATE_INPUT)}
+            className={`dateInput`}
+            value={
+              inputRangeValues.endDate
+                ? inputRangeValues.endDate
+                : momentToInputDate(localRange.endDate)
+            }
+            onChange={e =>
+              this.handleInputValueChange({
+                value: e.target.value,
+                input: END_DATE_INPUT,
+              })
+            }
+          />
+          {focusedInput && (
+            <div className={DATE_PICKER_POSITIONS[datePickerPosition]}>
+              <DayPickerRangeController
+                focusedInput={focusedInput}
+                numberOfMonths={numberOfMonths}
+                onFocusChange={focusedInput => this.setInputFocus(focusedInput)}
+                initialVisibleMonth={getInitialVisibleMonth}
+                startDate={localRange.startDate}
+                endDate={localRange.endDate}
+                isOutsideRange={() => false}
+                onDatesChange={range => this.setState({ localRange: range })}
+                hideKeyboardShortcutsPanel
+                keepOpenOnDateSelect
+              />
+              <div className={`calendarNavbar`}>
+                <div
+                  className={`cancelButton`}
+                  onClick={() => {
+                    this.setState({
+                      inputRangeValues: {},
+                      localRange: { ...initialRange },
+                    });
+                    this.setInputFocus(null);
+                  }}
+                >
+                  cancel
                 </div>
-                <input
-                  ref={el => (this.endDateInput = el)}
-                  onFocus={() => setInputFocus(END_DATE_INPUT)}
-                  className={`dateInput`}
-                  value={
-                    inputRangeValues.endDate
-                      ? inputRangeValues.endDate
-                      : momentToInputDate(localRange.endDate)
-                  }
-                  onChange={e =>
-                    handleInputValueChange({
-                      value: e.target.value,
-                      input: END_DATE_INPUT,
-                    })
-                  }
-                />
-                {focusedInput && (
-                  <div className={DATE_PICKER_POSITIONS[datePickerPosition]}>
-                    <DayPickerRangeController
-                      {...{
-                        focusedInput,
-                        numberOfMonths: 2,
-                        onFocusChange: focusedInput =>
-                          setInputFocus(focusedInput),
-                        initialVisibleMonth: getInitialVisibleMonth,
-                        startDate: localRange.startDate,
-                        endDate: localRange.endDate,
-                        isOutsideRange: () => false,
-                        onDatesChange: range => {
-                          setState({
-                            localRange: range,
-                          });
-                        },
-                      }}
-                      hideKeyboardShortcutsPanel
-                      keepOpenOnDateSelect
-                    />
-                    <div className={`calendarNavbar`}>
-                      <div
-                        className={`cancelButton`}
-                        onClick={() => {
-                          setState({
-                            inputRangeValues: {},
-                            localRange: { ...initialRange },
-                          });
-                          setInputFocus(null);
-                        }}
-                      >
-                        cancel
-                      </div>
-                      <div
-                        className={`submitButton`}
-                        onClick={() => {
-                          onDatesSet(localRange);
-                          setInputFocus(null);
-                          setState({
-                            inputRangeValues: {},
-                          });
-                        }}
-                      >
-                        Apply
-                      </div>
-                    </div>
-                  </div>
-                )}
+                <div
+                  className={`submitButton`}
+                  onClick={() => {
+                    this.onDatesSet(localRange);
+                    this.setInputFocus(null);
+                    this.setState({
+                      inputRangeValues: {},
+                    });
+                  }}
+                >
+                  Apply
+                </div>
               </div>
-            );
-          }}
-        </Component>
+            </div>
+          )}
+        </div>
       </AggsWrapper>
     );
   }
