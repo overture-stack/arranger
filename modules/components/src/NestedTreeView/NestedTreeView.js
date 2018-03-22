b323b7dca2931de30c6b025bdee3bc455c24cda8
Minh <hlminh2000@gmail.com>
2018-03-22 10:45:27 -0400

    implements indentation with cumulative padding rather than nested margins

--- modules/components/src/NestedTreeView/NestedTreeView.js
+++ modules/components/src/NestedTreeView/NestedTreeView.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactTreeView from './ReactTreeView';
+import { css } from 'emotion';
 import './style.css';
 
 const NestedTreeView = ({
   dataSource,
   depth = 0,
+  indentationPx = 10,
   onLeafSelect = () => {},
   selectedPath = '',
 }) =>
   dataSource.map(({ title, id, children, path }, i) => {
     const selectedPathArray =
       selectedPath?.split('.').filter(str => str.length) || [];
+    const indentationStyle = css`
+      padding-left: ${indentationPx * depth}px;
+    `;
     return children ? (
       <ReactTreeView
         key={path}
@@ -29,9 +34,9 @@ const NestedTreeView = ({
           </div>
         }
         defaultCollapsed={true}
-        itemClassName={`${
-          selectedPath === (id || title) ? 'selected' : ''
-        } ${depth === 0 && 'header'}`}
+        itemClassName={`${selectedPath === (id || title) ? 'selected' : ''} ${
+          depth === 0 ? 'header' : ''
+        } ${indentationStyle}`}
       >
         <NestedTreeView
           onLeafSelect={selectedPath => {
@@ -51,6 +56,7 @@ const NestedTreeView = ({
         className={`tree-view_children leaf
           ${depth === 0 ? 'header' : ''}
           ${selectedPath === (id || title) ? 'selected' : ''}
+          ${indentationStyle}
         `}
       >
         {title}
