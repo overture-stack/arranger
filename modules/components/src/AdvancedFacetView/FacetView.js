fd8e5fc706e2a1c96f04f334f4121e2ff8cb0c7f
Minh <hlminh2000@gmail.com>
2018-03-23 10:29:15 -0400

    structures data to render flat facet list

--- modules/components/src/AdvancedFacetView/FacetView.js
+++ modules/components/src/AdvancedFacetView/FacetView.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import FacetViewNode from './FacetViewNode';
 import { debounce } from 'lodash';
+import { css } from 'emotion';
 
 const serializeToDomId = path => path.split('.').join('__');
+
+const flattenDisplayTreeData = displayTreeData => {
+  return displayTreeData.reduce(
+    (acc, node) => [
+      ...acc,
+      ...(node.children ? flattenDisplayTreeData(node.children) : [node]),
+    ],
+    [],
+  );
+};
 
 export default class FacetView extends React.Component {
   scrollToPath = path => {
@@ -21,25 +32,42 @@ export default class FacetView extends React.Component {
       constructEntryId,
       searchboxSelectionObservable,
       valueCharacterLimit,
+      extendedMapping,
     } = this.props;
     return (
       <div className="facetView" ref={el => (this.root = el)}>
-        {displayTreeData.map(node => {
+        {flattenDisplayTreeData(displayTreeData).map(({ title, path }) => {
+          const metaData = extendedMapping.find(({ field }) => field === path);
+          const { type } = metaData || {};
+          const paths = path
+            .split('.')
+            .reduce(
+              (acc, node, i, paths) => [
+                ...acc,
+                [...paths.slice(0, i), node].join('.'),
+              ],
+              [],
+            );
+          const pathDisplayNames = paths.map(
+            path =>
+              extendedMapping.find(({ field }) => field === path)?.displayName,
+          );
           return (
-            <FacetViewNode
-              valueCharacterLimit={valueCharacterLimit}
-              searchboxSelectionObservable={searchboxSelectionObservable}
-              constructEntryId={constructEntryId}
-              depth={0}
-              sqon={sqon}
-              key={node.path}
-              path={node.path}
-              aggregations={aggregations}
-              onValueChange={({ value, path, esType, aggType }) =>
-                onValueChange({ value, path, esType, aggType })
-              }
-              {...node}
-            />
+            <div
+              key={path}
+              className={css`
+                padding: 10px;
+                border: solid 1px red;
+              `}
+            >
+              <div>{title}</div>
+              <div>
+                {pathDisplayNames
+                  .slice(0, pathDisplayNames.length - 1)
+                  .join(' >> ')}
+              </div>
+              <div>{type}</div>
+            </div>
           );
         })}
       </div>
