8bc305f0639be4222e2b2761ffb238edb50dd5ac
Minh <hlminh2000@gmail.com>
2018-03-23 14:29:38 -0400

    refactors

---
+++ modules/components/src/AdvancedFacetView/aggComponents.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { TermAgg, RangeAgg, BooleanAgg } from '../Aggs';
+import { currentFieldValue } from '../SQONView/utils';
+import { inCurrentSQON } from '../SQONView/utils';
+
+const composedTermAgg = ({ sqon, onValueChange, ...rest }) => (
+  <TermAgg
+    handleValueClick={({ generateNextSQON }) => {
+      onValueChange({ sqon: generateNextSQON(sqon) });
+    }}
+    isActive={d =>
+      inCurrentSQON({
+        value: d.value,
+        dotField: d.field,
+        currentSQON: sqon,
+      })
+    }
+    {...{ ...rest }}
+  />
+);
+
+const composedRangeAgg = ({ sqon, onValueChange, field, stats, ...rest }) => (
+  <RangeAgg
+    value={{
+      min:
+        currentFieldValue({ sqon, dotField: field, op: '>=' }) ||
+        stats?.min ||
+        0,
+      max:
+        currentFieldValue({ sqon, dotField: field, op: '<=' }) ||
+        stats?.max ||
+        0,
+    }}
+    handleChange={({ generateNextSQON }) =>
+      onValueChange({ sqon: generateNextSQON(sqon) })
+    }
+    {...{ ...rest, stats, field }}
+  />
+);
+
+const composedBooleanAgg = ({ sqon, onValueChange, ...rest }) => (
+  <BooleanAgg
+    isActive={d =>
+      inCurrentSQON({
+        value: d.value,
+        dotField: d.field,
+        currentSQON: sqon,
+      })
+    }
+    handleValueClick={({ generateNextSQON }) =>
+      onValueChange({ sqon: generateNextSQON(sqon) })
+    }
+    {...{ ...rest }}
+  />
+);
+
+export default {
+  keyword: composedTermAgg,
+  long: composedRangeAgg,
+  float: composedRangeAgg,
+  boolean: composedBooleanAgg,
+};
