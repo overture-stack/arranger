63cde84731fe14150fe56e88c227e1c0cf0ce5e2
Minh <hlminh2000@gmail.com>
2018-03-29 17:47:56 -0400

    implements loading screen

--- modules/components/src/AdvancedFacetView/index.js
+++ modules/components/src/AdvancedFacetView/index.js
@@ -18,6 +18,19 @@ import {
 } from './utils.js';
 import TextInput from '../Input';
 import SearchIcon from 'react-icons/lib/fa/search';
+import Spinner from 'react-spinkit';
+
+const spinner = (
+  <Spinner
+    fadeIn="none"
+    name="circle"
+    color="#a9adc0"
+    style={{
+      width: 30,
+      height: 30,
+    }}
+  />
+);
 
 export default class AdvancedFacetView extends React.Component {
   constructor(props) {
@@ -29,6 +42,7 @@ export default class AdvancedFacetView extends React.Component {
       searchTerm: null,
       displayTreeData: null,
       searchBoxDisplayValue: '',
+      isLoading: true,
     };
   }
   fieldMappingFromPath = path => {
@@ -48,31 +62,53 @@ export default class AdvancedFacetView extends React.Component {
   constructFilterId = ({ field, value }) =>
     value ? `${field}---${value}` : field;
 
-  handleFacetViewValueChange = ({ sqon }) => {
+  handleSqonChange = ({ sqon }) => {
     const { onSqonFieldChange = () => {} } = this.props;
-    onSqonFieldChange({ sqon });
+    this.setState(
+      {
+        isLoading: true,
+      },
+      () => {
+        onSqonFieldChange({ sqon });
+      },
+    );
   };
 
-  componentWillReceiveProps(nextProps) {
-    // use state to cache computation result
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    const aggChanged = !isEqual(
+      this.props.aggregations,
+      prevProps.aggregations,
+    );
+    const sqonChanged = !isEqual(this.props.sqon, prevProps.sqon);
+    return {
+      shouldEndLoading: aggChanged || sqonChanged,
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState, { shouldEndLoading }) {
     const shouldRecomputeDisplayTree = !isEqual(
       {
         elasticMapping: this.props.elasticMapping,
         extendedMapping: this.props.extendedMapping,
       },
       {
-        elasticMapping: nextProps.elasticMapping,
-        extendedMapping: nextProps.extendedMapping,
+        elasticMapping: prevProps.elasticMapping,
+        extendedMapping: prevProps.extendedMapping,
       },
     );
     if (shouldRecomputeDisplayTree) {
       this.setState({
         displayTreeData: orderDisplayTreeData(
           injectExtensionToElasticMapping(
-            nextProps.elasticMapping,
-            nextProps.extendedMapping,
+            this.props.elasticMapping,
+            this.props.extendedMapping,
           ),
         ),
+      });
+    }
+    if (shouldEndLoading) {
+      this.setState({
+        isLoading: false,
       });
     }
   }
@@ -100,6 +136,7 @@ export default class AdvancedFacetView extends React.Component {
       searchTerm,
       displayTreeData,
       searchBoxDisplayValue,
+      isLoading,
     } = this.state;
     const scrollFacetViewToPath = path => {
       this.facetView.scrollToPath({ path });
@@ -113,123 +150,141 @@ export default class AdvancedFacetView extends React.Component {
       : displayTreeData;
 
     return (
-      displayTreeData && (
-        <div className="advancedFacetViewWrapper">
-          <div>
-            <SQONView
-              sqon={sqon}
-              valueCharacterLimit={valueCharacterLimit}
-              FieldCrumb={({ field, ...props }) => (
-                <Field {...{ field, ...props }}>
-                  {extendedMapping.find(e => e.field === field)?.displayName}
-                </Field>
-              )}
-              ValueCrumb={({ value, nextSQON, ...props }) => (
-                <Value
-                  onClick={() => {
-                    onSqonFieldChange({ sqon: nextSQON });
-                  }}
-                  {...props}
-                >
-                  {truncate(value, {
-                    length: valueCharacterLimit || Infinity,
-                  })}
-                </Value>
-              )}
-              Clear={({ nextSQON }) => (
-                <Bubble
-                  className="sqon-clear"
-                  onClick={() => {
-                    onSqonFieldChange({ sqon: nextSQON });
-                  }}
-                >
-                  Clear
-                </Bubble>
-              )}
-            />
-          </div>
-          <div className="facetViewWrapper">
-            <div className="panel treeViewPanel">
-              <div className="treeView">
-                <div className="panelHeading">
-                  <span className="fieldsShown">
-                    {withValueOnly
-                      ? keys(
-                          filterOutNonValue({
-                            aggregations,
-                          }).aggregationsWithValue,
-                        ).length
-                      : Object.keys(aggregations).length}{' '}
-                    fields
-                  </span>
-                  <span
-                    className="valueOnlyCheck"
-                    style={{ cursor: 'pointer' }}
-                    onClick={() =>
-                      this.setState({
-                        selectedPath: displayTreeData[0]?.path,
-                        withValueOnly: !withValueOnly,
-                      })
-                    }
+      <div className="advancedFacetViewWrapper">
+        {displayTreeData && (
+          <>
+            <div>
+              <SQONView
+                sqon={sqon}
+                valueCharacterLimit={valueCharacterLimit}
+                FieldCrumb={({ field, ...props }) => (
+                  <Field {...{ field, ...props }}>
+                    {extendedMapping.find(e => e.field === field)?.displayName}
+                  </Field>
+                )}
+                ValueCrumb={({ value, nextSQON, ...props }) => (
+                  <Value
+                    onClick={() => {
+                      this.handleSqonChange({ sqon: nextSQON });
+                    }}
+                    {...props}
                   >
-                    <input type="checkBox" checked={withValueOnly} />
-                    Show only fields with value
-                  </span>
+                    {truncate(value, {
+                      length: valueCharacterLimit || Infinity,
+                    })}
+                  </Value>
+                )}
+                Clear={({ nextSQON }) => (
+                  <Bubble
+                    className="sqon-clear"
+                    onClick={() => {
+                      this.handleSqonChange({ sqon: nextSQON });
+                    }}
+                  >
+                    Clear
+                  </Bubble>
+                )}
+              />
+            </div>
+            <div className="facetViewWrapper">
+              <div className="panel treeViewPanel">
+                <div className="treeView">
+                  <div className="panelHeading">
+                    <span className="fieldsShown">
+                      {withValueOnly
+                        ? keys(
+                            filterOutNonValue({
+                              aggregations,
+                            }).aggregationsWithValue,
+                          ).length
+                        : Object.keys(aggregations).length}{' '}
+                      fields
+                    </span>
+                    <span
+                      className="valueOnlyCheck"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() =>
+                        this.setState({
+                          selectedPath: displayTreeData[0]?.path,
+                          withValueOnly: !withValueOnly,
+                        })
+                      }
+                    >
+                      <input type="checkBox" checked={withValueOnly} />
+                      Show only fields with value
+                    </span>
+                  </div>
+                  <NestedTreeView
+                    searchString={searchTerm}
+                    defaultCollapsed={({ depth }) => depth !== 0}
+                    dataSource={visibleDisplayTreeData}
+                    selectedPath={selectedPath}
+                    onLeafSelect={path => {
+                      scrollFacetViewToPath(path);
+                      this.setState({ selectedPath: path });
+                    }}
+                  />
                 </div>
-                <NestedTreeView
-                  searchString={searchTerm}
-                  defaultCollapsed={({ depth }) => depth !== 0}
-                  dataSource={visibleDisplayTreeData}
-                  selectedPath={selectedPath}
-                  onLeafSelect={path => {
-                    scrollFacetViewToPath(path);
-                    this.setState({ selectedPath: path });
-                  }}
-                />
+              </div>
+              <div className={`panel facetsPanel`}>
+                <div className={`panelHeading`}>
+                  {/* using a thin local state here for rendering performance optimization */}
+                  <Component initialState={{ value: searchTerm || '' }}>
+                    {({ state: { value }, setState }) => (
+                      <TextInput
+                        icon={<SearchIcon />}
+                        className="filterInput"
+                        type="text"
+                        placeholder="Filter"
+                        value={value}
+                        onChange={e => {
+                          const value = e.target.value;
+                          setState({ value }, () => {
+                            this.setSearchTerm(value);
+                          });
+                        }}
+                      />
+                    )}
+                  </Component>
+                </div>
+                <div className={`facets`}>
+                  <FacetView
+                    extendedMapping={extendedMapping}
+                    valueCharacterLimit={valueCharacterLimit}
+                    constructEntryId={this.constructFilterId}
+                    ref={view => (this.facetView = view)}
+                    sqon={sqon}
+                    onValueChange={this.handleSqonChange}
+                    aggregations={aggregations}
+                    searchString={searchTerm}
+                    displayTreeData={filterDisplayTreeDataBySearchTerm({
+                      displayTree: visibleDisplayTreeData,
+                      aggregations,
+                      searchTerm: searchTerm,
+                    })}
+                  />
+                </div>
               </div>
             </div>
-            <div className={`panel facetsPanel`}>
-              <div className={`panelHeading`}>
-                {/* using a thin local state here for rendering performance optimization */}
-                <Component initialState={{ value: searchTerm || '' }}>
-                  {({ state: { value }, setState }) => (
-                    <TextInput
-                      icon={<SearchIcon />}
-                      className="filterInput"
-                      type="text"
-                      placeholder="Filter"
-                      value={value}
-                      onChange={e => {
-                        const value = e.target.value;
-                        setState({ value }, () => {
-                          this.setSearchTerm(value);
-                        });
-                      }}
-                    />
-                  )}
-                </Component>
-              </div>
-              <div className={`facets`}>
-                <FacetView
-                  extendedMapping={extendedMapping}
-                  valueCharacterLimit={valueCharacterLimit}
-                  constructEntryId={this.constructFilterId}
-                  ref={view => (this.facetView = view)}
-                  sqon={sqon}
-                  onValueChange={this.handleFacetViewValueChange}
-                  aggregations={aggregations}
-                  searchString={searchTerm}
-                  displayTreeData={filterDisplayTreeDataBySearchTerm({
-                    displayTree: visibleDisplayTreeData,
-                    aggregations,
-                    searchTerm: searchTerm,
-                  })}
-                />
-              </div>
-            </div>
+          </>
+        )}
+        {isLoading && (
+          <div
+            className={`loadingScreen ${css`
+              position: absolute;
+              left: 0px;
+              right: 0px;
+              top: 0px;
+              bottom: 0px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            `}`}
+          >
+            {spinner}
           </div>
-        </div>
-      )
+        )}
+      </div>
     );
   }
 }
