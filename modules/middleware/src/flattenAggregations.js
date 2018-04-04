a098ccc139c9c51be68b1f51e1bfefefb96814cb
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2018-04-04 12:20:37 -0400

    ✨ add missing bucket (#285)
    * ✨ add missing bucket
    
    * fix tests

--- modules/middleware/src/flattenAggregations.js
+++ modules/middleware/src/flattenAggregations.js
@@ -1,24 +1,32 @@
+import { get } from 'lodash';
 import { HISTOGRAM, STATS } from './constants';
 
 function flattenAggregations(aggregations) {
   return Object.entries(aggregations).reduce((prunedAggs, [key, value]) => {
     const [field, aggregationType = null] = key.split(':');
 
-    if ([STATS, HISTOGRAM].includes(aggregationType)) {
+    if (aggregationType === 'missing') {
+      return prunedAggs;
+    } else if ([STATS, HISTOGRAM].includes(aggregationType)) {
       return {
         ...prunedAggs,
         [field]: { ...prunedAggs[field], [aggregationType]: value },
       };
     } else if (Array.isArray(value.buckets)) {
+      const missing = get(aggregations, [`${field}:missing`]);
+      const buckets = [
+        ...value.buckets,
+        ...(missing ? [{ ...missing, key: '_missing' }] : []),
+      ];
       return {
         ...prunedAggs,
         [field]: {
-          buckets: [
-            ...value.buckets.map(({ rn, ...bucket }) => ({
+          buckets: buckets
+            .map(({ rn, ...bucket }) => ({
               ...bucket,
               doc_count: rn ? rn.doc_count : bucket.doc_count,
-            })),
-          ],
+            }))
+            .filter(b => b.doc_count),
         },
       };
     } else {
