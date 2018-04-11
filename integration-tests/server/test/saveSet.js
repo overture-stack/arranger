732961a6a5cb37b0b697a6c6e363b85f219e44ad
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-04-11 13:51:15 -0400

    :sparkles: add a placeholder for set integration testing

---
+++ integration-tests/server/test/saveSet.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+export default ({ server, api, port, esHost, projectId }) => {
+  it('should successfully save a set', done => {
+    // TODO: write this test
+    expect(true).to.equal(true);
+
+    done();
+  });
+};
