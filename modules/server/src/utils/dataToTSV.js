320c3e4be950beeb65feddad578a81e0526be664
Minh Ha <hlminh2000@gmail.com>
2018-04-13 13:23:03 -0400

    🐛 end tsv with new line so there is no merging issue (#330)
    * end tsv with new line so there is no merging issue
    
    * updates test

--- modules/server/src/utils/dataToTSV.js
+++ modules/server/src/utils/dataToTSV.js
@@ -87,15 +87,17 @@ export function columnsToHeader({ columns }) {
 }
 
 export function dataToTSV({ data, index, uniqueBy, columns, emptyValue }) {
-  return flatten(
-    get(data, `data['${index}'].hits.edges`, []).map(row => {
-      return getRows({
-        row: row.node,
-        paths: (uniqueBy || '').split('[].').filter(Boolean),
-        columns: columns,
-      }).map(row => row.map(r => r || emptyValue).join('\t'));
-    }),
-  ).join('\n');
+  return (
+    flatten(
+      get(data, `data['${index}'].hits.edges`, []).map(row => {
+        return getRows({
+          row: row.node,
+          paths: (uniqueBy || '').split('[].').filter(Boolean),
+          columns: columns,
+        }).map(row => row.map(r => r || emptyValue).join('\t'));
+      }),
+    ).join('\n') + '\n'
+  );
 }
 
 export default function({ columns, index, uniqueBy, emptyValue = '--' }) {
