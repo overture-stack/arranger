6e7cddaf8e22832f3ba9327dd315b20d359894dc
Minh <hlminh2000@gmail.com>
2018-05-17 17:59:51 -0400

    temporary solution

--- modules/mapping-utils/src/resolveAggregations.js
+++ modules/mapping-utils/src/resolveAggregations.js
@@ -1,4 +1,6 @@
 import getFields from 'graphql-fields';
+import { flattenDeep } from 'lodash';
+
 import {
   buildQuery,
   buildAggregations,
@@ -6,6 +8,68 @@ import {
 } from '@arranger/middleware';
 
 let toGraphqlField = (acc, [a, b]) => ({ ...acc, [a.replace(/\./g, '__')]: b });
+
+const getSetIdsFromSqon = ({ content }) => {
+  return (content || [])
+    .reduce((setIds, { content: subContent }) => {
+      return [
+        ...setIds,
+        ...(subContent.value || []).filter(
+          value => value.indexOf('set_id:') === 0,
+        ),
+      ];
+    }, [])
+    .map(setId => setId.replace('set_id:', ''));
+};
+
+const resolveValuesFromSetId = es => setId =>
+  es
+    .search({
+      index: 'arranger-sets',
+      type: 'arranger-sets',
+      body: {
+        query: {
+          bool: {
+            must: { match: { setId } },
+          },
+        },
+      },
+    })
+    .then(({ hits: { hits } }) => hits.map(({ _source: { ids } }) => ids));
+
+const resolveSetsInSqon = ({ sqon, es }) => {
+  const setIds = getSetIdsFromSqon(sqon || {});
+  if (setIds.length) {
+    return Promise.all(setIds.map(resolveValuesFromSetId(es))).then(
+      searchResult => {
+        const setIdsToValueMap = setIds.reduce(
+          (collection, id, i) => ({
+            ...collection,
+            [`set_id:${id}`]: flattenDeep(searchResult),
+          }),
+          {},
+        );
+        console.log('sqon: ');
+        const output = {
+          ...sqon,
+          content: sqon.content.map(op => ({
+            ...op,
+            content: {
+              ...op.content,
+              value:
+                flattenDeep(
+                  op.content.value.map(value => setIdsToValueMap[value]),
+                ) || op.content.value,
+            },
+          })),
+        };
+        return output;
+      },
+    );
+  } else {
+    return sqon;
+  }
+};
 
 export default type => async (
   obj,
@@ -19,7 +83,12 @@ export default type => async (
   info,
 ) => {
   const nestedFields = type.nested_fields;
-  const query = buildQuery({ nestedFields, filters });
+
+  const resolvedFilter = await resolveSetsInSqon({ sqon: filters, es });
+
+  console.log('resolvedFilter: ', JSON.stringify(resolvedFilter));
+
+  const query = buildQuery({ nestedFields, filters: resolvedFilter });
   const aggs = buildAggregations({
     query,
     graphqlFields: getFields(info),
