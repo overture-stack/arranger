4786f0d730c3d765b585af92ba4eac95f660d949
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-03 12:06:51 -0400

    :art: finesse the export structure to help with import

--- modules/server/src/projects/export.js
+++ modules/server/src/projects/export.js
@@ -2,6 +2,9 @@ import { groupBy } from 'lodash';
 import zlib from 'zlib';
 import tar from 'tar-stream';
 
+import getTypes from '../utils/getTypes';
+import mapHits from '../utils/mapHits';
+
 const getAllResults = async ({ es, search, results = [] }) => {
   const response = await es.search(search);
   const nextResults = [...results, ...response.hits.hits];
@@ -17,38 +20,57 @@ const getAllResults = async ({ es, search, results = [] }) => {
     : nextResults;
 };
 
+const jamTypeStateInPack = async ({ type, pack, id, es }) => {
+  const indexPrefix = `arranger-projects-${id}`;
+  const fileName = ({ key, typeKey = key.replace(`${indexPrefix}-`, '') }) =>
+    typeKey === type ? `extended` : typeKey.replace(`${type}-`, '');
+
+  let results;
+  try {
+    results = groupBy(
+      await getAllResults({
+        es,
+        search: {
+          index: `${indexPrefix}-${type}*`,
+          size: 500,
+          sort: ['_index:asc', '_id:asc'],
+        },
+      }),
+      '_index',
+    );
+  } catch (err) {
+    results = {};
+  }
+
+  await Promise.all(
+    Object.keys(results).map(
+      key =>
+        new Promise((res, rej) =>
+          pack.entry(
+            {
+              name: `arranger-project-${id}/${type}/${fileName({
+                key,
+              })}.json`,
+            },
+            JSON.stringify(results[key], null, 2),
+            err => (err ? rej(err) : res()),
+          ),
+        ),
+    ),
+  );
+};
+
 export default async (req, res) => {
   let { es } = req.context;
   let { id } = req.params;
 
   if (!id) return res.json({ error: 'project empty' });
 
-  id = id.toLowerCase();
-
-  const results = groupBy(
-    await getAllResults({
-      es,
-      search: {
-        index: `arranger-projects-${id}*`,
-        size: 500,
-        sort: ['_index:asc', '_id:asc'],
-      },
-    }),
-    '_index',
-  );
+  const types = mapHits(await getTypes({ id, es })).map(x => x.index);
 
   const pack = tar.pack();
   await Promise.all(
-    Object.keys(results).map(
-      k =>
-        new Promise((res, rej) =>
-          pack.entry(
-            { name: `${k}.json` },
-            JSON.stringify(results[k], null, 2),
-            err => (err ? rej(err) : res()),
-          ),
-        ),
-    ),
+    types.map(type => jamTypeStateInPack({ type, pack, id, es })),
   );
   await pack.finalize();
 
