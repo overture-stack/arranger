9485ba7cd36fdeff24287a24956a9ed61d077e79
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-03 12:06:32 -0400

    :art: move getTypes into its own helper

---
+++ modules/server/src/utils/getTypes.js
@@ -0,0 +1,12 @@
+const getTypes = async ({ id, es }) => {
+  const index = `arranger-projects-${id}`;
+
+  try {
+    return await es.search({ index, type: index });
+  } catch (error) {
+    await es.indices.create({ index });
+    return null;
+  }
+};
+
+export default getTypes;
