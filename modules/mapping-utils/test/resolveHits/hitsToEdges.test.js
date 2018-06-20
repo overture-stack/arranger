9efbfa842ec019464a41a9b21d8d43aab55461f6
Minh <hlminh2000@gmail.com>
2018-06-20 15:32:32 -0400

    use parallel in dev dependencies for testing

--- modules/mapping-utils/test/resolveHits/hitsToEdges.test.js
+++ modules/mapping-utils/test/resolveHits/hitsToEdges.test.js
@@ -2,8 +2,9 @@ import hits from './mockData/wrangledHits.json';
 import nestedFields from './mockData/nestedFields.json';
 import expectedEdges from './mockData/wrangledExpectedEdges.json';
 import { hitsToEdges } from '../../src/resolveHits';
+import Parallel from 'paralleljs';
 
 test('hitsToEdges should be acurate', async () => {
-  const edges = await hitsToEdges({ hits, nestedFields });
+  const edges = await hitsToEdges({ hits, nestedFields, Parallel });
   expect(edges).toEqual(expectedEdges);
 });
