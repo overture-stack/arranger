b7385ef20ec4518ffefaceb9db7011fba9a54f56
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-16 17:55:24 -0400

    Add x icon

--- modules/components/src/Aggs/TermAgg.js
+++ modules/components/src/Aggs/TermAgg.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { compose, withState } from 'recompose';
 import { isEmpty, orderBy, partition, truncate } from 'lodash';
 import DefaultSearchIcon from 'react-icons/lib/fa/search';
+import FaTimesCircleO from 'react-icons/lib/fa/times-circle';
+
 import { css } from 'emotion';
 
 import './AggregationCard.css';
@@ -168,6 +170,7 @@ const TermAgg = ({
                   value={searchText}
                   placeholder={searchPlaceholder}
                   icon={<DefaultSearchIcon />}
+                  rightIcon={<FaTimesCircleO />}
                   onChange={({ target: { value } }) =>
                     setSearchText(value || '')
                   }
