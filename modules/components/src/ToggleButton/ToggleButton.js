56b819e984418e1eb4ea14487bda0d81e39ddb5b
Ciarán Schütte <ciaranschutte@gmail.com>
2018-10-17 14:50:34 -0400

    Feature/809 boolean agg disabled (#456)
    * add two boolean agg stories to test
    
    * add toggle button disabled css
    
    * add disabled option to toggle button
    
    * add disabled check in boolean agg
    
    * show disabled button segment if undefined
    
    * update toggle button disable style
    
    * update package.lock
    
    * cleanup

--- modules/components/src/ToggleButton/ToggleButton.js
+++ modules/components/src/ToggleButton/ToggleButton.js
@@ -3,14 +3,20 @@ import './ToggleButton.css';
 
 export default ({ value, options = [], onChange = () => {} }) => (
   <div className="toggle-button">
-    {options.map(x => (
-      <div
-        key={x.value}
-        className={`toggle-button-option ${value === x.value ? 'active' : ''}`}
-        onClick={() => onChange({ value: x.value })}
-      >
-        {x.title}
-      </div>
-    ))}
+    {options.map(x => {
+      const isDisabled = x ? x.disabled : false;
+
+      return (
+        <div
+          key={x.value}
+          className={`toggle-button-option ${
+            value === x.value ? 'active' : ''
+          }${isDisabled ? 'disabled' : ''}`}
+          onClick={() => (!isDisabled ? onChange({ value: x.value }) : null)}
+        >
+          {x.title}
+        </div>
+      );
+    })}
   </div>
 );
