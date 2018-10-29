3fc029b52cbd652fe9f150b8fabeb9b8dcdad0dd
Minh <hlminh2000@gmail.com>
2018-10-29 18:57:08 -0400

    more typings of the elasticsearch mapping

--- modules/admin/src/services/elasticsearch.ts
+++ modules/admin/src/services/elasticsearch.ts
@@ -6,12 +6,54 @@ export const createClient = (esHost: string) =>
     // log: 'trace',
   });
 
+enum EsTypes {
+  keyword = 'keyword',
+  long = 'long',
+  integer = 'integer',
+  short = 'short',
+  byte = 'byte',
+  double = 'double',
+  float = 'float',
+  half_float = 'half_float',
+  scaled_float = 'scaled_float',
+  date = 'date',
+  boolean = 'boolean',
+  binary = 'binary',
+  integer_range = 'integer_range',
+  float_range = 'float_range',
+  long_range = 'long_range',
+  double_range = 'double_range',
+  date_range = 'date_range',
+  object = 'object',
+  nested = 'nested',
+  geo_point = 'geo_point',
+  geo_shape = 'geo_shape',
+  ip = 'ip',
+  completion = 'completion',
+  token_count = 'token_count',
+  murmur3 = 'murmur3',
+  join = 'join',
+}
+
+interface EsFieldMapping {
+  type: EsTypes;
+  fields?: {
+    [key: string]: EsFieldMapping;
+  };
+  properties?: {
+    [key: string]: EsFieldMapping;
+  };
+}
+
 interface EsMapping {
   [key: string]: {
     mappings: {
       [key: string]: {
-        properties: {
+        _meta?: {
           [key: string]: any;
+        };
+        properties: {
+          [key: string]: EsFieldMapping;
         };
       };
     };
