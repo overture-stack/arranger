e959110ed4b81c83c837d3cd17f5fb4302857192
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-10-30 15:49:31 -0400

    Adds aria check eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,12 @@ module.exports = {
     'prettier/flowtype',
     'plugin:flowtype/recommended',
     'react-app',
+    'plugin:jsx-a11y/strict',
   ],
-  plugins: ['flowtype', 'prettier'],
+  plugins: ['flowtype', 'prettier', 'jsx-a11y'],
   rules: {
     'prettier/prettier': [1, { trailingComma: 'all', singleQuote: true }],
     'flowtype/define-flow-type': 1,
+    'jsx-a11y/href-no-hash': 'off',
   },
 };
