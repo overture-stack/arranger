9002747947114a6495f52da6fd16d8e5d13af6c5
Minh <hlminh2000@gmail.com>
2018-11-09 18:11:32 -0500

    data import to api is working but need to remodel storage to allow for parallel updates...

--- modules/admin-ui/src/pages/VersionDashboard/AddProjectForm/graphqlService.tsx
+++ modules/admin-ui/src/pages/VersionDashboard/AddProjectForm/graphqlService.tsx
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { Mutation, MutationFn } from 'react-apollo';
 import { uniqBy } from 'lodash';
 import { ApolloConsumer } from 'react-apollo';
+import { pick } from 'lodash';
 
 import { THoc } from 'src/utils';
 import {
@@ -18,65 +19,166 @@ import {
 } from './types';
 import { ApolloClient } from 'apollo-boost';
 
-/******************
- * Provides server mutation to add multiple indices given a project
- ******************/
-const ProjectIndicesMutationProvider: React.ComponentType<{
-  children: (
-    {
-      createNewProjectIndex,
-    }: { createNewProjectIndex: MutationFn<{}, INewIndexInput> },
-  ) => React.ReactNode;
-}> = ({ children }) => {
-  const MUTATION = gql`
-    mutation(
-      $projectId: String!
-      $graphqlField: String!
-      $esIndex: String!
-      $esType: String!
-    ) {
-      newIndex(
-        projectId: $projectId
-        graphqlField: $graphqlField
-        esIndex: $esIndex
-        esType: $esType
-      ) {
-        id
-      }
-    }
-  `;
-  return (
-    <Mutation mutation={MUTATION}>
-      {(applyMutation: MutationFn<{}, INewIndexInput>) =>
-        children({ createNewProjectIndex: applyMutation })
-      }
-    </Mutation>
-  );
-};
-
 const saveAggsState = (client: ApolloClient<{}>) => (
   projectId: string,
   graphqlField: string,
 ) => async (state: IAggsState): Promise<{}> => {
-  return {};
+  const MUTATION = gql`
+    mutation(
+      $projectId: String!
+      $graphqlField: String!
+      $state: [AggStateInput]!
+    ) {
+      saveAggsState(
+        projectId: $projectId
+        graphqlField: $graphqlField
+        state: $state
+      ) {
+        state {
+          field
+        }
+      }
+    }
+  `;
+  return client.mutate({
+    mutation: MUTATION,
+    variables: {
+      projectId,
+      graphqlField,
+      state: state.map(s => pick(s, ['field', 'active', 'show'])),
+    },
+  });
 };
+
 const saveColumnsState = (client: ApolloClient<{}>) => (
   projectId: string,
   graphqlField: string,
 ) => async (state: IColumnsState): Promise<{}> => {
-  return {};
+  const MUTATION = gql`
+    mutation(
+      $projectId: String!
+      $graphqlField: String!
+      $state: ColumnStateInput!
+    ) {
+      saveColumnsState(
+        projectId: $projectId
+        graphqlField: $graphqlField
+        state: $state
+      ) {
+        ... on ColumnsState {
+          timestamp
+        }
+      }
+    }
+  `;
+  return client.mutate({
+    mutation: MUTATION,
+    variables: {
+      projectId,
+      graphqlField,
+      state: {
+        ...pick(state, ['type', 'keyField']),
+        defaultSorted: state.defaultSorted.map(s => pick(s, ['id', 'desc'])),
+        columns: state.columns.map(s =>
+          pick(s, [
+            'show',
+            'type',
+            'sortable',
+            'canChangeShow',
+            'query',
+            'jsonPath',
+            'id',
+            'field',
+            'accessor',
+          ]),
+        ),
+      },
+    },
+  });
 };
+
 const saveExtendedMapping = (client: ApolloClient<{}>) => (
   projectId: string,
   graphqlField: string,
 ) => async (state: IExtendedMapping): Promise<{}> => {
+  const MUTATION = gql`
+    mutation(
+      $projectId: String!
+      $graphqlField: String!
+      $field: String!
+      $extendedFieldMappingInput: ExtendedFieldMappingInput!
+    ) {
+      updateExtendedMapping(
+        projectId: $projectId
+        graphqlField: $graphqlField
+        field: $field
+        extendedFieldMappingInput: $extendedFieldMappingInput
+      ) {
+        field
+      }
+    }
+  `;
+  for (var s of state) {
+    await client.mutate({
+      mutation: MUTATION,
+      variables: {
+        projectId,
+        graphqlField,
+        field: s.field,
+        extendedFieldMappingInput: pick(s, [
+          'type',
+          'displayName',
+          'active',
+          'isArray',
+          'primaryKey',
+          'quickSearchEnabled',
+          'unit',
+          'displayValues',
+          'rangeStep',
+        ]),
+      },
+    });
+  }
   return {};
 };
+
 const saveMatboxState = (client: ApolloClient<{}>) => (
   projectId: string,
   graphqlField: string,
 ) => async (state: IMatchboxState): Promise<{}> => {
-  return {};
+  const MUTATION = gql`
+    mutation(
+      $projectId: String!
+      $graphqlField: String!
+      $state: [MatchBoxFieldInput]
+    ) {
+      saveMatchBoxState(
+        projectId: $projectId
+        graphqlField: $graphqlField
+        state: $state
+      ) {
+        state {
+          field
+        }
+      }
+    }
+  `;
+  return client.mutate({
+    mutation: MUTATION,
+    variables: {
+      projectId,
+      graphqlField,
+      state: state.map(s =>
+        pick(s, [
+          'displayName',
+          'field',
+          'isActive',
+          'keyField',
+          'searchFields',
+        ]),
+      ),
+    },
+  });
 };
 
 /******************
@@ -134,6 +236,42 @@ const validateProjectConfigData = (indexConfigs: INewIndexArgs[]) => {
   });
 };
 
+/******************
+ * Provides server mutation to add multiple indices given a project
+ ******************/
+const ProjectIndicesMutationProvider: React.ComponentType<{
+  children: (
+    {
+      createNewProjectIndex,
+    }: { createNewProjectIndex: MutationFn<{}, INewIndexInput> },
+  ) => React.ReactNode;
+}> = ({ children }) => {
+  const MUTATION = gql`
+    mutation(
+      $projectId: String!
+      $graphqlField: String!
+      $esIndex: String!
+      $esType: String!
+    ) {
+      newIndex(
+        projectId: $projectId
+        graphqlField: $graphqlField
+        esIndex: $esIndex
+        esType: $esType
+      ) {
+        id
+      }
+    }
+  `;
+  return (
+    <Mutation mutation={MUTATION}>
+      {(applyMutation: MutationFn<{}, INewIndexInput>) =>
+        children({ createNewProjectIndex: applyMutation })
+      }
+    </Mutation>
+  );
+};
+
 /*****************
  * Provides server transaction to add index
  *****************/
@@ -167,7 +305,7 @@ const withAddProjectMutation: THoc<
                       projectId,
                     },
                   });
-                  indexConfigs.forEach(async indexConfig => {
+                  for (var indexConfig of indexConfigs) {
                     const { config } = indexConfig;
                     await createNewProjectIndex({
                       variables: indexConfig.newIndexMutationInput,
@@ -179,42 +317,33 @@ const withAddProjectMutation: THoc<
                         matchboxState,
                         extended,
                       } = config;
-                      const configPromises: Array<Promise<{}>> = [];
                       if (aggsState) {
-                        configPromises.push(
-                          saveAggsState(client)(
-                            projectId,
-                            indexConfig.newIndexMutationInput.graphqlField,
-                          )(aggsState),
-                        );
+                        await saveAggsState(client)(
+                          projectId,
+                          indexConfig.newIndexMutationInput.graphqlField,
+                        )(aggsState);
                       }
                       if (columnsState) {
-                        configPromises.push(
-                          saveColumnsState(client)(
-                            projectId,
-                            indexConfig.newIndexMutationInput.graphqlField,
-                          )(columnsState),
-                        );
+                        await saveColumnsState(client)(
+                          projectId,
+                          indexConfig.newIndexMutationInput.graphqlField,
+                        )(columnsState);
                       }
                       if (matchboxState) {
-                        configPromises.push(
-                          saveMatboxState(client)(
-                            projectId,
-                            indexConfig.newIndexMutationInput.graphqlField,
-                          )(matchboxState),
-                        );
+                        await saveMatboxState(client)(
+                          projectId,
+                          indexConfig.newIndexMutationInput.graphqlField,
+                        )(matchboxState);
                       }
                       if (extended) {
-                        configPromises.push(
-                          saveExtendedMapping(client)(
-                            projectId,
-                            indexConfig.newIndexMutationInput.graphqlField,
-                          )(extended),
-                        );
+                        await saveExtendedMapping(client)(
+                          projectId,
+                          indexConfig.newIndexMutationInput.graphqlField,
+                        )(extended);
                       }
-                      await configPromises;
                     }
-                  });
+                  }
+                  return;
                 };
                 return <Wrapped addProject={addProject} {...props} />;
               }}
