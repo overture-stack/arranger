e30bbda2fb87ae7b8cfadb16cb126978c42860e3
Minh <hlminh2000@gmail.com>
2018-11-13 13:26:45 -0500

    more some features for the extended mapping interface

--- modules/admin-ui/src/store/configEditorReducer.ts
+++ modules/admin-ui/src/store/configEditorReducer.ts
@@ -4,6 +4,7 @@ import { IGqlData } from 'src/gql/queries/allProjectData';
 export enum ActionType {
   PROJECT_SELECT = 'PROJECT_SELECT',
   PROJECT_DATA_LOADED = 'PROJECT_DATA_LOADED',
+  'EXTENDED_MAPPING_DISPLAY_NAME_CHANGE' = 'EXTENDED_MAPPING_DISPLAY_NAME_CHANGE',
 }
 
 export interface IProjectConfigEditorState {
@@ -18,13 +19,27 @@ export interface IProjectDataLoadedAction
 
 const reducer = (
   state = initialState,
-  action: IProjectDataLoadedAction,
+  action:
+    | IReduxAction<ActionType.PROJECT_DATA_LOADED, { data: IGqlData }>
+    | IReduxAction<
+        ActionType.EXTENDED_MAPPING_DISPLAY_NAME_CHANGE,
+        {
+          graphqlField: string;
+          field: string;
+          value: string;
+        }
+      >,
 ): IProjectConfigEditorState => {
   switch (action.type) {
     case ActionType.PROJECT_DATA_LOADED:
       return {
         ...state,
         currentProjectData: action.payload.data,
+      };
+    case ActionType.EXTENDED_MAPPING_DISPLAY_NAME_CHANGE:
+      const { payload: { field, graphqlField, value } } = action;
+      return {
+        ...state,
       };
     default:
       return state;
