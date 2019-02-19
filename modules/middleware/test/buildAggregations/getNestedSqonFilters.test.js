46dd65f3920051267edcd9b3552a835f437c99b6
Minh <hlminh2000@gmail.com>
2019-02-19 14:00:06 -0500

    make getNestedSqonFilters handle nested sqons

--- modules/middleware/test/buildAggregations/getNestedSqonFilters.test.js
+++ modules/middleware/test/buildAggregations/getNestedSqonFilters.test.js
@@ -1,32 +1,75 @@
 import getNestedSqonFilters from '../../src/buildAggregations/getNestedSqonFilters';
+import { AND_OP, IN_OP } from '../../src/constants';
 
 test('getNestedSqonFilters should be able to extract filters applied on nested fields', () => {
   const sqon = {
-    op: 'and',
+    op: AND_OP,
     content: [
-      { op: 'in', content: { field: 'a', value: [] } },
-      { op: 'in', content: { field: 'a', value: [] } },
-      { op: 'in', content: { field: 'a.c', value: [] } },
-      { op: 'in', content: { field: 'a.b.c', value: [] } },
-      { op: 'in', content: { field: 'a.b.d', value: [] } },
+      { op: IN_OP, content: { field: 'a', value: [] } },
+      { op: IN_OP, content: { field: 'a', value: [] } },
+      { op: IN_OP, content: { field: 'a.c', value: [] } },
+      { op: IN_OP, content: { field: 'a.b.c', value: [] } },
+      { op: IN_OP, content: { field: 'a.b.d', value: [] } },
     ],
   };
   const nestedFields = ['a', 'a.b'];
   const expectedOutput = {
-    a: [{ op: 'in', content: { field: 'a.c', value: [] } }],
+    a: [{ op: IN_OP, content: { field: 'a.c', value: [] } }],
     'a.b': [
-      { op: 'in', content: { field: 'a.b.c', value: [] } },
-      { op: 'in', content: { field: 'a.b.d', value: [] } },
+      { op: IN_OP, content: { field: 'a.b.c', value: [] } },
+      { op: IN_OP, content: { field: 'a.b.d', value: [] } },
     ],
   };
   expect(getNestedSqonFilters({ sqon, nestedFields })).toEqual(expectedOutput);
 });
 
-test('getNestedSqonFilters should handle sqon contents without fields', () => {
+test('getNestedSqonFilters should handle nested sqons', () => {
   const sqon = {
-    op: 'and',
-    content: [{}],
+    op: AND_OP,
+    pivot: null,
+    content: [
+      {
+        op: AND_OP,
+        content: [
+          {
+            op: IN_OP,
+            pivot: null,
+            content: {
+              field: 'files.kf_id',
+              value: ['GF_V1C32MZ6'],
+            },
+          },
+          {
+            op: IN_OP,
+            pivot: null,
+            content: {
+              field: 'files.kf_id',
+              value: ['GF_C78A0NP8'],
+            },
+          },
+        ],
+      },
+    ],
   };
-  const nestedFields = [];
-  expect(getNestedSqonFilters({ sqon, nestedFields })).toEqual({});
+  const nestedFields = ['files'];
+  expect(getNestedSqonFilters({ sqon, nestedFields })).toEqual({
+    ['files']: [
+      {
+        op: IN_OP,
+        pivot: null,
+        content: {
+          field: 'files.kf_id',
+          value: ['GF_V1C32MZ6'],
+        },
+      },
+      {
+        op: IN_OP,
+        pivot: null,
+        content: {
+          field: 'files.kf_id',
+          value: ['GF_C78A0NP8'],
+        },
+      },
+    ],
+  });
 });
