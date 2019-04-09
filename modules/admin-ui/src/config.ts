a72c9db76dcd4c521ea81bc5d8c0e2e70bee799a
Minh <hlminh2000@gmail.com>
2019-04-09 11:13:11 -0400

    modularizes admin-ui

---
+++ modules/admin-ui/src/config.ts
@@ -0,0 +1,3 @@
+export const adminApiRoot =
+  process.env.REACT_APP_ARRANGER_ADMIN_ROOT ||
+  'http://localhost:5050/admin/graphql';
--- modules/admin-ui/webpack.config.npm.js
+++ modules/admin-ui/webpack.config.npm.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
