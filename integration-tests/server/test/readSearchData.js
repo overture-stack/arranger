fea98baf9350b5aa917593450642b893058b4057
Minh <hlminh2000@gmail.com>
2019-12-20 14:43:02 -0500

    :bug: more stable test

--- integration-tests/server/test/readSearchData.js
+++ integration-tests/server/test/readSearchData.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import gql from 'graphql-tag';
 import { print } from 'graphql';
-import get from 'lodash/get';
+import orderBy from 'lodash/orderBy';
 
 export default ({ api, graphqlField, gqlPath }) => {
   it('reads aggregations properly', async () => {
@@ -45,14 +45,92 @@ export default ({ api, graphqlField, gqlPath }) => {
       },
     });
   });
-  it('reads hits properly', async () => {
+  it('reads aggregations with sqon properly', async () => {
     let response = await api.post({
       endpoint: gqlPath,
       body: {
         query: print(gql`
           {
             ${graphqlField} {
-              hits {
+              aggregations(
+                filters: {
+                  op: "and",
+                  content: [
+                    {
+                      op: "in",
+                      content: {
+                        field: "clinical_diagnosis.clinical_stage_grouping",
+                        value: "Stage I"
+                      }
+                    }
+                  ]
+                }, 
+                aggregations_filter_themselves: true
+              ) {
+                clinical_diagnosis__clinical_stage_grouping {
+                  buckets {
+                    doc_count
+                    key
+                  }
+                }
+              }
+            }
+          }
+        `),
+      },
+    });
+    expect({
+      data: {
+        [graphqlField]: {
+          aggregations: {
+            clinical_diagnosis__clinical_stage_grouping: {
+              buckets: orderBy(
+                response.data[graphqlField].aggregations
+                  .clinical_diagnosis__clinical_stage_grouping.buckets,
+                'key',
+              ),
+            },
+          },
+        },
+      },
+    }).to.eql({
+      data: {
+        model: {
+          aggregations: {
+            clinical_diagnosis__clinical_stage_grouping: {
+              buckets: [
+                {
+                  doc_count: 1,
+                  key: 'Stage I',
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+  });
+  it('reads hits with sqon properly', async () => {
+    let response = await api.post({
+      endpoint: gqlPath,
+      body: {
+        query: print(gql`
+          {
+            ${graphqlField} {
+              hits(
+                filters: {
+                  op: "and",
+                  content: [
+                    {
+                      op: "in",
+                      content: {
+                        field: "clinical_diagnosis.clinical_stage_grouping",
+                        value: "Stage I"
+                      }
+                    }
+                  ]
+                }
+              ) {
                 total
                 edges {
                   node {
@@ -69,11 +147,8 @@ export default ({ api, graphqlField, gqlPath }) => {
       data: {
         model: {
           hits: {
-            total: 2,
-            edges: [
-              { node: { id: '5da62fbad545d210fe1c63a9' } },
-              { node: { id: '5dc9b6c3d614630f9809f7d0' } },
-            ],
+            total: 1,
+            edges: [{ node: { id: '5da62fbad545d210fe1c63a9' } }],
           },
         },
       },
