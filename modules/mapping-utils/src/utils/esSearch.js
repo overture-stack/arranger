ba930e514aca49376ad3794caac356d503882842
Minh <hlminh2000@gmail.com>
2019-12-24 09:33:25 -0500

    :bug: removes mapping utils dependency on elasticsearch

--- modules/mapping-utils/src/utils/esSearch.js
+++ modules/mapping-utils/src/utils/esSearch.js
@@ -1,12 +1,4 @@
-import { Client, RequestParams } from '@elastic/elasticsearch';
-
-/**
- * @param {Client} es
- */
 export default es => {
-  /**
-   * @param {RequestParams.Search} params
-   */
   const output = async params => (await es.search(params)).body;
   return output;
 };
