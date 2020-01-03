5b54ee10534a665146665641011ae0871011c700
Minh <hlminh2000@gmail.com>
2020-01-03 16:34:43 -0500

    :construction_worker: tests out cd script

---
+++ publish.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process');
+const promisify = require('util').promisify;
+
+(async () => {
+  const run = (...args) =>
+    promisify(exec)(...args).then(({ stdout, stderr }) => {
+      if (stderr) {
+        console.error(stderr);
+        process.exit(1);
+      } else {
+        console.log(stdout);
+        return stdout;
+      }
+    });
+  const { version } = require('./lerna.json');
+
+  await run(`npm run lerna version -- --yes ${version}`);
+})();
