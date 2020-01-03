81a2640bac14a985f2ce5d7f45a6f7666da1e634
Minh <hlminh2000@gmail.com>
2020-01-03 14:51:12 -0500

    :fire: no more dashboard

--- modules/components/src/index.js
+++ modules/components/src/index.js
@@ -1,4 +1,3 @@
 export * from './Arranger';
 export { default as api, addHeaders } from './utils/api';
-export { default as Dashboard } from './Admin/Dashboard';
 export { default as Query, withQuery } from './Query';
