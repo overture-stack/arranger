aacb6eece426d5fb8c6bc797f3e2f03d32abd69d
Anders Richardsson <justin.richardsson@gmail.com>
2021-03-09 02:01:06 -0500

    allow BASE_URL upon docker run

---
+++ docker/ui/env-config.template.js
@@ -0,0 +1,4 @@
+window.__env__ = {
+  REACT_APP_BASE_URL: '${REACT_APP_BASE_URL}',
+  REACT_APP_ARRANGER_ADMIN_ROOT: '/${REACT_APP_BASE_URL}/admin/graphql'.replace('//', '/'),
+};
--- modules/admin-ui/src/config.ts
+++ modules/admin-ui/src/config.ts
@@ -1,2 +1,17 @@
+declare global {
+  interface Window {
+    __env__: {
+      REACT_APP_ARRANGER_ADMIN_ROOT: string;
+      REACT_APP_BASE_URL: string;
+    };
+  }
+}
+
 export const adminApiRoot =
-  process.env.REACT_APP_ARRANGER_ADMIN_ROOT || 'http://localhost:5050/admin/graphql';
+  window.__env__.REACT_APP_ARRANGER_ADMIN_ROOT ||
+  process.env.REACT_APP_ARRANGER_ADMIN_ROOT ||
+  'http://localhost:5050/admin/graphql';
+
+const baseURLenv = window.__env__.REACT_APP_BASE_URL || process.env.REACT_APP_BASE_URL;
+
+export const baseURL = baseURLenv && `/${baseURLenv}`;
