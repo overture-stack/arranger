b7a6da0f7be1b2966cb665eb66b2e1744a870705
Leonardo Rivera <leorivera_88@hotmail.com>
2023-02-08 13:17:12 -0500

    filter select column dropdown
    adding a text filter to dropdown component

--- modules/components/src/DataTable/TableToolbar/TableToolbar.js
+++ modules/components/src/DataTable/TableToolbar/TableToolbar.js
@@ -19,16 +19,16 @@ import exporterProcessor from './helpers';
 import './Toolbar.css';
 
 const enhance = compose(
-  withProps(({ columns }) => ({
-    canChangeShowColumns: columns.filter((column) => column.canChangeShow),
-  })),
-  withPropsOnChange(['onFilterChange'], ({ onFilterChange = () => {} }) => ({
-    debouncedOnFilterChange: debounce(onFilterChange, 300),
-  })),
-  withState('filterVal', 'setFilterVal', ''),
-  withPropsOnChange(['sqon'], ({ sqon, setFilterVal }) => {
-    if (!currentFilterValue(sqon)) setFilterVal('');
-  }),
+	withProps(({ columns }) => ({
+		canChangeShowColumns: columns.filter((column) => column.canChangeShow),
+	})),
+	withPropsOnChange(['onFilterChange'], ({ onFilterChange = () => {} }) => ({
+		debouncedOnFilterChange: debounce(onFilterChange, 300),
+	})),
+	withState('filterVal', 'setFilterVal', ''),
+	withPropsOnChange(['sqon'], ({ sqon, setFilterVal }) => {
+		if (!currentFilterValue(sqon)) setFilterVal('');
+	}),
 );
 
 /** Advanced Implementation details
@@ -55,60 +55,61 @@ const enhance = compose(
  */
 
 const TableToolbar = ({
-  allColumns = [],
-  allowTogglingColumns = true,
-  allowTSVExport = true,
-  canChangeShowColumns,
-  columnDropdownText = 'Show columns',
-  columns,
-  customActions = null,
-  customHeaderContent = null,
-  debouncedOnFilterChange,
-  defaultColumns,
-  downloadUrl,
-  enableDropDownControls = false,
-  enableSelectedTableRowsExporterFilter = false,
-  selectedRowsFilterPropertyName = 'file_autocomplete',
-  exporter = null,
-  exporterLabel = 'Download',
-  exportMaxRows,
-  exportTSVFilename = '',
-  exportTSVText = 'Export TSV',
-  filterInputPlaceholder = 'Filter',
-  filterVal,
-  InputComponent,
-  onColumnsChange,
-  onFilterChange,
-  onMultipleColumnsChange,
-  page = 0,
-  pageSize = 0,
-  propsData,
-  selectedTableRows = [],
-  setFilterVal,
-  showFilterInput = true,
-  sqon = null,
-  style,
-  total = propsData?.total || 0,
-  transformParams = (params) => params,
+	allColumns = [],
+	allowTogglingColumns = true,
+	allowTSVExport = true,
+	canChangeShowColumns,
+	columnDropdownText = 'Show columns',
+	columns,
+	customActions = null,
+	customHeaderContent = null,
+	debouncedOnFilterChange,
+	defaultColumns,
+	downloadUrl,
+	enableDropDownControls = false,
+	enableSelectColumnDropdownTextFilter = false,
+	enableSelectedTableRowsExporterFilter = false,
+	selectedRowsFilterPropertyName = 'file_autocomplete',
+	exporter = null,
+	exporterLabel = 'Download',
+	exportMaxRows,
+	exportTSVFilename = '',
+	exportTSVText = 'Export TSV',
+	filterInputPlaceholder = 'Filter',
+	filterVal,
+	InputComponent,
+	onColumnsChange,
+	onFilterChange,
+	onMultipleColumnsChange,
+	page = 0,
+	pageSize = 0,
+	propsData,
+	selectedTableRows = [],
+	setFilterVal,
+	showFilterInput = true,
+	sqon = null,
+	style,
+	total = propsData?.total || 0,
+	transformParams = (params) => params,
 }) => {
-  const { documentType } = useDataContext({ callerName: 'OldTableToolbar' });
-  const {
-    components: { Table: { DropDown: themeDropDownProps = emptyObj } = emptyObj } = emptyObj,
-  } = useThemeContext({ callerName: 'OldTableToolBar' });
+	const { documentType } = useDataContext({ callerName: 'OldTableToolbar' });
+	const {
+		components: { Table: { DropDown: themeDropDownProps = emptyObj } = emptyObj } = emptyObj,
+	} = useThemeContext({ callerName: 'OldTableToolBar' });
 
-  const oneOrManyDocuments = pluralize(
-    documentType,
-    isPlural({ total, pageSize, currentPage: page }) ? 2 : 1,
-  );
+	const oneOrManyDocuments = pluralize(
+		documentType,
+		isPlural({ total, pageSize, currentPage: page }) ? 2 : 1,
+	);
 
-  const { singleExporter, exporterArray, multipleExporters } = exporterProcessor(
-    exporter,
-    allowTSVExport,
-    exportTSVText,
-    exportMaxRows,
-  );
+	const { singleExporter, exporterArray, multipleExporters } = exporterProcessor(
+		exporter,
+		allowTSVExport,
+		exportTSVText,
+		exportMaxRows,
+	);
 
-  const hasSelectedRows = selectedTableRows.length > 0;
+	const hasSelectedRows = selectedTableRows.length > 0;
 
 	const downloadSqon =
 		enableSelectedTableRowsExporterFilter && hasSelectedRows
@@ -123,175 +124,176 @@ const TableToolbar = ({
 			  }
 			: sqon;
 
-  return (
-    <div style={{ display: 'flex', flex: 'none', ...style }} className="tableToolbar">
-      <div
-        className="currentlyDisplayed"
-        style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}
-      >
-        <span className="numbers">
-          {`Showing ${(page * pageSize + 1).toLocaleString()} - ${Math.min(
-            (page + 1) * pageSize,
-            total,
-          ).toLocaleString()}`}
-        </span>{' '}
-        <span className="ofTotal">of {total?.toLocaleString()}</span>{' '}
-        <span className="type">{oneOrManyDocuments}</span>
-      </div>
-      {customHeaderContent || null}
+	return (
+		<div style={{ display: 'flex', flex: 'none', ...style }} className="tableToolbar">
+			<div
+				className="currentlyDisplayed"
+				style={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}
+			>
+				<span className="numbers">
+					{`Showing ${(page * pageSize + 1).toLocaleString()} - ${Math.min(
+						(page + 1) * pageSize,
+						total,
+					).toLocaleString()}`}
+				</span>{' '}
+				<span className="ofTotal">of {total?.toLocaleString()}</span>{' '}
+				<span className="type">{oneOrManyDocuments}</span>
+			</div>
+			{customHeaderContent || null}
 
-      {showFilterInput && (
-        <div className="group">
-          <TextFilter
-            Component={InputComponent}
-            onChange={({ value, generateNextSQON } = {}) => {
-              setFilterVal(value);
-              debouncedOnFilterChange({ value, generateNextSQON });
-            }}
-            placeholder={filterInputPlaceholder}
-            value={filterVal}
-          />
-        </div>
-      )}
+			{showFilterInput && (
+				<div className="group">
+					<TextFilter
+						Component={InputComponent}
+						onChange={({ value, generateNextSQON } = {}) => {
+							setFilterVal(value);
+							debouncedOnFilterChange({ value, generateNextSQON });
+						}}
+						placeholder={filterInputPlaceholder}
+						value={filterVal}
+					/>
+				</div>
+			)}
 
-      <div className="group">
-        {allowTogglingColumns &&
-          (enableDropDownControls ? (
-            <MultiSelectDropDown
-              allowControls
-              allowSelection
-              buttonAriaLabelClosed={`Open column selection menu`}
-              buttonAriaLabelOpen={`Close column selection menu`}
-              itemSelectionLegend={`Select columns to display`}
-              selectAllAriaLabel={`Select all columns`}
-              resetToDefaultAriaLabel={`Reset to default columns`}
-              itemToString={(i) => i.displayName}
-              items={canChangeShowColumns}
-              defaultColumns={defaultColumns}
-              onChange={(item) => {
-                setFilterVal('');
-                onFilterChange({
-                  value: '',
-                  generateNextSQON: generateNextSQON(''),
-                });
-                onColumnsChange({ ...item, show: !item.show });
-              }}
-              onMultipleChange={(changes) => {
-                onMultipleColumnsChange(changes);
-              }}
-              theme={themeDropDownProps}
-            >
-              {columnDropdownText}
-            </MultiSelectDropDown>
-          ) : (
-            <DropDown
-              aria-label={`Select columns`}
-              itemToString={(i) => i.displayName}
-              items={canChangeShowColumns}
-              onChange={(item) => {
-                setFilterVal('');
-                onFilterChange({
-                  value: '',
-                  generateNextSQON: generateNextSQON(''),
-                });
-                onColumnsChange({ ...item, show: !item.show });
-              }}
-              theme={themeDropDownProps}
-            >
-              {columnDropdownText}
-            </DropDown>
-          ))}
+			<div className="group">
+				{allowTogglingColumns &&
+					(enableDropDownControls ? (
+						<MultiSelectDropDown
+							allowControls
+							allowSelection
+							buttonAriaLabelClosed={`Open column selection menu`}
+							buttonAriaLabelOpen={`Close column selection menu`}
+							itemSelectionLegend={`Select columns to display`}
+							selectAllAriaLabel={`Select all columns`}
+							resetToDefaultAriaLabel={`Reset to default columns`}
+							itemToString={(i) => i.displayName}
+							items={canChangeShowColumns}
+							defaultColumns={defaultColumns}
+							onChange={(item) => {
+								setFilterVal('');
+								onFilterChange({
+									value: '',
+									generateNextSQON: generateNextSQON(''),
+								});
+								onColumnsChange({ ...item, show: !item.show });
+							}}
+							onMultipleChange={(changes) => {
+								onMultipleColumnsChange(changes);
+							}}
+							theme={themeDropDownProps}
+						>
+							{columnDropdownText}
+						</MultiSelectDropDown>
+					) : (
+						<DropDown
+							aria-label={`Select columns`}
+							itemToString={(i) => i.displayName}
+							items={canChangeShowColumns}
+							onChange={(item) => {
+								setFilterVal('');
+								onFilterChange({
+									value: '',
+									generateNextSQON: generateNextSQON(''),
+								});
+								onColumnsChange({ ...item, show: !item.show });
+							}}
+							theme={themeDropDownProps}
+							enableSelectColumnDropdownTextFilter={enableSelectColumnDropdownTextFilter}
+						>
+							{columnDropdownText}
+						</DropDown>
+					))}
 
-        {multipleExporters ? ( // check if we're given more than one custom exporter
-          <div className="buttonWrapper">
-            <DropDown
-              aria-label={`Download options`}
-              hasSelectedRows={hasSelectedRows}
-              items={exporterArray}
-              itemToString={(i) => <MetaMorphicChild>{i.exporterLabel}</MetaMorphicChild>}
-              onChange={({
-                exporterColumns,
-                exporterLabel,
-                exporterFileName,
-                exporterFunction,
-                exporterMaxRows,
-                exporterRequiresRowSelection,
-              }) =>
-                (exporterRequiresRowSelection && !hasSelectedRows) ||
-                exporterFunction?.(
-                  transformParams({
-                    files: [
-                      {
-                        allColumns,
-                        columns,
-                        documentType,
-                        maxRows: exporterMaxRows,
-                        fileName: exporterFileName
-                          ? `${exporterFileName}${
-                              exporterFileName.toLowerCase().endsWith('.tsv') ? '' : '.tsv'
-                            }`
-                          : `${stringCleaner(exporterLabel.toLowerCase())}.tsv`,
-                        fileType: 'tsv',
-                        sqon: downloadSqon,
-                        ...(exporterColumns && { exporterColumns }),
-                      },
-                    ],
-                    selectedTableRows,
-                    url: downloadUrl,
-                  }),
-                  download,
-                )
-              }
-              singleSelect={true}
-              theme={themeDropDownProps}
-            >
-              {exporterLabel}
-            </DropDown>
-          </div>
-        ) : (
-          // else, use a custom function if any is given, or use the default saveTSV if the flag is on
-          singleExporter && (
-            <div className="buttonWrapper">
-              <Button
-                css={css`
-                  display: flex;
-                  min-height: 16;
-                  white-space: nowrap;
+				{multipleExporters ? ( // check if we're given more than one custom exporter
+					<div className="buttonWrapper">
+						<DropDown
+							aria-label={`Download options`}
+							hasSelectedRows={hasSelectedRows}
+							items={exporterArray}
+							itemToString={(i) => <MetaMorphicChild>{i.exporterLabel}</MetaMorphicChild>}
+							onChange={({
+								exporterColumns,
+								exporterLabel,
+								exporterFileName,
+								exporterFunction,
+								exporterMaxRows,
+								exporterRequiresRowSelection,
+							}) =>
+								(exporterRequiresRowSelection && !hasSelectedRows) ||
+								exporterFunction?.(
+									transformParams({
+										files: [
+											{
+												allColumns,
+												columns,
+												documentType,
+												maxRows: exporterMaxRows,
+												fileName: exporterFileName
+													? `${exporterFileName}${
+															exporterFileName.toLowerCase().endsWith('.tsv') ? '' : '.tsv'
+													  }`
+													: `${stringCleaner(exporterLabel.toLowerCase())}.tsv`,
+												fileType: 'tsv',
+												sqon: downloadSqon,
+												...(exporterColumns && { exporterColumns }),
+											},
+										],
+										selectedTableRows,
+										url: downloadUrl,
+									}),
+									download,
+								)
+							}
+							singleSelect={true}
+							theme={themeDropDownProps}
+						>
+							{exporterLabel}
+						</DropDown>
+					</div>
+				) : (
+					// else, use a custom function if any is given, or use the default saveTSV if the flag is on
+					singleExporter && (
+						<div className="buttonWrapper">
+							<Button
+								css={css`
+									display: flex;
+									min-height: 16;
+									white-space: nowrap;
 
-                  &:not(:disabled):hover {
-                    cursor: pointer;
-                  }
-                `}
-                disabled={exporter?.[0]?.requiresRowSelection && !hasSelectedRows}
-                onClick={() => {
-                  (exporter?.[0]?.requiresRowSelection && !hasSelectedRows) ||
-                    singleExporter(
-                      transformParams({
-                        files: [
-                          {
-                            columns,
-                            documentType,
-                            fileName: exportTSVFilename || `${documentType}-table.tsv`,
-                            fileType: 'tsv',
-                            sqon: downloadSqon,
-                          },
-                        ],
-                        selectedTableRows,
-                        url: downloadUrl,
-                      }),
-                      download,
-                    );
-                }}
-              >
-                {exportTSVText}
-              </Button>
-            </div>
-          )
-        )}
-        {customActions}
-      </div>
-    </div>
-  );
+									&:not(:disabled):hover {
+										cursor: pointer;
+									}
+								`}
+								disabled={exporter?.[0]?.requiresRowSelection && !hasSelectedRows}
+								onClick={() => {
+									(exporter?.[0]?.requiresRowSelection && !hasSelectedRows) ||
+										singleExporter(
+											transformParams({
+												files: [
+													{
+														columns,
+														documentType,
+														fileName: exportTSVFilename || `${documentType}-table.tsv`,
+														fileType: 'tsv',
+														sqon: downloadSqon,
+													},
+												],
+												selectedTableRows,
+												url: downloadUrl,
+											}),
+											download,
+										);
+								}}
+							>
+								{exportTSVText}
+							</Button>
+						</div>
+					)
+				)}
+				{customActions}
+			</div>
+		</div>
+	);
 };
 
 export default enhance(TableToolbar);
--- modules/components/src/DropDown/DropDown.js
+++ modules/components/src/DropDown/DropDown.js
@@ -5,135 +5,161 @@ import { css } from '@emotion/react';
 import { ArrowIcon } from '@/Icons';
 import noopFn, { emptyObj } from '@/utils/noops';
 import { withTheme } from '@/ThemeContext';
+import TextFilter from '@/TextFilter';
+import TextHighlight from '@/TextHighlight';
+import internalTranslateSQONValue from '@/utils/translateSQONValue';
+import strToReg from '@/utils/strToReg';
 
 import './DropDown.css';
 
 class DropDown extends React.Component {
-  state = { isOpen: false };
-  handleToggleMenu = (event) => {
-    event.target?.attributes?.disabled || this.setState(({ isOpen }) => ({ isOpen: !isOpen }));
-  };
-  handleStateChange = (changes) => {
-    const { isOpen, type } = changes;
+	state = { isOpen: false, searchText: '' };
+	handleToggleMenu = (event) => {
+		event.target?.attributes?.disabled || this.setState(({ isOpen }) => ({ isOpen: !isOpen }));
+	};
+	handleStateChange = (changes) => {
+		const { isOpen, type } = changes;
 
-    if (type === '__autocomplete_click_button__') {
-      this.setState({ isOpen: !isOpen });
-    }
-  };
+		if (type === '__autocomplete_click_button__') {
+			this.setState({ isOpen: !isOpen });
+		}
+	};
 
-  render() {
-    const { isOpen } = this.state;
-    const {
-      arrowColor: customArrowColor,
-      arrowTransition: customArrowTransition,
-      hasSelectedRows,
-      items,
-      onChange = noopFn,
-      itemToString,
-      children,
-      align = 'right',
-      singleSelect = false,
-      theme: {
-        components: {
-          DropDown: {
-            arrowColor: themeArrowColor,
-            arrowTransition: themeArrowTransition,
-            ...themeArrowProps
-          } = emptyObj,
-        } = emptyObj,
-      } = emptyObj,
-    } = this.props;
+	render() {
+		const { isOpen, searchText } = this.state;
+		const {
+			arrowColor: customArrowColor,
+			arrowTransition: customArrowTransition,
+			hasSelectedRows,
+			items,
+			onChange = noopFn,
+			itemToString,
+			children,
+			align = 'right',
+			singleSelect = false,
+			enableSelectColumnDropdownTextFilter = false,
+			searchPlaceholder = 'Search',
+			theme: {
+				components: {
+					DropDown: {
+						arrowColor: themeArrowColor,
+						arrowTransition: themeArrowTransition,
+						...themeArrowProps
+					} = emptyObj,
+				} = emptyObj,
+			} = emptyObj,
+		} = this.props;
 
-    const disableDownloads =
-      items.every((item) => item.exporterRequiresRowSelection) && !hasSelectedRows;
+		const disableDownloads =
+			items.every((item) => item.exporterRequiresRowSelection) && !hasSelectedRows;
 
-    return (
-      <Downshift
-        itemToString={itemToString}
-        onChange={onChange}
-        selectedItem={items.filter((item) => item.show)}
-        isOpen={isOpen}
-        onStateChange={this.handleStateChange}
-      >
-        {({
-          getInputProps,
-          getButtonProps,
-          getItemProps,
-          isOpen,
-          toggleMenu,
-          clearSelection,
-          selectedItem,
-          inputValue,
-          highlightedIndex,
-        }) => (
-          <div className="dropDownHeader">
-            <button
-              aria-label={`Show columns to select`}
-              className="dropDownButton"
-              {...getButtonProps({
-                disabled: disableDownloads,
-                onClick: this.handleToggleMenu,
-              })}
-            >
-              <div className="dropDownButtonContent">{children}</div>
-              <ArrowIcon
-                css={css`
-                  margin-left: 0.3rem;
-                  margin-top: 0.1rem;
-                `}
-                fill={customArrowColor || themeArrowColor}
-                pointUp={isOpen}
-                transition={customArrowTransition || themeArrowTransition}
-                {...themeArrowProps}
-              />
-            </button>
+		return (
+			<Downshift
+				itemToString={itemToString}
+				onChange={onChange}
+				selectedItem={items.filter((item) => item.show)}
+				isOpen={isOpen}
+				onStateChange={this.handleStateChange}
+			>
+				{({
+					getInputProps,
+					getButtonProps,
+					getItemProps,
+					isOpen,
+					toggleMenu,
+					clearSelection,
+					selectedItem,
+					inputValue,
+					highlightedIndex,
+				}) => (
+					<div className="dropDownHeader">
+						<button
+							aria-label={`Show columns to select`}
+							className="dropDownButton"
+							{...getButtonProps({
+								disabled: disableDownloads,
+								onClick: this.handleToggleMenu,
+							})}
+						>
+							<div className="dropDownButtonContent">{children}</div>
+							<ArrowIcon
+								css={css`
+									margin-left: 0.3rem;
+									margin-top: 0.1rem;
+								`}
+								fill={customArrowColor || themeArrowColor}
+								pointUp={isOpen}
+								transition={customArrowTransition || themeArrowTransition}
+								{...themeArrowProps}
+							/>
+						</button>
 
-            {isOpen && (
-              <div
-                className={`dropDownContent ${singleSelect ? 'single' : 'multiple'}`}
-                style={{
-                  right: align === 'right' ? 0 : 'auto',
-                  left: align === 'right' ? 'auto' : 0,
-                }}
-                {...(singleSelect && { onClick: this.handleToggleMenu })}
-              >
-                {items.map((item, index) => {
-                  const { id, ...itemProps } = getItemProps({
-                    item,
-                    index,
-                    disabled: item.exporterRequiresRowSelection && !hasSelectedRows,
-                  });
-                  const label = itemToString(item);
-                  const labelIsComponent = React.isValidElement(label);
-                  return (
-                    <div
-                      className={`dropDownContentElement${
-                        labelIsComponent ? ' custom' : ' clickable'
-                      }`}
-                      key={item.id || id}
-                      {...itemProps}
-                    >
-                      {label}
-                      {!(singleSelect || labelIsComponent) && (
-                        <input
-                          readOnly
-                          type="checkbox"
-                          checked={selectedItem.indexOf(item) > -1}
-                          aria-label={`Select column ${
-                            item.id || (typeof label === 'string' ? label : id)
-                          }`}
-                        />
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-        )}
-      </Downshift>
-    );
-  }
+						{isOpen && (
+							<div
+								className={`dropDownContent ${singleSelect ? 'single' : 'multiple'}`}
+								style={{
+									right: align === 'right' ? 0 : 'auto',
+									left: align === 'right' ? 'auto' : 0,
+								}}
+								{...(singleSelect && { onClick: this.handleToggleMenu })}
+							>
+								{enableSelectColumnDropdownTextFilter && (
+									<TextFilter
+										aria-label={`Search data`}
+										onChange={({ value }) => this.setState({ searchText: value || '' })}
+										placeholder={searchPlaceholder}
+										type="text"
+										value={searchText}
+									/>
+								)}
+								{items
+									.filter(
+										(item) =>
+											!searchText ||
+											internalTranslateSQONValue(itemToString(item)).match(strToReg(searchText)),
+									)
+									// .filter((item) => itemToString(item).toLowerCase() == searchText)
+									.map((item, index) => {
+										const { id, ...itemProps } = getItemProps({
+											item,
+											index,
+											disabled: item.exporterRequiresRowSelection && !hasSelectedRows,
+										});
+										const label = itemToString(item);
+										const labelIsComponent = React.isValidElement(label);
+										return (
+											<div
+												className={`dropDownContentElement${
+													labelIsComponent ? ' custom' : ' clickable'
+												}`}
+												key={item.id || id}
+												{...itemProps}
+											>
+												{enableSelectColumnDropdownTextFilter ? (
+													<TextHighlight content={label} highlightText={searchText} />
+												) : (
+													label
+												)}
+												{!(singleSelect || labelIsComponent) && (
+													<input
+														readOnly
+														type="checkbox"
+														checked={selectedItem.indexOf(item) > -1}
+														aria-label={`Select column ${
+															item.id || (typeof label === 'string' ? label : id)
+														}`}
+													/>
+												)}
+											</div>
+										);
+									})}
+							</div>
+						)}
+					</div>
+				)}
+			</Downshift>
+		);
+	}
 }
 
 export default withTheme(DropDown);
