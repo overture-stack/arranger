7b4aec555636d03d71a11f2abfaa1450c2a46cae
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-06-05 11:18:47 -0400

    add fetch field

--- modules/server/src/network/index.ts
+++ modules/server/src/network/index.ts
@@ -1,10 +1,13 @@
 import { NetworkAggregationInterface } from '@/config/types';
 
+const fetchRemoteSchemas = (networkConfig: NetworkAggregationInterface[]) => {};
+
 export const createSchemaFromNetworkConfig = ({
 	networkConfig,
 }: {
 	networkConfig: NetworkAggregationInterface[];
 }) => {
 	console.log('network config', networkConfig);
+	const remoteSchemas = fetchRemoteSchemas(networkConfig);
 };
 export const mergeSchemas = () => {};
