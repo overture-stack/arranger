45761b3e4c3817fc6a8898348024690ffc467825
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-06-08 15:32:11 -0400

    example

--- modules/server/src/network/index.ts
+++ modules/server/src/network/index.ts
@@ -1,16 +1,17 @@
 import { NetworkAggregationInterface } from '@/config/types';
-import { buildClientSchema, GraphQLObjectType, GraphQLSchema } from 'graphql';
-import { GraphQLNonEmptyString } from 'graphql-scalars';
+import { buildClientSchema } from 'graphql';
 import { fetchGql, getIntrospectionQuery } from './gql';
 import { toJSON } from './util';
 
-const validateField = (field) => {
-	// todo: account for array eg. [Donor]
-	const value = field.type.name;
-	return acceptedFieldTypes.includes(value);
-};
-
-const validateFields = (x) => x;
+/**
+ * 
+network config:
+		{
+			"displayName": "Barc",
+			"graphqlUrl": "http://localhost:7070/",
+			"documentType": "Aggs"
+		}
+ */
 
 const fetchRemoteSchemas = async ({
 	networkConfig,
@@ -41,6 +42,7 @@ const fetchRemoteSchemas = async ({
 	);
 
 	const schemaResults = networkQueries.map((networkResult) => {
+		// responses need to be cleaned up
 		const response = { id: '', schema: undefined, errors: undefined, config };
 		if (networkResult.status === 'fulfilled') {
 			return {
@@ -54,54 +56,29 @@ const fetchRemoteSchemas = async ({
 		}
 	});
 
-	console.log(schemaResults);
 	return schemaResults;
 };
 
-const acceptedFieldTypes = ['Aggregations', 'NumericAggregations']; // todo: config
-const config = { acceptedDocumentTypes: ['Aggregations', 'NumericAggregations'] };
 export const createSchemaFromNetworkConfig = async ({
 	networkConfig,
 }: {
 	networkConfig: NetworkAggregationInterface[];
 }) => {
 	try {
-		/**
-		 * Query remote nodes for schema
-		 */
+		// query remote nodes
 		const remoteSchemasResult = await fetchRemoteSchemas({
 			networkConfig,
-			config: {},
 		});
 
-		/**
-		 * Merge remote schemas
-		 */
-		const mergedFields = remoteSchemasResult.filter((s) => s.schema).reduce(mergeSchemas, {});
-
-		// this root of merged fields, needs to be wrapped in GraphObjectType also
-		// formatted correctly to construct schema
-		const xxx = new GraphQLObjectType({
-			name: 'network',
-			fields: mergedFields,
-		});
-
-		const otherSchema = new GraphQLSchema({
-			query: xxx,
-		});
-		console.log('network schema', otherSchema);
+		// merge remote schemas
+		const mergedFields = mergeRemoteSchemas(remoteSchemasResult);
 	} catch (error) {
-		//TOOD handle remote schemas not working
-
 		console.log(error);
 	}
 };
 
-export const mergeSchemas = (acc, schema) => {
-	const documentType = schema.config.documentType;
-	console.log('sc', schema.schema);
-	const fields = schema.schema['_typeMap'][documentType]['_fields'];
-	return Object.entries(fields).reduce((acc, [key, value]) => {
-		return { ...acc, [key]: { type: GraphQLNonEmptyString } };
-	}, {});
+const mergeRemoteSchemas = (remoteSchemasResult) => {
+	// want to reuse config in here to pull out `documentType`
+	// alternative is to it in as 2nd param to mergeRemoteSchemas
+	// but then I have to link remoteSchemasResult.schema <===> networkConfig[node].documentType
 };
