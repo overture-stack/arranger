7b82c9e3cf1cebc880b14a1a0fe42ffc5c64827c
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-06-07 21:49:29 -0400

    fix GQL obj wrapping on schema construction

--- modules/server/src/network/index.ts
+++ modules/server/src/network/index.ts
@@ -1,19 +1,11 @@
 import { NetworkAggregationInterface } from '@/config/types';
-import { buildClientSchema, print } from 'graphql';
-import gql from 'graphql-tag';
+import { buildClientSchema, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { GraphQLNonEmptyString } from 'graphql-scalars';
 import { fetchGql, getIntrospectionQuery } from './gql';
+import { toJSON } from './util';
 
-const REMOTE_SERVER_STATUS = {
-	CONNECTED: 'connected',
-	DISCONNECTED: 'disconnected',
-} as const;
-
-const toJSON = (resp) => resp.json(); // todo: util
-
-// validate step
 const validateField = (field) => {
 	// todo: account for array eg. [Donor]
-	// todo: nested types
 	const value = field.type.name;
 	return acceptedFieldTypes.includes(value);
 };
@@ -21,14 +13,17 @@ const validateField = (field) => {
 const validateFields = (x) => x;
 
 const fetchRemoteSchemas = async ({
-	availableConnections,
+	networkConfig,
 }: {
-	availableConnections: NetworkAggregationInterface[];
+	networkConfig: NetworkAggregationInterface[];
 }) => {
-	const schemas = await Promise.allSettled(
-		availableConnections.map(async (config) => {
-			const { graphqlUrl, documentType } = config;
+	const networkQueries = await Promise.allSettled(
+		networkConfig.map(async (config) => {
+			const { graphqlUrl } = config;
 			try {
+				/**
+				 * get full schema (needed for buildClientSchema) and convert json to gql object
+				 */
 				const remoteSchema = await fetchGql({
 					url: graphqlUrl,
 					gqlRequest: { query: getIntrospectionQuery() },
@@ -36,31 +31,32 @@ const fetchRemoteSchemas = async ({
 					.then(toJSON)
 					.then((schemaJSON) => buildClientSchema(schemaJSON.data));
 
-				return remoteSchema;
+				return { config, res: remoteSchema };
 			} catch (error) {
 				console.log(`Failed to retrieve schema from url: ${config.graphqlUrl}`);
 				console.error(error);
-				throw error;
-			}
-		}),
-	).then((schemaResults) =>
-		schemaResults.map((result) => {
-			const response = { id: '', schemaJSON: undefined, errors: undefined };
-			if (result.status === 'fulfilled') {
-				return { ...response, schemaJSON: result.value, errors: [] };
-			} else {
-				return { ...response, schemaJSON: null, errors: [result.reason] };
+				return { config, res: error };
 			}
 		}),
 	);
 
-	console.log(schemas);
-	return schemas;
+	const schemaResults = networkQueries.map((networkResult) => {
+		const response = { id: '', schema: undefined, errors: undefined, config };
+		if (networkResult.status === 'fulfilled') {
+			return {
+				...response,
+				schema: networkResult.value.res,
+				config: networkResult.value.config,
+				errors: [],
+			};
+		} else {
+			return { ...response, schema: null, config: null, errors: [networkResult.reason] };
+		}
+	});
+
+	console.log(schemaResults);
+	return schemaResults;
 };
-
-const ping = (x) => x;
-
-const everyAll = () => {};
 
 const acceptedFieldTypes = ['Aggregations', 'NumericAggregations']; // todo: config
 const config = { acceptedDocumentTypes: ['Aggregations', 'NumericAggregations'] };
@@ -70,26 +66,42 @@ export const createSchemaFromNetworkConfig = async ({
 	networkConfig: NetworkAggregationInterface[];
 }) => {
 	try {
-		const availableConnections = await ping(networkConfig);
-		const remoteSchemas: { id: string; schemaJSON: any } = await fetchRemoteSchemas({
-			availableConnections,
+		/**
+		 * Query remote nodes for schema
+		 */
+		const remoteSchemasResult = await fetchRemoteSchemas({
+			networkConfig,
 			config: {},
 		});
-		console.log('r', remoteSchemas);
-		//const m = mergeSchemas(remoteSchemasJSON);
-	} catch (error) {}
 
-	const schema = gql`
-		{
-			network
-		}
-	`;
+		/**
+		 * Merge remote schemas
+		 */
+		const mergedFields = remoteSchemasResult.filter((s) => s.schema).reduce(mergeSchemas, {});
+
+		// this root of merged fields, needs to be wrapped in GraphObjectType also
+		// formatted correctly to construct schema
+		const xxx = new GraphQLObjectType({
+			name: 'network',
+			fields: mergedFields,
+		});
+
+		const otherSchema = new GraphQLSchema({
+			query: xxx,
+		});
+		console.log('network schema', otherSchema);
+	} catch (error) {
+		//TOOD handle remote schemas not working
+
+		console.log(error);
+	}
 };
 
-// !TODO tricky with diff arranger versions
-// node could have more fields, less fields etc
-export const mergeSchemas = (s) => {
-	s.reduce((acc, current) => {
-		return { acc, ...current };
+export const mergeSchemas = (acc, schema) => {
+	const documentType = schema.config.documentType;
+	console.log('sc', schema.schema);
+	const fields = schema.schema['_typeMap'][documentType]['_fields'];
+	return Object.entries(fields).reduce((acc, [key, value]) => {
+		return { ...acc, [key]: { type: GraphQLNonEmptyString } };
 	}, {});
 };
