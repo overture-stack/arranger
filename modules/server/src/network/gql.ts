09738b20854b9f392338b4c1886bb91dcc70ae90
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-06-08 23:50:04 -0400

    cleanup gql file

--- modules/server/src/network/gql.ts
+++ modules/server/src/network/gql.ts
@@ -1,39 +1,11 @@
-import { getIntrospectionQuery, print } from 'graphql';
-import gql from 'graphql-tag';
+import { print } from 'graphql';
 
-// export const introspectionQueryGql = gql`
-// 	query ($name: String!) {
-// 		__type(name: $name) {
-// 			name
-// 			fields {
-// 				name
-// 				type {
-// 					name
-// 				}
-// 			}
-// 		}
-// 	}
-// `;
-export const introspectionQueryGql = getIntrospectionQuery();
-
-//export const fullDocumentQueryGql = gql``;
-
-// TS THIS?
 export const createGqlQuery = ({ query: queryAST, variables }) => {
 	// queries need to be strings when using regular http
 	const query = print(queryAST);
 	return JSON.stringify({ query, variables });
 };
 
-export const createIntrospectionQuery = (documentType: string) => {
-	const queryAST = introspectionQueryGql;
-	// queries need to be strings when using regular http
-	const query = print(queryAST);
-	const variables = { name: documentType };
-	return JSON.stringify({ query, variables });
-};
-
-type GqlRequest = { query: any; variables: any }; // TOOD: native type?
 export const fetchGql = ({ url, gqlRequest }) => {
 	const query = JSON.stringify(gqlRequest);
 	const options = {
@@ -44,5 +16,3 @@ export const fetchGql = ({ url, gqlRequest }) => {
 
 	return fetch(url, options);
 };
-
-export { getIntrospectionQuery } from 'graphql';
