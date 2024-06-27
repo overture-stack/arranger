4b7fc2c220c507d7dd4e2e686b26a9a9276d8a01
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-06-27 17:45:34 -0400

    add test watch command with basic test

---
+++ modules/server/src/network/__tests__/index.test.ts
@@ -0,0 +1,11 @@
+/**
+ * Basic test
+ */
+
+test('x', () => {
+	const expectedOutput: any[] = [];
+
+	const actualOutput: any[] = [];
+
+	const result = expect(actualOutput).toEqual(expectedOutput);
+});
