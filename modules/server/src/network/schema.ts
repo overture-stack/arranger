78804fb827a487bcc5cb99a37374d63545154986
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-07-03 14:02:31 -0400

    Fix type. narrow scope. hoist filtering

--- modules/server/src/network/schema.ts
+++ modules/server/src/network/schema.ts
@@ -1,38 +1,21 @@
 import { Config, mergeTypeDefs } from '@graphql-tools/merge';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { TypeSource } from '@graphql-tools/utils';
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
-import { NetworkAggregationConfig } from './types';
+import { DocumentNode, GraphQLSchema } from 'graphql';
 
 /**
- * string array of field names to exclude
- * @returns string[]
+ *
+ * @remarks
+ * mergeTypeDefs function can handle a lot of options including
+ * exclusions and customising how to handle conflicts.
+ * See full documentation here: https://the-guild.dev/graphql/tools/docs/api/modules/merge_src#mergetypedefs
+ *
+ * @param networkConfigs
+ * @returns
  */
-const generateExclusions = (config: any): string[] => {
-	return [];
-};
-
-// return schema graphobject schema
-export const createNetworkAggregationSchema = (newtworkConfigs: NetworkAggregationConfig[]) => {
-	// filter remote connections with valid schemas
-	const typesArr: TypeSource = newtworkConfigs
-		.filter((c) => c.schema !== null)
-		.map((c) => c.schema as GraphQLSchema);
-
-	const exclusions = generateExclusions(config);
-
-	// https://the-guild.dev/graphql/tools/docs/api/modules/merge_src#mergetypedefs
+export const createNetworkAggregationTypeDefs = (gqlTypes: GraphQLSchema[]): DocumentNode => {
 	const options: Omit<Partial<Config>, 'commentDescriptions'> = {
 		forceSchemaDefinition: true,
-		//onFieldTypeConflict: (x) => console.log(x),
-		exclusions,
 	};
+	const typeDefs = mergeTypeDefs(gqlTypes, options);
 
-	const typeDefs = mergeTypeDefs(typesArr, options);
-	console.log(typeDefs);
-	// const x = new GraphQLObjectType({name: 'network', fields: })
-
-	const networkSchema = makeExecutableSchema({ typeDefs });
-
-	return networkSchema;
+	return typeDefs;
 };
