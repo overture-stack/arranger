504bed171ece1f7bfd8ed7d4996ea4a5e7aaee67
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-07-17 15:57:11 -0400

    query remote connections

--- modules/server/src/network/resolvers/aggregations.ts
+++ modules/server/src/network/resolvers/aggregations.ts
@@ -1,9 +1,8 @@
-import { ObjectValues } from '@/utils/types';
-import { gql } from 'apollo-server-core';
 import { fetchGql } from '../gql';
+import { remoteConnectionQuery } from '../queries';
 import { NetworkAggregationConfig, NetworkFieldType } from '../types';
 
-const getConnectionsToQuery = (
+const getConnectionURLs = (
 	field: NetworkFieldType,
 	configs: NetworkAggregationConfig[],
 ): string[] => {
@@ -12,52 +11,63 @@ const getConnectionsToQuery = (
 		.map((config) => config.graphqlUrl);
 };
 
-const q = `#graphql
-query q(x: String) 
-	{
-		aggs {
-                donors {
-                        gender
-                }
-        }
-}
-
-	
-`;
-
-const networkToSingleQuery = {
-	NetworkAggregations: q,
-	NumericNetworkAggregations: q,
+const getGQLQuery = (fieldType: string) => {
+	const query = remoteConnectionQuery.get(fieldType);
+	if (query) {
+		return query;
+	} else {
+		throw Error('Query not found for field');
+	}
 };
 
-const queryRemoteConnection = (url: string, fieldType: string) => {
-	const gqlQuery = networkToSingleQuery[fieldType];
-	return fetchGql({ url, gqlQuery });
+// TODO: narrow fieldType early?
+
+const queryRemoteConnection = async (url: string, fieldType: string) => {
+	try {
+		const gqlQuery = getGQLQuery(fieldType);
+		const response = await fetchGql({ url, gqlQuery });
+		if (response.status === 200 && response.statusText === 'OK') {
+			const responseData = response.data?.data;
+			console.log('fetch success', responseData);
+			return responseData;
+		}
+		throw Error(`Query to ${url} failed`);
+	} catch (error) {
+		console.error(error);
+		return;
+	}
 };
 
-const resolveAggregation = (response: any, type: string) => {
-	// if networkagg do thing
-	/// if numericnetworkagg do thing
-};
+/**
+ * TODO: Placeholder for resolving multiple remote connection responses into single aggregation
+ */
+const resolveAggregation = (response: any, type: string) => {};
 
 /**
  *
  * @param field
  * @returns
  */
-const createResolver =
-	(field: NetworkFieldType, configs: NetworkAggregationConfig[]) => async () => {
-		const connectionsToQuery = getConnectionsToQuery(field, configs);
-		const responsePromises = connectionsToQuery
-			.map((url: string) => queryRemoteConnection(url, field.type))
-			.allSettled();
-		const responses = responsePromises.filter(
+const createResolver = (field: NetworkFieldType, configs: NetworkAggregationConfig[]) => {
+	const connectionsToQuery = getConnectionURLs(field, configs);
+
+	return async () => {
+		const connectionResponses = await Promise.allSettled(
+			connectionsToQuery.map((url: string) => queryRemoteConnection(url, field.type)),
+		);
+		// TODO: error or log if connection not made
+		const responses = connectionResponses.filter(
 			(result: { status: string }) => result.status === 'fulfilled',
 		);
 
 		const resolvedData = resolveAggregation(responses, field.type);
+
+		/**
+		 * TODO: return resolvedData
+		 */
 		return { test: field.name };
 	};
+};
 
 /**
  *
