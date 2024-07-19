d8af0aae8adadfc0016243a45ed1b6006fb7868a
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-07-19 04:06:12 -0400

    add explicit name mapping

--- modules/server/src/network/common.ts
+++ modules/server/src/network/common.ts
@@ -1,11 +1,17 @@
 import { ObjectValues } from '@/utils/types';
 
+/*
+ * Network aggregations
+ */
 export const NETWORK_AGGREGATIONS = {
-	NetworkAggregation: 'NetworkAggregation',
+	NetworkAggregations: 'NetworkAggregations',
 	NetworkNumericAggregations: 'NetworkNumericAggregations',
 } as const;
 export type NetworkAggregation = ObjectValues<typeof NETWORK_AGGREGATIONS>;
 
+/*
+ * Supported single aggregations that can be used by network aggregation
+ */
 export const SUPPORTED_AGGREGATIONS = {
 	Aggregations: 'Aggregations',
 	NumericAggregations: 'NumericAggregations',
@@ -17,3 +23,23 @@ export const SUPPORTED_AGGREGATIONS_LIST: SUPPORTED_AGGREGATIONS_TYPE[] = [
 	SUPPORTED_AGGREGATIONS.Aggregations,
 	SUPPORTED_AGGREGATIONS.NumericAggregations,
 ];
+
+/**
+ * Multiple cases when we need to map from the "single" aggregation types to the "network"
+ *
+ * @example
+ * "Aggregations" => "NetworkAggregations"
+ */
+export const singleToNetworkNameMap = new Map<SUPPORTED_AGGREGATIONS_TYPE, NetworkAggregation>()
+	.set(SUPPORTED_AGGREGATIONS.Aggregations, NETWORK_AGGREGATIONS.NetworkAggregations)
+	.set(SUPPORTED_AGGREGATIONS.NumericAggregations, NETWORK_AGGREGATIONS.NetworkNumericAggregations);
+
+/**
+ * Multiple cases when we need to map from the "network" aggregation types to the "single"
+ *
+ * @example
+ * "NetworkAggregations" => "Aggregations"
+ */
+export const networkToSingleNameMap = new Map<NetworkAggregation, SUPPORTED_AGGREGATIONS_TYPE>()
+	.set(NETWORK_AGGREGATIONS.NetworkAggregations, SUPPORTED_AGGREGATIONS.Aggregations)
+	.set(NETWORK_AGGREGATIONS.NetworkNumericAggregations, SUPPORTED_AGGREGATIONS.NumericAggregations);
