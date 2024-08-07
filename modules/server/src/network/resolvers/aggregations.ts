7834eacdd8e7b600b67f32d4b1cbd8fd1bfe1166
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-08-07 03:00:23 -0400

    rework logic to use single request and mapping fields, instead of slicing central query

--- modules/server/src/network/resolvers/aggregations.ts
+++ modules/server/src/network/resolvers/aggregations.ts
@@ -1,101 +1,120 @@
-import { SupportedAggregation } from '../common';
+import { gql } from 'apollo-server-core';
+import { DocumentNode, GraphQLResolveInfo } from 'graphql';
+import graphqlFields from 'graphql-fields';
+import { config } from 'process';
 import { fetchGql } from '../gql';
-import { remoteConnectionQuery } from '../queries';
-import { NetworkAggregationConfig, SupportedNetworkFieldType } from '../types';
+import { supportedAggregationQueries } from '../queries';
+import { NetworkAggregationConfig } from '../types';
+import { ASTtoString } from '../util';
 
 /**
- * Returns an array of connection URLs
- * @param field
- * @param configs
- * @returns
+ * Returns only top level fields from a GQL request
+ *
+ * @param info GQL request info object
+ * @returns List of top level fields
  */
-const getConnectionURLs = (
-	field: SupportedNetworkFieldType,
-	configs: NetworkAggregationConfig[],
-): string[] => {
-	return configs
-		.filter((config) => config.supportedAggregations?.find((agg) => agg.name === field.name))
-		.map((config) => config.graphqlUrl);
+const getRootFields = (info: GraphQLResolveInfo) => {
+	const requestedFields = graphqlFields(info);
+	const fieldsAsList = Object.keys(requestedFields);
+	return fieldsAsList;
 };
 
 /**
- * Returns gql query based on type of field
- * @param fieldType
+ * Query remote connections and handle network responses
+ *
+ * @param query
  * @returns
  */
-const getGQLQuery = (fieldType: SupportedAggregation) => {
-	const query = remoteConnectionQuery.get(fieldType);
-	if (query) {
-		return query;
-	} else {
-		throw Error('Query not found for field');
-	}
-};
-
-/**
- * Queries remote connection gql server with query for field
- * @param url
- * @param fieldType
- * @returns
- */
-const queryRemoteConnection = async (url: string, fieldType: SupportedAggregation) => {
+const queryConnection = async (query: NetworkQuery) => {
+	const { url, gqlQuery } = query;
 	try {
-		const gqlQuery = getGQLQuery(fieldType);
-		const response = await fetchGql({ url, gqlQuery });
+		const response = await fetchGql({
+			url,
+			gqlQuery: ASTtoString(gqlQuery),
+		});
+
+		// axios response "data" field, graphql response "data" field
+		const responseData = response.data?.data;
 		if (response.status === 200 && response.statusText === 'OK') {
-			const responseData = response.data?.data;
 			return responseData;
 		}
-		throw Error(`Query to ${url} failed`);
+
+		console.error('unexpected response data');
 	} catch (error) {
-		console.error(error);
+		/**
+		 * TODO: expand on error handling for instance of Axios error for example
+		 */
+		console.error(`network failure`, error);
 		return;
 	}
 };
 
-/**
- * TODO: Placeholder for resolving multiple remote connection responses into single aggregation
- */
-const resolveAggregation = (response: any, type: string) => {};
+export const queryConnections = async (queries: NetworkQuery[]) => {
+	const networkQueryPromises = queries.map(async (query) => {
+		const response = await queryConnection(query);
+		return { config, response };
+	});
 
-/**
- * Creates resolver function
- * @param field
- * @returns resolver function
- */
-const createResolver = (field: SupportedNetworkFieldType, configs: NetworkAggregationConfig[]) => {
-	const connectionsToQuery = getConnectionURLs(field, configs);
+	// TODO: expand on network condition handling, eg. timeouts, single connection failure
+	const networkResults = await Promise.allSettled(networkQueryPromises);
+	return networkResults;
+};
 
-	return async () => {
-		const connectionResponses = await Promise.allSettled(
-			connectionsToQuery.map((url: string) => queryRemoteConnection(url, field.type)),
-		);
-
-		const responses = connectionResponses.filter(
-			(result: { status: string }) => result.status === 'fulfilled',
-		);
-
-		const resolvedData = resolveAggregation(responses, field.type);
-
-		/**
-		 * TODO: return resolvedData
-		 */
-		return { test: field.name };
-	};
+type NetworkQuery = {
+	url: string;
+	gqlQuery: DocumentNode;
 };
 
 /**
- * Returns a resolver map of functions
- * @param configs
- * @param allTypeDefs
- * @returns resolver map
+ * Find requested field in remote connection supported fields
+ *
+ * @param config
+ * @param fieldName
+ * @returns
  */
-export const createAggregationResolvers = (
+const findMatchedAggregationField = (config: NetworkAggregationConfig, fieldName: string) => {
+	return config.supportedAggregations.find((agg) => agg.name === fieldName);
+};
+
+/**
+ * Parse central query and build individual queries for remote connections based on available fields
+ *
+ * @param configs
+ * @param info
+ * @returns
+ */
+export const createNetworkQueries = (
 	configs: NetworkAggregationConfig[],
-	networkFieldTypes: SupportedNetworkFieldType[],
-) => {
-	return networkFieldTypes.reduce(
-		(resolvers, field) => ({ ...resolvers, [field.name]: createResolver(field, configs) }),
-		{},
-	);
+	info: GraphQLResolveInfo,
+): NetworkQuery[] => {
+	const rootQueryFields = getRootFields(info);
+
+	const queries = configs
+		.map((config) => {
+			// construct gql string { [fieldName] { [Aggregation query fields] } }
+			const gqlString = rootQueryFields.reduce((gqlString, fieldName) => {
+				const matchedAggregationField = findMatchedAggregationField(config, fieldName);
+				if (matchedAggregationField) {
+					const { name, type } = matchedAggregationField;
+					// get gql query string for supported aggregation
+					const aggregationFieldQueryString = supportedAggregationQueries.get(type);
+					return gqlString + `${name}${aggregationFieldQueryString}`;
+				}
+				return gqlString;
+			}, '');
+
+			/*
+			 * convert string to AST object to use as query
+			 * not needed if gqlString is formatted correctly but this acts as a validity check
+			 */
+			try {
+				const gqlQuery = gql`{${gqlString}}`;
+				return { url: config.graphqlUrl, gqlQuery };
+			} catch (err) {
+				console.error(err);
+				return false;
+			}
+		})
+		.filter(Boolean);
+	return queries;
 };
