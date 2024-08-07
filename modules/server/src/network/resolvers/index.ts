5c052a56302857b901949c608370a20ccfdc7ed3
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-08-07 02:58:17 -0400

    move aggregations resolver code into a module

--- modules/server/src/network/resolvers/index.ts
+++ modules/server/src/network/resolvers/index.ts
@@ -1,24 +1,36 @@
-import { NetworkAggregationConfig, SupportedNetworkFieldType } from '../types';
-import { createAggregationResolvers } from './aggregations';
+import { type GraphQLResolveInfo } from 'graphql';
+import { resolveAggregations } from '../aggregations';
+import { NetworkAggregationConfig, RemoteConnectionData } from '../types';
+import { createNetworkQueries, queryConnections } from './aggregations';
 import { resolveRemoteConnectionNodes } from './remoteConnections';
 
+type NetworkSearchRoot = {
+	nodes: RemoteConnectionData[];
+	aggregations: Record<string, unknown>;
+};
+
 /**
- * Create GQL resolvers
+ * Create GQL resolvers.
  * @param networkConfigsWithSchemas
  * @param networkFieldTypes
  * @returns
  */
-export const createResolvers = (
-	configs: NetworkAggregationConfig[],
-	networkFieldTypes: SupportedNetworkFieldType[],
-) => {
-	const aggregationsResolvers = createAggregationResolvers(configs, networkFieldTypes);
-
+export const createResolvers = (configs: NetworkAggregationConfig[]) => {
 	return {
 		Query: {
 			nodes: async () => await resolveRemoteConnectionNodes(configs),
-			aggregations: () => [], // TODO: Why do we need truthy val returned?
+			aggregations: async (
+				parent: NetworkSearchRoot,
+				args: {},
+				context: unknown,
+				info: GraphQLResolveInfo,
+			) => {
+				const networkQueries = createNetworkQueries(configs, info);
+				const networkResults = await queryConnections(networkQueries);
+				// TODO: implement aggregating
+				const resolvedResults = resolveAggregations(networkResults);
+				return resolvedResults;
+			},
 		},
-		Aggregations: aggregationsResolvers,
 	};
 };
