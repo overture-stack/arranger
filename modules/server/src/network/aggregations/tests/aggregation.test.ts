112fa1a5a4a5ebc99cd90f5a2d9700b4d32d29ed
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-10-01 16:27:54 -0400

    add first test for requested fields

--- modules/server/src/network/aggregations/tests/aggregation.test.ts
+++ modules/server/src/network/aggregations/tests/aggregation.test.ts
@@ -1,27 +1,10 @@
-import { resolveAggregation } from '..';
-import { aggregation } from './fixture';
+import { AggregationAccumulator } from '../AggregationAccumulator';
 
-describe('Aggregation', () => {
-	it('should resolve multiple objects of type Aggregation into a single object of type NetworkAggregation', () => {
-		const result = resolveAggregation([aggregation.inputA, aggregation.inputB]);
+describe('Network aggregation resolution', () => {
+	it('should compute requested keys from info map constructor param', () => {
+		const requestedFields = { donors_age: {}, donors_gender: {} };
+		const totalAggs = new AggregationAccumulator(requestedFields);
 
-		const resultForMaleCount =
-			result.buckets.find((bucket) => bucket.key === 'Male')?.doc_count || 0;
-		const resultForFemaleCount =
-			result.buckets.find((bucket) => bucket.key === 'Female')?.doc_count || 0;
-
-		const expectedMaleCount =
-			(aggregation.inputA.buckets.find((bucket) => bucket.key === 'Male')?.doc_count || 0) +
-			(aggregation.inputB.buckets.find((bucket) => bucket.key === 'Male')?.doc_count || 0);
-
-		const expectedFemaleCount =
-			(aggregation.inputA.buckets.find((bucket) => bucket.key === 'Female')?.doc_count || 0) +
-			(aggregation.inputB.buckets.find((bucket) => bucket.key === 'Female')?.doc_count || 0);
-
-		expect(result.bucket_count).toEqual(
-			aggregation.inputA.bucket_count + aggregation.inputB.bucket_count,
-		);
-		expect(resultForMaleCount).toEqual(expectedMaleCount);
-		expect(resultForFemaleCount).toEqual(expectedFemaleCount);
+		expect(totalAggs.requestedFields.sort()).toEqual(Object.keys(requestedFields).sort());
 	});
 });
