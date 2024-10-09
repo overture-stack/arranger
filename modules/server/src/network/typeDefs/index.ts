8f745a4afa67fc8a96d34c39dd59647d1e60b893
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-10-09 01:32:57 -0400

    remove unused typeDefs, simplify creation

--- modules/server/src/network/typeDefs/index.ts
+++ modules/server/src/network/typeDefs/index.ts
@@ -1,10 +1,7 @@
-import { mergeTypeDefs } from '@graphql-tools/merge';
 import { SupportedNetworkFieldType } from '../types/types';
 import { createNetworkAggregationTypeDefs } from './aggregations';
-import { remoteConnectionTypes } from './remoteConnections';
 
 export const createTypeDefs = (networkFieldTypes: SupportedNetworkFieldType[]) => {
-	const aggregationTypes = createNetworkAggregationTypeDefs(networkFieldTypes);
-	const typeDefs = mergeTypeDefs([remoteConnectionTypes, aggregationTypes]);
+	const typeDefs = createNetworkAggregationTypeDefs(networkFieldTypes);
 	return typeDefs;
 };
