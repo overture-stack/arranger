a16623c9af2e890f1307f4cbcd71ee7b7fc45dfb
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2024-10-29 06:45:41 -0400

    Fix broken build (#907)
    * remove unsued type defs
    
    * add type watch script
    
    * fix import
    
    * fix import
    
    * type an ANY type
    
    * fix agg resolver typing
    
    * ts no-check for test file
    
    * fix Agg Accumulator types
    
    * type response formatter
    
    * fix test file type
    
    * fix gql query creation test
    
    * query back to normal test
    
    * disables tests, adds comment
    
    * fix merged file
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>

--- modules/server/src/network/tests/query.test.ts
+++ modules/server/src/network/tests/query.test.ts
@@ -12,6 +12,11 @@ describe('gql query creation', () => {
 			// whitespace doesn't matter in the gql string, there are spaces for readability in code
 			.replaceAll(' ', '');
 
-		expect(result).toEqual('{testFile{hits{total}aggregations{donors{buckets{bucket_count}}}}}');
+		const expected =
+			'query nodeQuery($filters: JSON, $aggregations_filter_themselves: Boolean, $include_missing: Boolean) {testFile { hits { total }  aggregations(filters:$filters,aggregations_filter_themselves:$aggregations_filter_themselves,include_missing:$include_missing){donors{buckets{bucket_count}}} }}'.replaceAll(
+				' ',
+				'',
+			);
+		expect(result).toEqual(expected);
 	});
 });
