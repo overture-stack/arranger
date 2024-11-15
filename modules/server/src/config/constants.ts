064c3bc020d6e9cbe287e3bcd93aa9004941c007
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-11-15 09:28:42 -0500

    alphabetize

--- modules/server/src/config/constants.ts
+++ modules/server/src/config/constants.ts
@@ -9,6 +9,7 @@ export const DOCUMENT_TYPE = process.env.DOCUMENT_TYPE || '';
 export const DOWNLOAD_STREAM_BUFFER_SIZE =
 	stringToNumber(process.env.DOWNLOAD_STREAM_BUFFER_SIZE) || 2000;
 export const ENABLE_ADMIN = stringToBool(process.env.ENABLE_ADMIN);
+export const ENABLE_AGGREGATION_MODE = stringToBool(process.env.ENABLE_AGGREGATION_MODE);
 export const ENABLE_LOGS = stringToBool(process.env.ENABLE_LOGS);
 export const ES_HOST = process.env.ES_HOST || 'http://127.0.0.1:9200';
 export const ES_INDEX = process.env.ES_INDEX || '';
@@ -22,4 +23,3 @@ export const PING_MS = stringToNumber(process.env.PING_MS) || 2200;
 export const PING_PATH = process.env.PING_PATH || '/ping';
 export const PORT = stringToNumber(process.env.PORT) || 5050;
 export const ROW_ID_FIELD_NAME = process.env.ROW_ID_FIELD_NAME || 'id';
-export const ENABLE_AGGREGATION_MODE = stringToBool(process.env.ENABLE_AGGREGATION_MODE);
