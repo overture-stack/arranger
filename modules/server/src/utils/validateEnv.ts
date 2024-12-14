18cff80b134edf939d8f8bc3b58281dadfd1c0bd
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-12-14 17:19:15 -0500

    remove unused func

--- modules/server/src/utils/validateEnv.ts
+++
@@ -1,8 +0,0 @@
-export const validatePositiveNumber = (name: string, str: string | undefined) => {
-	const value = Number(str || '');
-	if (value > 0) {
-		return value;
-	} else {
-		throw Error(`${name} value is invalid.`);
-	}
-};
