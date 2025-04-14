040c2f099ada1c063f58c07ddd3bf6e8dd939544
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2025-04-14 16:47:53 -0400

    Update modules/server/src/mapping/resolvers.ts
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>

--- modules/server/src/mapping/resolvers.ts
+++ modules/server/src/mapping/resolvers.ts
@@ -1,5 +1,6 @@
 import { ConfigProperties, type ExtendedConfigsInterface } from '#config/types.js';
 import { type Resolver, type Root } from '#gqlServer.js';
+
 import { type CreateConnectionResolversArgs } from './createConnectionResolvers.js';
 import { applyAggregationMasking } from './masking.js';
 import getAggregationsResolver, { type AggregationsResolver, aggregationsToGraphql } from './resolveAggregations.js';
