f938e62a3181817f520fdb62606194566d327e04
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2025-06-09 15:13:03 -0400

    fix vite config alias mapping with wildcard

--- modules/charts/vite.config.ts
+++ modules/charts/vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite';
 import path, { resolve } from 'path';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
-		alias: {
-			'#': path.resolve(__dirname, './src/*'),
-		},
+		alias: [
+			{
+				find: /^#(.+)$/,
+				replacement: path.resolve('./src/$1'),
+			},
+		],
 	},
 	build: {
 		outDir: 'dist',
 		emptyOutDir: true,
 		sourcemap: true,
 		lib: {
-			entry: resolve(__dirname, 'src/main.tsx'),
+			entry: resolve(__dirname, './src/main.tsx'),
 			name: 'charts',
 			fileName: 'main',
 			formats: ['es'],
@@ -24,7 +26,7 @@ export default defineConfig({
 		},
 	},
 	plugins: [
-		// dts({
+		,// dts({
 		//   rollupTypes: true,
 		//   tsconfigPath: "./tsconfig.app.json",
 		// }),
