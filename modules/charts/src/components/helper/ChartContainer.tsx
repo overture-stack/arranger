df942dd7fed613a25c7691c868b7dc6e132ecdab
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2025-06-14 03:47:06 -0400

    storybook working

--- modules/charts/src/components/helper/ChartContainer.tsx
+++ modules/charts/src/components/helper/ChartContainer.tsx
@@ -1,24 +1,21 @@
-import { css } from "@emotion/react";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren } from 'react';
 
 /**
  * Use to render response chart correctly in container
+ * TODO: revisit - is this needed ?
  */
-export const ChartContainer = ({
-  children,
-  height,
-}: PropsWithChildren<{ height?: any }>) => (
-  <div css={css({ flex: 1, position: "relative" })}>
-    <div
-      css={css({
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: height || "100%",
-      })}
-    >
-      {children}
-    </div>
-  </div>
+export const ChartContainer = ({ children, height }: PropsWithChildren<{ height?: any }>) => (
+	<div style={{ flex: 1, position: 'relative' }}>
+		<div
+			style={{
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				width: '100%',
+				height: height || '100%',
+			}}
+		>
+			{children}
+		</div>
+	</div>
 );
