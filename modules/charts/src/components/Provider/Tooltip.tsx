e2168e9008080f834d6374f857425f16c39920ee
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2025-06-17 13:32:21 -0400

    add default tooltip comp

--- modules/charts/src/components/Provider/Tooltip.tsx
+++ modules/charts/src/components/Provider/Tooltip.tsx
@@ -17,42 +17,45 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import { css } from '@emotion/react';
 import { PropsWithChildren } from 'react';
 
 export const Tooltip = ({ children }: PropsWithChildren<{}>) => {
 	return (
-		<>
-			<style>
-				{`'.chart-tooltip &:before': {
-						content: '""',
-						display: 'block',
-						position: 'absolute',
-						width: 0,
-						height: 0,
-						border: '5px solid transparent',
-						pointerEvents: 'none',
-						right: '50%',
-						top: '100%',
-						borderTopColor: theme.colors.primary_1,
-						borderLeft: '5px solid transparent',
-						borderRight: '5px solid transparent',
-						marginLeft: '-5px',
-					}`}
-			</style>
+		<div
+			css={css({
+				fontFamily: 'Work Sans, sans-serif',
+				fontSize: '11px',
+				fontWeight: 'normal',
+				fontStyle: 'normal',
+				fontStretch: 'normal',
+				lineHeight: '1.27',
+				letterSpacing: 'normal',
+				borderRadius: ' 2px',
+				padding: '2px 4px',
+				color: 'white',
+				maxWidth: '100px',
+				maxHeight: '100px',
+				background: '#4f546d',
 
-			<div
-				className="chart-tooltip"
-				style={{
-					borderRadius: ' 2px',
-					padding: '2px 4px',
-					color: 'white',
-					maxWidth: '100px',
-					maxHeight: '100px',
-					background: '#4f546d',
-				}}
-			>
-				{children}
-			</div>
-		</>
+				'&:before': {
+					content: '""',
+					display: 'block',
+					position: 'absolute',
+					width: 0,
+					height: 0,
+					border: '5px solid transparent',
+					pointerEvents: 'none',
+					right: '50%',
+					top: '100%',
+					borderTopColor: '#4f546d',
+					borderLeft: '5px solid transparent',
+					borderRight: '5px solid transparent',
+					marginLeft: '-5px',
+				},
+			})}
+		>
+			{children}
+		</div>
 	);
 };
