e7ef2699191c22930b3ea8ea1f61d4036d0cdd6f
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2025-07-23 15:26:15 -0400

    revert barchart changes

--- modules/charts/src/components/charts/Barchart/Barchart.tsx
+++ modules/charts/src/components/charts/Barchart/Barchart.tsx
@@ -1,66 +1,29 @@
 import { ResponsiveBar } from '@nivo/bar';
 
 import { Chart } from '#components/Chart';
-import { ArrangerChartProps } from '#theme/arranger';
+import { ArrangerChartProps, ArrangerChartTheme } from '#theme/arranger';
 import { arrangerToNivoBarChart } from '#theme/nivo/nivo';
-import { css } from '@emotion/react';
-import { useRef } from 'react';
-import z from 'zod';
 
 /**
  * Resolve to a Nivo Bar chart component
  */
-export const BarChartView = ({ data, config }: ArrangerChartProps) => {
-	// create div ref for toggling css style
-	const wrapperRef = useRef(null);
-	const resolvedTheme = arrangerToNivoBarChart({ data, theme, wrapperRef });
+export const BarchartComp = ({ data, theme }: ArrangerChartProps) => {
+	const resolvedTheme = arrangerToNivoBarChart({ data, theme });
 
 	return (
-		<div
-			css={css({ width: '100%', height: '100%' })}
-			ref={wrapperRef}
-		>
-			<ResponsiveBar
-				data={data}
-				{...resolvedTheme}
-			/>
-		</div>
-	);
-};
-
-export const Barchart = ({ data, chart }: BarChartProps) => {
-	const fieldName = data.fieldNames[0];
-	const theme = {chart.xAxis.title.isVisible}
-	return (
-		<Chart
-			fieldName={fieldName}
-			theme={theme}
-			DisplayComponent={(data) => <BarChartView data={data} config={config}/>}
+		<ResponsiveBar
+			data={data}
+			{...resolvedTheme}
 		/>
 	);
 };
 
-const dataActions = z.literal(['onClick']);
-
-const AxisSchema = z.object({
-	title: z.object({
-		isVisible: z.boolean().default(true),
-	}),
-});
-
-type t = z.infer<typeof dataActions>;
-const BarChartPropsSchema = z.object({
-	data: z.object({
-		fieldNames: z.array(z.string()),
-		onAction: z.custom<({ action, data }: { action: t; data: any }) => any>(),
-		buckets: z.object({
-			orderBy: z.custom<(param: string) => string>(),
-		}),
-	}),
-	chart: z.object({
-		onAction: z.custom<({ action, data }: { action: t; data: any }) => any>(),
-		xAxis: AxisSchema,
-		yAxis: AxisSchema,
-	}),
-});
-export type BarChartProps = z.infer<typeof BarChartPropsSchema>;
+export const Barchart = ({ fieldName, theme }: { fieldName: string; theme: ArrangerChartTheme }) => {
+	return (
+		<Chart
+			fieldName={fieldName}
+			theme={theme}
+			DisplayComponent={BarchartComp}
+		/>
+	);
+};
