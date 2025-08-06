cf2381a384d84808dfb234328ed2dc7afe0bb7ee
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2025-08-06 11:20:46 -0400

    fix path - build error

--- modules/charts/src/main.tsx
+++ modules/charts/src/main.tsx
@@ -1,5 +1,4 @@
-import { BarChart } from '#components/charts/BarChart/BarChart';
-
+import { BarChart } from '#components/charts/BarChart/Barchart';
 import { HeadlessChart } from './components/Headless';
 
 import { ChartsProvider } from './components/Provider/Provider';
