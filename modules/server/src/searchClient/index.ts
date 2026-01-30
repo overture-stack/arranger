dbcce45e805a4d1db6becacad6c5fe38c184ac8e
Dan DeMaria <ddemaria@oicr.on.ca>
2026-01-30 13:24:47 -0500

    Working Test Client setup

--- modules/server/src/searchClient/index.ts
+++ modules/server/src/searchClient/index.ts
@@ -1,7 +1,16 @@
-import { Client, type ClientOptions } from '@elastic/elasticsearch';
+import { Client as ElasticClient, type ClientOptions as ESClientOptions } from '@elastic/elasticsearch';
+import { Client as OpenSearchClient, type ClientOptions as OSClientOptions } from '@opensearch-project/opensearch';
 
-const SearchClient = (options: ClientOptions) => {
-	return new Client(options);
+const SearchClient = (options: ESClientOptions | OSClientOptions) => {
+	const clientIsElasticSearch = false;
+
+	if (clientIsElasticSearch) {
+		const clientOptions = options as ESClientOptions;
+		return new ElasticClient(clientOptions);
+	} else {
+		const clientOptions = options as OSClientOptions;
+		return new OpenSearchClient(clientOptions);
+	}
 };
 
 export type SearchClientType = ReturnType<typeof SearchClient>;
