ca8179a99570e956f7ca27029d911182f43842fe
Dan DeMaria <ddemaria@oicr.on.ca>
2026-01-30 14:25:59 -0500

    Basic check for version.distribution, remove console.log

--- modules/server/src/searchClient/index.ts
+++ modules/server/src/searchClient/index.ts
@@ -1,16 +1,21 @@
 import { Client as ElasticClient, type ClientOptions as ESClientOptions } from '@elastic/elasticsearch';
 import { Client as OpenSearchClient, type ClientOptions as OSClientOptions } from '@opensearch-project/opensearch';
 
-const SearchClient = (options: ESClientOptions | OSClientOptions) => {
-	const clientIsElasticSearch = false;
+import { ENV_CONFIG } from '#config/index.js';
 
-	if (clientIsElasticSearch) {
+const SearchClient = async (options: ESClientOptions | OSClientOptions) => {
+	const { ES_HOST } = ENV_CONFIG;
+	const searchConfig = await (await fetch(ES_HOST)).json();
+	const { distribution } = searchConfig.version;
+
+	if (distribution === 'elasticsearch') {
 		const clientOptions = options as ESClientOptions;
 		return new ElasticClient(clientOptions);
-	} else {
+	} else if (distribution === 'opensearch') {
 		const clientOptions = options as OSClientOptions;
 		return new OpenSearchClient(clientOptions);
 	}
+	throw new Error(`Error configuring Search Client with distribution ${distribution}`);
 };
 
 export type SearchClientType = ReturnType<typeof SearchClient>;
