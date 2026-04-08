41cd99cecc64e1d952d500d93043c5e43eac4121
Dan DeMaria <ddemaria@oicr.on.ca>
2026-04-08 11:53:40 -0400

    Add library Client types

--- modules/server/src/searchClient/createElasticSearchClient.ts
+++ modules/server/src/searchClient/createElasticSearchClient.ts
@@ -1,15 +1,5 @@
-import {
-	Client,
-	type ClientOptions,
-	type TransportRequestOptions,
-	type IndicesClose,
-	type IndicesCreate,
-	type IndicesDelete,
-	type IndicesExists,
-	type IndicesGetMapping,
-	type IndicesPutSettings,
-	type IndicesPutMapping,
-} from '@elastic/elasticsearch';
+import { Client, type ClientOptions, type RequestParams } from '@elastic/elasticsearch';
+import { type TransportRequestOptions, type ApiResponse } from '@elastic/elasticsearch/lib/Transport';
 
 import type { SearchClient } from './types.js';
 
@@ -22,80 +12,125 @@ export function createElasticSearchClient(options: ESClientOptions) {
 
 	const searchClient: SearchClient = {
 		indices: {
-			close: async (input: IndicesClose, options?: TransportRequestOptions) => {
+			close: async (
+				input: RequestParams.IndicesClose,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.close(input, options);
 				return output;
 			},
-			create: async (input: IndicesCreate<Record<string, any>>, options?: TransportRequestOptions) => {
+			create: async (
+				input: RequestParams.IndicesCreate<Record<string, any>>,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.create(input, options);
 				return output;
 			},
-			delete: async (input: IndicesDelete, options?: TransportRequestOptions) => {
+			delete: async (
+				input: RequestParams.IndicesDelete,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.delete(input, options);
 				return output;
 			},
-			exists: async (input: IndicesExists | undefined, options?: TransportRequestOptions) => {
+			exists: async (
+				input: RequestParams.IndicesExists | undefined,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<boolean, unknown>> => {
 				const output = await elasticSearchClient.indices.exists(input, options);
 				return output;
 			},
-			getMapping: async (input: IndicesGetMapping, options?: TransportRequestOptions) => {
+			getMapping: async (
+				input: RequestParams.IndicesGetMapping,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.getMapping(input, options);
 				return output;
 			},
 			putSettings: async (
-				input: IndicesPutSettings<Record<string, any>> | undefined,
+				input: RequestParams.IndicesPutSettings<Record<string, any>> | undefined,
 				options?: TransportRequestOptions,
-			) => {
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.putSettings(input, options);
 				return output;
 			},
 			putMapping: async (
-				input: IndicesPutMapping<Record<string, any>> | undefined,
+				input: RequestParams.IndicesPutMapping<Record<string, any>> | undefined,
 				options?: TransportRequestOptions,
-			) => {
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.putMapping(input, options);
 				return output;
 			},
-			open: async (input: any, options?: TransportRequestOptions) => {
+			open: async (
+				input: RequestParams.IndicesOpen,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.open(input, options);
 				return output;
 			},
-			refresh: async (input: any, options?: TransportRequestOptions) => {
+			refresh: async (
+				input: RequestParams.IndicesRefresh,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.indices.refresh(input, options);
 				return output;
 			},
 		},
 		cat: {
-			aliases: async (input: any, options?: TransportRequestOptions) => {
+			aliases: async (
+				input: RequestParams.CatAliases,
+				options?: TransportRequestOptions,
+			): Promise<ApiResponse<Record<string, any>, unknown>> => {
 				const output = await elasticSearchClient.cat.aliases(input, options);
 				return output;
 			},
 		},
-		bulk: async (input: any, options?: TransportRequestOptions) => {
+		bulk: async (
+			input: RequestParams.Bulk<Record<string, any>[]> | undefined,
+			options?: TransportRequestOptions,
+		): Promise<ApiResponse<Record<string, any>, unknown>> => {
 			const output = await elasticSearchClient.bulk(input, options);
 			return output;
 		},
-		index: async (input: any, options?: TransportRequestOptions) => {
+		index: async (
+			input: RequestParams.Index<Record<string, any>> | undefined,
+			options?: TransportRequestOptions,
+		): Promise<ApiResponse<Record<string, any>, unknown>> => {
 			const output = await elasticSearchClient.index(input, options);
 			return output;
 		},
-		search: async (input: any, options?: TransportRequestOptions) => {
+		search: async (
+			input: RequestParams.Search<Record<string, any>> | undefined,
+			options?: TransportRequestOptions,
+		): Promise<ApiResponse<Record<string, any>, unknown>> => {
 			const output = await elasticSearchClient.search(input, options);
 			return output;
 		},
-		update: async (input: any, options?: TransportRequestOptions) => {
+		update: async (
+			input: RequestParams.Update<Record<string, any>> | undefined,
+			options?: TransportRequestOptions,
+		): Promise<ApiResponse<Record<string, any>, unknown>> => {
 			const output = await elasticSearchClient.update(input, options);
 			return output;
 		},
-		create: async (input: any, options?: TransportRequestOptions) => {
+		create: async (
+			input: RequestParams.Create<Record<string, any>> | undefined,
+			options?: TransportRequestOptions,
+		): Promise<ApiResponse<Record<string, any>, unknown>> => {
 			const output = await elasticSearchClient.create(input, options);
 			return output;
 		},
-		delete: async (input: any, options?: TransportRequestOptions) => {
+		delete: async (
+			input: RequestParams.Delete,
+			options?: TransportRequestOptions,
+		): Promise<ApiResponse<Record<string, any>, unknown>> => {
 			const output = await elasticSearchClient.delete(input, options);
 			return output;
 		},
-		deleteByQuery: async (input: any, options?: TransportRequestOptions) => {
+		deleteByQuery: async (
+			input: RequestParams.DeleteByQuery<Record<string, any>> | undefined,
+			options?: TransportRequestOptions,
+		): Promise<ApiResponse<Record<string, any>, unknown>> => {
 			const output = await elasticSearchClient.deleteByQuery(input, options);
 			return output;
 		},
--- modules/server/src/searchClient/createOpenSearchClient.ts
+++ modules/server/src/searchClient/createOpenSearchClient.ts
@@ -1,25 +1,5 @@
-import {
-	Client,
-	type ClientOptions,
-	type Bulk_Request,
-	type Cat_Aliases_Request,
-	type Create_Request,
-	type Delete_Request,
-	type DeleteByQuery_Request,
-	type Index_Request,
-	type Indices_Close_Request,
-	type Indices_Create_Request,
-	type Indices_Delete_Request,
-	type Indices_Exists_Request,
-	type Indices_GetMapping_Request,
-	type Indices_PutSettings_Request,
-	type Indices_PutMapping_Request,
-	type Indices_Open_Request,
-	type Indices_Refresh_Request,
-	type Search_Request,
-	type Update_Request,
-	type TransportRequestOptions,
-} from '@opensearch-project/opensearch';
+import { Client, type ClientOptions, type API } from '@opensearch-project/opensearch';
+import { type TransportRequestOptions } from '@opensearch-project/opensearch/lib/Transport.js';
 
 import type { SearchClient } from './types.js';
 
@@ -32,76 +12,110 @@ export function createOpenSearchClient(options: OSClientOptions): SearchClient {
 
 	const searchClient: SearchClient = {
 		indices: {
-			close: async (input: Indices_Close_Request, options?: TransportRequestOptions) => {
+			close: async (
+				input: API.Indices_Close_Request,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_Close_Response> => {
 				const output = await openSearchClient.indices.close(input, options);
 				return output;
 			},
-			create: async (input: Indices_Create_Request, options?: TransportRequestOptions) => {
+			create: async (
+				input: API.Indices_Create_Request,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_Create_Response> => {
 				const output = await openSearchClient.indices.create(input, options);
 				return output;
 			},
-			delete: async (input: Indices_Delete_Request, options?: TransportRequestOptions) => {
+			delete: async (
+				input: API.Indices_Delete_Request,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_Delete_Response> => {
 				const output = await openSearchClient.indices.delete(input, options);
 				return output;
 			},
-			exists: async (input: Indices_Exists_Request, options?: TransportRequestOptions) => {
+			exists: async (
+				input: API.Indices_Exists_Request,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_Exists_Response> => {
 				const output = await openSearchClient.indices.exists(input, options);
 				return output;
 			},
-			getMapping: async (input: Indices_GetMapping_Request | undefined, options?: TransportRequestOptions) => {
+			getMapping: async (
+				input: API.Indices_GetMapping_Request | undefined,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_GetMapping_Response> => {
 				const output = await openSearchClient.indices.getMapping(input, options);
 				return output;
 			},
-			putSettings: async (input: Indices_PutSettings_Request, options?: TransportRequestOptions) => {
+			putSettings: async (
+				input: API.Indices_PutSettings_Request,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_PutSettings_Response> => {
 				const output = await openSearchClient.indices.putSettings(input, options);
 				return output;
 			},
-			putMapping: async (input: Indices_PutMapping_Request, options?: TransportRequestOptions) => {
+			putMapping: async (
+				input: API.Indices_PutMapping_Request,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_PutMapping_Response> => {
 				const output = await openSearchClient.indices.putMapping(input, options);
 				return output;
 			},
-			open: async (input: Indices_Open_Request, options?: TransportRequestOptions) => {
+			open: async (
+				input: API.Indices_Open_Request,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_Open_Response> => {
 				const output = await openSearchClient.indices.open(input, options);
 				return output;
 			},
-			refresh: async (input: Indices_Refresh_Request | undefined, options?: TransportRequestOptions) => {
+			refresh: async (
+				input: API.Indices_Refresh_Request | undefined,
+				options?: TransportRequestOptions,
+			): Promise<API.Indices_Refresh_Response> => {
 				const output = await openSearchClient.indices.refresh(input, options);
 				return output;
 			},
 		},
 		cat: {
-			aliases: async (input: Cat_Aliases_Request | undefined, options?: TransportRequestOptions) => {
+			aliases: async (
+				input: API.Cat_Aliases_Request | undefined,
+				options?: TransportRequestOptions,
+			): Promise<API.Cat_Aliases_Response> => {
 				const output = await openSearchClient.cat.aliases(input, options);
 				return output;
 			},
 		},
-		bulk: async (input: Bulk_Request, options?: TransportRequestOptions) => {
-			console.log('bulk input', input);
+		bulk: async (input: API.Bulk_Request, options?: TransportRequestOptions): Promise<API.Bulk_Response> => {
 			const output = await openSearchClient.bulk(input, options);
-			console.log('bulk output', output);
 			return output;
 		},
-		create: async (input: Create_Request, options?: TransportRequestOptions) => {
+		create: async (input: API.Create_Request, options?: TransportRequestOptions): Promise<API.Create_Response> => {
 			const output = await openSearchClient.create(input, options);
 			return output;
 		},
-		delete: async (input: Delete_Request, options?: TransportRequestOptions) => {
+		delete: async (input: API.Delete_Request, options?: TransportRequestOptions): Promise<API.Delete_Response> => {
 			const output = await openSearchClient.delete(input, options);
 			return output;
 		},
-		deleteByQuery: async (input: DeleteByQuery_Request, options?: TransportRequestOptions) => {
+		deleteByQuery: async (
+			input: API.DeleteByQuery_Request,
+			options?: TransportRequestOptions,
+		): Promise<API.DeleteByQuery_Response> => {
 			const output = await openSearchClient.deleteByQuery(input, options);
 			return output;
 		},
-		index: async (input: Index_Request, options?: TransportRequestOptions) => {
+		index: async (input: API.Index_Request, options?: TransportRequestOptions): Promise<API.Index_Response> => {
 			const output = await openSearchClient.index(input, options);
 			return output;
 		},
-		search: async (input: Search_Request | undefined, options?: TransportRequestOptions) => {
+		search: async (
+			input: API.Search_Request | undefined,
+			options?: TransportRequestOptions,
+		): Promise<API.Search_Response> => {
 			const output = await openSearchClient.search(input, options);
 			return output;
 		},
-		update: async (input: Update_Request, options?: TransportRequestOptions) => {
+		update: async (input: API.Update_Request, options?: TransportRequestOptions): Promise<API.Update_Response> => {
 			const output = await openSearchClient.update(input, options);
 			return output;
 		},
