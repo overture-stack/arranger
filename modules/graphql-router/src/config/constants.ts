b48c44836848b93279cc78dca1b706534fd1aad5
Jon Eubank <joneubank@gmail.com>
2026-05-20 19:43:35 -0400

    Fixing merge errors

--- modules/graphql-router/src/config/constants.ts
+++ modules/graphql-router/src/config/constants.ts
@@ -8,17 +8,15 @@ import {
 } from '@overture-stack/arranger-types/configs/constants';
 
 // individual values
-const DOWNLOAD_MAX_ROWS = 100 as const;
-const DOWNLOAD_STREAM_BUFFER_SIZE = 2000 as const;
-const ES_ARRANGER_SETS = 'arranger-sets' as const;
-const MAX_RESULTS_WINDOW = 10000 as const;
-const ROW_ID_FIELD_NAME = 'id' as const;
-const SEARCH_ENGINE = 'elasticsearch' as const;
+const DOWNLOAD_MAX_ROWS = 100;
+const DOWNLOAD_STREAM_BUFFER_SIZE = 2000;
+const ES_ARRANGER_SETS = 'arranger-sets';
+const MAX_RESULTS_WINDOW = 10000;
+const ROW_ID_FIELD_NAME = 'id';
 
 // fallback configs for module start and testing
 const fallbackCatalogConfigs = {
 	...featureFlagDefaults,
-	[configRootProperties.SEARCH_ENGINE]: SEARCH_ENGINE,
 	[configRootProperties.DOWNLOADS]: {
 		[downloadProperties.ALLOW_CUSTOM_MAX_ROWS]: false,
 		[downloadProperties.MAX_ROWS]: DOWNLOAD_MAX_ROWS,
